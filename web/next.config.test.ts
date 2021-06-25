e638527d2d31183ee27dbc74e2164865d27e1092
Ian Routledge <ian.routledge@nice.org.uk>
2021-06-25 10:48:54 +0100

    NXT-6 Add security-headers (#4)

---
+++ web/next.config.test.ts
@@ -0,0 +1,51 @@
+import nextConfig from "./next.config";
+
+describe("next.config.js", () => {
+	it("should disable x-powered-by header", () => {
+		expect(nextConfig.poweredByHeader).toBe(false);
+	});
+
+	it("should add security headers to all pages", async () => {
+		if (!nextConfig.headers) return expect(nextConfig.headers).toBeTruthy();
+
+		const headers = await nextConfig.headers(),
+			globalHeader = headers.find((h) => h.source === "/(.*)");
+
+		expect(globalHeader?.headers).toMatchInlineSnapshot(`
+		Array [
+		  Object {
+		    "key": "X-DNS-Prefetch-Control",
+		    "value": "on",
+		  },
+		  Object {
+		    "key": "Strict-Transport-Security",
+		    "value": "max-age=31536000; includeSubDomains; preload",
+		  },
+		  Object {
+		    "key": "X-Frame-Options",
+		    "value": "DENY",
+		  },
+		  Object {
+		    "key": "X-XSS-Protection",
+		    "value": "1; mode=block",
+		  },
+		  Object {
+		    "key": "Permissions-Policy",
+		    "value": "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+		  },
+		  Object {
+		    "key": "X-Content-Type-Options",
+		    "value": "nosniff",
+		  },
+		  Object {
+		    "key": "Referrer-Policy",
+		    "value": "strict-origin-when-cross-origin",
+		  },
+		  Object {
+		    "key": "Content-Security-Policy",
+		    "value": "frame-ancestors 'none'",
+		  },
+		]
+	`);
+	});
+});
