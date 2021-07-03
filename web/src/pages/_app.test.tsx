764a24c49864f323dd4a26d1b7fb7c81b6c5930d
Ian Routledge <ian@ediblecode.com>
2021-07-03 19:59:21 +0100

    NXT-4 Add app test for SEO

--- web/src/pages/_app.test.tsx
+++ web/src/pages/_app.test.tsx
@@ -1,44 +1,58 @@
+/* eslint-disable testing-library/no-node-access */
 import type { AppProps } from "next/app";
+import { NextSeo } from "next-seo";
 import { render, screen } from "@testing-library/react";
 
 import NextWebApp from "./_app.page";
 import { getMockRouter } from "@/test-utils";
 
+const FakeComponentWithSEO: AppProps["Component"] = () => (
+	<>
+		<NextSeo title="A test title" />
+		<p>some content</p>
+	</>
+);
+
+const renderApp = (routerParams?: Parameters<typeof getMockRouter>[0]) =>
+	render(
+		<NextWebApp
+			pageProps={{}}
+			Component={FakeComponentWithSEO}
+			router={getMockRouter(routerParams) as AppProps["router"]}
+		/>
+	);
+
 describe("NextWebApp", () => {
-	const FakeComponent: AppProps["Component"] = () => <>some content</>;
+	describe("SEO and meta tags", () => {
+		it("should append '| NICE' to page title", async () => {
+			renderApp();
+
+			expect(document.title).toBe("A test title | NICE");
+		});
+
+		it("should add twitter site meta tag", async () => {
+			renderApp();
+
+			expect(
+				document.querySelector("meta[name='twitter:site']")
+			).toHaveAttribute("content", "@NICEComms");
+		});
+	});
 
 	it("should wrap main element around content", () => {
-		render(
-			<NextWebApp
-				pageProps={{}}
-				Component={FakeComponent}
-				router={getMockRouter({ pathname: "/about" }) as AppProps["router"]}
-			/>
-		);
+		renderApp();
 
 		expect(screen.getByRole("main")).toHaveTextContent("some content");
 	});
 
 	it("should add content-start skip link target id to main element", () => {
-		render(
-			<NextWebApp
-				pageProps={{}}
-				Component={FakeComponent}
-				router={getMockRouter({ pathname: "/about" }) as AppProps["router"]}
-			/>
-		);
+		renderApp();
 
 		expect(screen.getByRole("main")).toHaveAttribute("id", "content-start");
 	});
 
 	it("shouldn't highlight guidance on the menu for routes outside guidance", () => {
-		render(
-			<NextWebApp
-				pageProps={{}}
-				Component={FakeComponent}
-				router={getMockRouter({ pathname: "/about" }) as AppProps["router"]}
-			/>
-		);
+		renderApp({ pathname: "/not-guidance" });
 
 		expect(
 			screen.getByText("NICE guidance", { selector: "header a" })
@@ -46,17 +60,7 @@ describe("NextWebApp", () => {
 	});
 
 	it("should highlight guidance on the menu for routes under guidance", () => {
-		render(
-			<NextWebApp
-				pageProps={{}}
-				Component={FakeComponent}
-				router={
-					getMockRouter({
-						pathname: "/guidance/published",
-					}) as AppProps["router"]
-				}
-			/>
-		);
+		renderApp({ pathname: "/guidance/published" });
 
 		expect(
 			screen.getByText("NICE guidance", { selector: "header a" })
