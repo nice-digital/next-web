5931fb4a748f84552033eaaefeaea072d173855e
Ian Routledge <ian@ediblecode.com>
2021-07-07 15:22:50 +0100

    NXT-10 Add favicons/default meta tags

--- web/src/config/config.ts
+++ web/src/config/config.ts
@@ -8,17 +8,23 @@ const { publicRuntimeConfig, serverRuntimeConfig } = getConfig();
  */
 export interface PublicConfig {
 	/** Name of the environment e.g. _dev_ or _test_ */
-	environment: string;
+	readonly environment: string;
 
 	/** The base URL of the website including protocol and port e.g. http://localhost:3000 for local dev or http://dev.nice.org.uk.
 	 *
 	 * **Note** the lack of trailing slash! It will get prepended to paths that start with a slash. */
-	baseUrl: string;
+	readonly baseUrl: string;
 
 	/**
 	 * The absolute URL to the NICE cookie banner script include
 	 */
-	cookieBannerScriptUrl: string;
+	readonly cookieBannerScriptUrl: string;
+
+	/**
+	 * The base URL to the deployed NextJS _public_ folder, see https://nextjs.org/docs/basic-features/static-file-serving.
+	 * Empty string means relative to the deployed app. Set it to an absolute path in _config.yml_ to use a CDN.
+	 */
+	readonly publicBaseUrl: string;
 }
 
 /**
---
+++ web/src/pages/next-seo.config.ts
@@ -0,0 +1,127 @@
+import { NextSeoProps } from "next-seo";
+import { publicRuntimeConfig } from "@/config";
+
+/**
+ * Gets the default SEO config for the given page.
+ *
+ * @param pathname The path of the current page
+ * @returns The Next SEO config object
+ */
+export const getDefaultSeoConfig = (pathname: string): NextSeoProps => ({
+	titleTemplate: `%s | NICE`,
+	openGraph: {
+		type: "website",
+		locale: "en_GB",
+		url: publicRuntimeConfig.baseUrl + pathname,
+		site_name: "NICE",
+		images: [
+			// Landscape open graph image used on Twitter's 'summary_large_image' card type and Facebook (+ others)
+			{
+				url:
+					publicRuntimeConfig.publicBaseUrl +
+					"/open-graph/open-graph-1200x630.png",
+				width: 1200,
+				height: 630,
+				alt: "Logo for NICE (The National Institute for Health and Care Excellence)",
+			},
+			// Square open graph image: used for Twitter's 'summary' card type
+			{
+				url:
+					publicRuntimeConfig.publicBaseUrl +
+					"/open-graph/open-graph-1200x1200.png",
+				width: 1200,
+				height: 1200,
+				alt: "Logo for NICE (The National Institute for Health and Care Excellence)",
+			},
+		],
+	},
+	twitter: {
+		handle: "@NICEComms",
+		site: "@NICEComms",
+		cardType: "summary",
+	},
+	additionalLinkTags: [
+		// Old school favicon
+		{
+			rel: "icon",
+			href: publicRuntimeConfig.publicBaseUrl + "/favicon.ico",
+		},
+		// PNG 'favicons'
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "16x16",
+			href: publicRuntimeConfig.publicBaseUrl + "/icons/icon-16x16.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "32x32",
+			href: publicRuntimeConfig.publicBaseUrl + "/icons/icon-32x32.png",
+		},
+		// Apple icons
+		{
+			// iPhone (X/Plus)
+			rel: "apple-touch-icon",
+			sizes: "120x120",
+			href: publicRuntimeConfig.publicBaseUrl + "/icons/icon-120x120.png",
+		},
+		{
+			// iPad, iPad mini
+			rel: "apple-touch-icon",
+			sizes: "152x152",
+			href: publicRuntimeConfig.publicBaseUrl + "/icons/icon-152x152.png",
+		},
+		{
+			// iPad Pro
+			rel: "apple-touch-icon",
+			sizes: "167x167",
+			href: publicRuntimeConfig.publicBaseUrl + "/icons/icon-167x167.png",
+		},
+		{
+			// iPhone
+			rel: "apple-touch-icon",
+			sizes: "180x180",
+			href: publicRuntimeConfig.publicBaseUrl + "/icons/icon-180x180.png",
+		},
+		// Safari pinned tab as per https://edibleco.de/3dRyFge
+		{
+			rel: "mask-icon",
+			color: "black",
+			href: publicRuntimeConfig.publicBaseUrl + "/icons/safari-pinned-tab.svg",
+		},
+	],
+	additionalMetaTags: [
+		{
+			name: "viewport",
+			content: "width=device-width, initial-scale=1, shrink-to-fit=no",
+		},
+		{
+			name: "application-name",
+			content: "NICE",
+		},
+		// Apple specific as per https://edibleco.de/3jQIcIf
+		{
+			name: "apple-mobile-web-app-capable",
+			content: "yes",
+		},
+		{
+			name: "apple-mobile-web-app-title",
+			content: "NICE",
+		},
+		// Theme colours
+		{
+			name: "theme-color",
+			content: "#004650",
+		},
+		{
+			name: "msapplication-TileColor",
+			content: "#004650",
+		},
+		// IE11 rendering
+		{
+			httpEquiv: "x-ua-compatible",
+			content: "IE=edge; chrome=1",
+		},
+	],
+});
