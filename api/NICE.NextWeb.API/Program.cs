b070524186d43dcf92ec0ed281719528c3b945f4
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-09-06 11:45:54 +0100

    NXT-18 Update case

--- api/NICE.NextWeb.API/Program.cs
+++ api/NICE.NextWeb.API/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NICE.NextWeb.Api
+namespace NICE.NextWeb.API
 {
     public class Program
     {
--- api/NICE.NextWeb.API/Startup.cs
+++ api/NICE.NextWeb.API/Startup.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NICE.NextWeb.Api
+namespace NICE.NextWeb.API
 {
     public class Startup
     {
