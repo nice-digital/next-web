4842d5853a7d466e8161b51458d3385b37e624f7
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-09-07 11:29:40 +0100

    NXT-35 Add basic testing project

---
+++ api/NICE.NextWeb.API.Tests/GeneralTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using NICE.NextWeb.API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NICE.NextWeb.API.Tests
+{
+	[TestFixture()]
+	public class GeneralTests
+	{
+		[Test()]
+		public void BasicTest()
+		{
+			Assert.Pass();
+		}
+	}
+}
