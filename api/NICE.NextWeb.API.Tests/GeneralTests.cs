fe32448d23c74865fafc21893a6b591a13109d06
Colin M <12713935+colinmazu@users.noreply.github.com>
2021-09-07 16:07:25 +0100

    NXT-35 Add testing project to API (#17)
    * NXT-18 First commit
    
    * NXT-18 Add readme
    
    * NXT-18 Update message
    
    * NXT-18 Added is packable to config
    
    * NXT-18 Update case
    
    * NXT-18 Remove is packable
    
    * NXT-35 Add basic testing project
    
    * NXT-35 Swapped to xUnit

---
+++ api/NICE.NextWeb.API.Tests/GeneralTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace NICE.NextWeb.API.Tests
+{
+	public class GeneralTests
+	{
+		[Fact()]
+		public void SampleTest()
+		{
+			Assert.True(true, "This test needs an implementation");
+		}
+	}
+}
