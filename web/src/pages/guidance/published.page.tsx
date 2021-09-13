e56d91e815328eadb8262b1f6401dab0b13a80a1
Warren <w@rren.dev>
2021-09-13 17:19:52 +0100

    NXT-24 Set inner html on endpoint titles

--- web/src/pages/guidance/published.page.tsx
+++ web/src/pages/guidance/published.page.tsx
@@ -143,7 +143,10 @@ export function Published({
 									return (
 										<tr key={id}>
 											<td>
-												<a href={pathAndQuery}>{title}</a>
+												<a
+													href={pathAndQuery}
+													dangerouslySetInnerHTML={{ __html: title }}
+												/>
 											</td>
 											<td>{guidanceRef}</td>
 											<td>
