7ced403af8d508fb2a23cc465ae20995225ac571
Ian Routledge <ian@ediblecode.com>
2021-09-21 16:51:11 +0100

    NXT-29 Add published page functionality

--- web/src/pages/guidance/published.page.test.tsx
+++ web/src/pages/guidance/published.page.test.tsx
@@ -1,4 +1,11 @@
-import { SearchResultsSuccess } from "@nice-digital/search-client/types";
+import { fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useRouter } from "next/router";
+
+import {
+	SearchResultsSuccess,
+	SearchUrl,
+} from "@nice-digital/search-client/types";
 
 import { render, screen, within } from "@/test-utils";
 import { formatDateStr } from "@/utils/index";
@@ -6,28 +13,61 @@ import { formatDateStr } from "@/utils/index";
 import sampleData from "./__mocks__/published.sample.json";
 import { Published } from "./published.page";
 
-jest.mock("next/router", () => ({
-	useRouter() {
-		return {
+describe("/guidance/published", () => {
+	let routerPush: jest.Mock;
+	beforeEach(() => {
+		routerPush = jest.fn();
+
+		(useRouter as jest.Mock).mockImplementation(() => ({
 			route: "/",
 			pathname: "",
 			query: "",
 			asPath: "",
-		};
-	},
-}));
+			push: routerPush,
+		}));
 
-describe("/guidance/published", () => {
-	describe("Table", () => {
-		beforeEach(() => {
-			render(
-				<Published
-					activeModifiers={[]}
-					results={sampleData as unknown as SearchResultsSuccess}
-				/>
-			);
+		render(
+			<Published
+				activeModifiers={[]}
+				results={sampleData as unknown as SearchResultsSuccess}
+				searchUrl={{} as SearchUrl}
+			/>
+		);
+	});
+
+	describe("Title filter", () => {
+		it("should render title filter input box and label", () => {
+			expect(
+				screen.getByLabelText("Filter by title or keyword")
+			).toBeInTheDocument();
 		});
 
+		it("should render placeholder attribute on title filter input", () => {
+			expect(
+				screen.getByLabelText("Filter by title or keyword")
+			).toHaveAttribute("placeholder", "E.g. 'diabetes' or 'NG28'");
+		});
+
+		it("should render search submit button", () => {
+			expect(screen.getByText("Search")).toBeInTheDocument();
+			expect(screen.getByText("Search")).toHaveProperty("tagName", "BUTTON");
+			expect(screen.getByText("Search")).toHaveAttribute("type", "submit");
+		});
+
+		it("should use NextJS router with serialized form on search button click", () => {
+			const input = screen.getByLabelText("Filter by title or keyword"),
+				button = screen.getByText("Search");
+
+			userEvent.type(input, "diabetes");
+			userEvent.click(button);
+
+			expect(routerPush).toHaveBeenCalledWith("?q=diabetes", undefined, {
+				scroll: false,
+			});
+		});
+	});
+
+	describe("Table", () => {
 		describe("Column headings", () => {
 			it.each([
 				["Title", 1],
--- web/src/pages/guidance/published.page.tsx
+++ web/src/pages/guidance/published.page.tsx
@@ -2,7 +2,13 @@ import serialize from "form-serialize";
 import { GetServerSidePropsContext } from "next";
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
-import { createRef } from "react";
+import React, {
+	createRef,
+	useCallback,
+	useEffect,
+	useMemo,
+	useState,
+} from "react";
 
 import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
 import {
@@ -22,20 +28,22 @@ import {
 	getActiveModifiers,
 	Modifier,
 	SearchUrl,
+	SearchResultsSuccess,
 } from "@nice-digital/search-client";
 
+import { Announcer } from "@/components/Announcer/Announcer";
+import { ErrorPageContent } from "@/components/ErrorPageContent/ErrorPageContent";
 import { GuidanceListNav } from "@/components/GuidanceListNav/GuidanceListNav";
 import { InlineTextFilter } from "@/components/InlineTextFilter/InlineTextFilter";
+import { Link } from "@/components/Link/Link";
 import { publicRuntimeConfig } from "@/config";
+import { logger } from "@/logger";
 import { formatDateStr } from "@/utils/index";
 
 import styles from "./published.module.scss";
 
-import type { Except } from "type-fest";
-
-const searchUrlDefaults: Except<SearchUrl, "fullUrl" | "route"> = {
+const searchUrlDefaults = {
 	s: "Date",
-	om: `[{"gst":["Published"]}]`,
 	ps: 10,
 };
 
@@ -47,45 +55,71 @@ interface PublishedGuidancePageProps {
 
 export function Published({
 	results,
-	searchUrl,
+	searchUrl: { q, s },
 	activeModifiers,
 }: PublishedGuidancePageProps): JSX.Element {
-	const router = useRouter();
+	const router = useRouter(),
+		formRef = createRef<HTMLFormElement>(),
+		doClientSideFormSubmit = useCallback(() => {
+			if (formRef.current) {
+				const url = serialize(formRef.current);
+				router.push(url ? "?" + url : "", undefined, { scroll: false });
+			}
+		}, [formRef, router]),
+		formSubmitHandler = useCallback(
+			(e: React.FormEvent<HTMLFormElement>) => {
+				if (formRef.current) {
+					e.preventDefault();
+					doClientSideFormSubmit();
+				}
+			},
+			[formRef, doClientSideFormSubmit]
+		),
+		// Announcement text, used for giving audible notifications to screen readers when results have changed
+		[announcement, setAnnouncement] = useState(""),
+		// Cache the breadcrumbs as their static and it means we can use them on the error view and success view
+		breadcrumbs = useMemo(
+			() => (
+				<Breadcrumbs>
+					<Breadcrumb to="/">Home</Breadcrumb>
+					<Breadcrumb to="/guidance">NICE guidance</Breadcrumb>
+					<Breadcrumb>
+						Published guidance, quality standards and&nbsp;advice
+					</Breadcrumb>
+				</Breadcrumbs>
+			),
+			[]
+		),
+		{
+			documents,
+			navigators,
+			pageSize,
+			firstResult,
+			lastResult,
+			resultCount,
+			unfilteredResultsUrl,
+		} = results as SearchResultsSuccess;
+
+	useEffect(() => {
+		setAnnouncement(
+			`Showing ${firstResult} to ${lastResult} of ${resultCount}`
+		);
+	}, [firstResult, lastResult, resultCount]);
 
 	if (results.failed)
 		return (
 			<>
-				Error: todo {results.errorMessage} {results.debug?.rawResponse}
+				<ErrorPageContent breadcrumbs={breadcrumbs} />
 			</>
 		);
-
-	const ref = createRef();
-
-	function submitForm() {
-		console.log(serialize(ref.current));
-		router.push("?" + serialize(ref.current), undefined, { scroll: false });
-	}
-
-	function handleChange() {
-		submitForm();
-	}
-
-	function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
-		e.preventDefault();
-		submitForm();
-	}
 
 	return (
 		<>
 			<NextSeo title="Published guidance, quality standards and advice" />
 
-			<Breadcrumbs>
-				<Breadcrumb to="/">Home</Breadcrumb>
-				<Breadcrumb to="/guidance">NICE guidance</Breadcrumb>
-				<Breadcrumb>
-					Published guidance, quality standards and&nbsp;advice
-				</Breadcrumb>
-			</Breadcrumbs>
+			<Announcer announcement={announcement} />
+
+			{breadcrumbs}
 
 			<PageHeader
 				preheading="Published"
@@ -96,110 +130,167 @@ export function Published({
 
 			<Grid gutter="loose" className={styles.sectionWrapper}>
 				<GridItem cols={12} md={4} lg={3} className={styles.panelWrapper}>
-					<FilterPanel heading="Filter" innerRef={ref} onSubmit={handleSubmit}>
-						<InlineTextFilter
-							label="Filter by title"
-							name="q"
-							defaultValue={searchUrl.q}
-							placeholder="Enter title or keyword"
+					<FilterPanel
+						aria-label="Filter results"
+						heading="Filter"
+						innerRef={formRef}
+						onSubmit={formSubmitHandler}
+					>
+						<input
+							type="hidden"
+							name="ps"
+							value={pageSize === searchUrlDefaults.ps ? "" : pageSize}
 						/>
-						{results.navigators.map(({ shortName, displayName, modifiers }) => (
-							<FilterGroup
-								key={shortName}
-								heading={displayName}
-								selectedCount={
-									modifiers.filter((modifier) => modifier.active).length
-								}
-							>
-								{modifiers.map((modifier) => (
-									<FilterOption
-										key={modifier.displayName}
-										isSelected={modifier.active}
-										onChanged={handleChange}
-										value={modifier.displayName}
-									>
-										{modifier.displayName}
-									</FilterOption>
-								))}
-							</FilterGroup>
-						))}
+						<input
+							type="hidden"
+							name="s"
+							value={s === searchUrlDefaults.s ? "" : s}
+						/>
+						<InlineTextFilter
+							label="Filter by title or keyword"
+							name="q"
+							defaultValue={q}
+							placeholder="E.g. 'diabetes' or 'NG28'"
+						/>
+						{navigators
+							.filter((nav) => nav.shortName !== "gst")
+							.map(({ shortName, displayName, modifiers }) => (
+								<FilterGroup
+									key={shortName}
+									heading={displayName}
+									id={shortName}
+									selectedCount={
+										modifiers.filter((modifier) => modifier.active).length
+									}
+								>
+									{modifiers.map((modifier) => (
+										<FilterOption
+											key={modifier.displayName}
+											isSelected={modifier.active}
+											onChanged={doClientSideFormSubmit}
+											groupId={shortName}
+											value={modifier.displayName}
+										>
+											{`${modifier.displayName} (${modifier.resultCount})`}
+										</FilterOption>
+									))}
+								</FilterGroup>
+							))}
 					</FilterPanel>
 				</GridItem>
 
 				<GridItem cols={12} md={8} lg={9}>
 					<FilterSummary
 						id="filter-summary"
-						activeFilters={activeModifiers.map((modifier) => ({
-							label: modifier.displayName,
-							destination: modifier.toggleUrl.fullUrl,
-						}))}
-						sorting={[{ label: "Date" }, { label: "Title" }]}
+						activeFilters={activeModifiers.map(
+							({ displayName, toggleUrl }) => ({
+								label: displayName,
+								destination: toggleUrl.fullUrl,
+								method: "href",
+								elementType: ({ children, ...props }) => (
+									<Link {...props} scroll={false}>
+										<a>{children}</a>
+									</Link>
+								),
+							})
+						)}
+						sorting={[
+							{
+								active: true,
+								label: s === "Title" ? "Title" : "Date",
+							},
+							{
+								active: false,
+								label: s !== "Title" ? "Title" : "Date",
+								destination: "?s=" + (s !== "Title" ? "Title" : "Date"),
+								elementType: ({ ...props }) => (
+									<Link {...props} scroll={false} />
+								),
+							},
+						]}
 					>
-						Showing {results.firstResult} to {results.lastResult} of{" "}
-						{results.resultCount}
+						{resultCount === 0 ? (
+							"Showing 0 results"
+						) : (
+							<>
+								Showing {firstResult} to {lastResult} of {resultCount}
+							</>
+						)}
 					</FilterSummary>
 
-					<Table aria-describedby="filter-summary">
-						<caption className="visually-hidden">
-							Published guidance, quality standards and advice
-						</caption>
-						<thead>
-							<tr>
-								<th scope="col">Title</th>
-								<th scope="col">Reference number</th>
-								<th scope="col">Published</th>
-								<th scope="col">Last updated</th>
-							</tr>
-						</thead>
-						<tbody>
-							{results.documents.map(
-								({
-									id,
-									title,
-									guidanceRef,
-									publicationDate,
-									lastUpdated,
-									pathAndQuery,
-								}) => {
-									return (
-										<tr key={id}>
-											<td>
-												<a
-													href={pathAndQuery}
-													dangerouslySetInnerHTML={{ __html: title }}
-												/>
-											</td>
-											<td>{guidanceRef}</td>
-											<td>
-												<time
-													className={styles.tableDate}
-													dateTime={String(publicationDate)}
-													data-shortdate={formatDateStr(
-														String(publicationDate),
-														true
-													)}
-												>
-													<span>{formatDateStr(String(publicationDate))}</span>
-												</time>
-											</td>
-											<td>
-												<time
-													className={styles.tableDate}
-													dateTime={String(lastUpdated)}
-													data-shortdate={formatDateStr(
-														String(lastUpdated),
-														true
-													)}
-												>
-													<span>{formatDateStr(String(lastUpdated))}</span>
-												</time>
-											</td>
-										</tr>
-									);
-								}
-							)}
-						</tbody>
-					</Table>
+					{documents.length === 0 ? (
+						<p>
+							We can&apos;t find any guidance or advice. Try{" "}
+							<Link to={unfilteredResultsUrl?.fullUrl as string} scroll={false}>
+								clearing your filters
+							</Link>{" "}
+							and starting again.
+						</p>
+					) : (
+						<Table aria-describedby="filter-summary">
+							<caption className="visually-hidden">
+								Published guidance, quality standards and advice
+							</caption>
+							<thead>
+								<tr>
+									<th scope="col">Title</th>
+									<th scope="col">Reference number</th>
+									<th scope="col">Published</th>
+									<th scope="col">Last updated</th>
+								</tr>
+							</thead>
+							<tbody>
+								{documents.map(
+									({
+										id,
+										title,
+										guidanceRef,
+										publicationDate,
+										lastUpdated,
+										pathAndQuery,
+									}) => {
+										return (
+											<tr key={id}>
+												<td>
+													<a
+														href={pathAndQuery}
+														dangerouslySetInnerHTML={{ __html: title }}
+													/>
+												</td>
+												<td>{guidanceRef}</td>
+												<td>
+													<time
+														className={styles.tableDate}
+														dateTime={String(publicationDate)}
+														data-shortdate={formatDateStr(
+															String(publicationDate),
+															true
+														)}
+													>
+														<span>
+															{formatDateStr(String(publicationDate))}
+														</span>
+													</time>
+												</td>
+												<td>
+													<time
+														className={styles.tableDate}
+														dateTime={String(lastUpdated)}
+														data-shortdate={formatDateStr(
+															String(lastUpdated),
+															true
+														)}
+													>
+														<span>{formatDateStr(String(lastUpdated))}</span>
+													</time>
+												</td>
+											</tr>
+										);
+									}
+								)}
+							</tbody>
+						</Table>
+					)}
 				</GridItem>
 			</Grid>
 		</>
@@ -212,14 +303,28 @@ export const getServerSideProps = async (
 	initialise({
 		baseURL: publicRuntimeConfig.search.baseURL,
 		index: "guidance",
+		usePrettyUrls: true,
 	});
 
 	const searchUrl: SearchUrl = {
 			...searchUrlDefaults,
 			...getSearchUrl(context.resolvedUrl),
+			// Pre filter results to just show published products for this tab
+			...{ gst: "Published" },
 		},
 		results = await search(searchUrl),
-		activeModifiers = results.failed ? [] : getActiveModifiers(results);
+		activeModifiers = results.failed
+			? []
+			: getActiveModifiers(results).filter(
+					// We pre filter by guidance status so don't want to be able to remove it
+					(mod) => mod.navigatorShortName !== "gst"
+			  );
+
+	if (results.failed)
+		logger.error(
+			`Error loading guidance from search on page ${context.resolvedUrl}: ${results.errorMessage}`,
+			results.debug
+		);
 
 	return {
 		props: {
