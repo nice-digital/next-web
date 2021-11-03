0e4f8d0cdab8d61de561fff60bd9b7fbfe7b293f
w@rren <w@rren.dev>
2021-11-03 14:44:42 +0000

    NXT-69 Initial search template

---
+++ web/src/pages/search.page.tsx
@@ -0,0 +1,13 @@
+export default function SearchPage(): JSX.Element {
+	return (
+		<>
+			<h1>Search results page</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam quasi
+				veniam, numquam voluptatibus doloremque illo architecto nesciunt amet
+				mollitia alias quae ab ducimus provident quis ullam laboriosam ad error
+				sint!
+			</p>
+		</>
+	);
+}
