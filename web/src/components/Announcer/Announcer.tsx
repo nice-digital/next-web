ac3da9b7a3646e75897ba914127a164e9317283a
John Davey <john.davey@nice.org.uk>
2021-11-05 16:49:39 +0000

    NXT-63 Append spaces to announcement to ensure it is read out by screenreader

--- web/src/components/Announcer/Announcer.tsx
+++ web/src/components/Announcer/Announcer.tsx
@@ -1,43 +1,35 @@
-import { useEffect, FC, useRef } from "react";
+import { useEffect, FC } from "react";
+import { isNoSubstitutionTemplateLiteral } from "typescript";
 
 export interface AnnounceProps {
 	announcement: string;
 }
 
 export const Announcer: FC<AnnounceProps> = ({ announcement }) => {
-	function usePrevious(value) {
-		const ref = useRef();
-		useEffect(() => {
-			ref.current = value;
-		});
-		return ref.current;
-	}
-
-	const previousAnnouncement = usePrevious(announcement);
-
 	useEffect(() => {
 		// Use NextJS's built in announcer div so we don't need to create our own
 		const nextJSRouteAnnouncer = document.getElementById(
 			"__next-route-announcer__"
 		);
-		if (nextJSRouteAnnouncer?.hasAttribute("aria-atomic") !== true) {
+		if (!nextJSRouteAnnouncer?.hasAttribute("aria-atomic")) {
 			nextJSRouteAnnouncer?.setAttribute("aria-atomic", "true");
 		}
-		let checkedAnnouncement = "";
-		if (nextJSRouteAnnouncer && announcement == previousAnnouncement) {
-			console.log("it's the same", announcement, " ", previousAnnouncement);
-			// const randomNumber = Math.floor(Math.random() * 101).toString();
-			// checkedAnnouncement = announcement + randomNumber;
-			checkedAnnouncement = announcement;
-		} else {
-			console.log("it's different", announcement, " ", previousAnnouncement);
-			checkedAnnouncement = announcement;
-		}
+
+		const appendSpaces = () => {
+			let stringToAppend = "";
+			let counter = 0;
+			const randomNumber = Math.floor(Math.random() * 25);
+			while (counter < randomNumber) {
+				counter++;
+				stringToAppend += "\xa0";
+			}
+			return stringToAppend;
+		};
 
 		if (nextJSRouteAnnouncer) {
 			nextJSRouteAnnouncer.textContent = "";
 			window.requestAnimationFrame((_timestamp) => {
-				nextJSRouteAnnouncer.textContent = checkedAnnouncement;
+				nextJSRouteAnnouncer.textContent = announcement + appendSpaces();
 			});
 		}
 	});
