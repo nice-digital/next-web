1831c790a29a04ac7929d332ae92769e98b4fade
John Davey <john.davey@nice.org.uk>
2021-11-05 14:52:03 +0000

    NXT-63 debugging for repeat announcements not being announced by screenreader issue

--- web/src/components/Announcer/Announcer.tsx
+++ web/src/components/Announcer/Announcer.tsx
@@ -1,22 +1,41 @@
-import { useEffect, FC } from "react";
+import { useEffect, FC, useRef } from "react";
 
 export interface AnnounceProps {
 	announcement: string;
 }
 
 export const Announcer: FC<AnnounceProps> = ({ announcement }) => {
+	function usePrevious(value) {
+		const ref = useRef();
+		useEffect(() => {
+			ref.current = value;
+		});
+		return ref.current;
+	}
+
+	const previousAnnouncement = usePrevious(announcement);
+
 	useEffect(() => {
 		// Use NextJS's built in announcer div so we don't need to create our own
 		const nextJSRouteAnnouncer = document.getElementById(
 			"__next-route-announcer__"
 		);
+		let checkedAnnouncement = "";
+		if (nextJSRouteAnnouncer && announcement == previousAnnouncement) {
+			console.log("it's the same", announcement, " ", previousAnnouncement);
+			const randomNumber = Math.floor(Math.random() * 101).toString();
+			checkedAnnouncement = announcement + randomNumber;
+		} else {
+			console.log("it's different", announcement, " ", previousAnnouncement);
+			checkedAnnouncement = announcement;
+		}
+
 		if (nextJSRouteAnnouncer) {
-			nextJSRouteAnnouncer.textContent = "";
 			window.requestAnimationFrame((_timestamp) => {
-				nextJSRouteAnnouncer.textContent = announcement;
+				nextJSRouteAnnouncer.textContent = checkedAnnouncement;
 			});
 		}
-	}, [announcement]);
+	});
 
 	return null;
 };
