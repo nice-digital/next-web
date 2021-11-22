7f7fd362897cd872fdd49a014aad2e0ae096b8c5
colinmazu <12713935+colinmazu@users.noreply.github.com>
2021-11-22 16:54:55 +0000

    NXT-85 Add slash

--- api/NICE.NextWeb.API/ScheduledTasks/Niceorg/RefreshGuidanceTaxonomyScheduledTask.cs
+++ api/NICE.NextWeb.API/ScheduledTasks/Niceorg/RefreshGuidanceTaxonomyScheduledTask.cs
@@ -20,7 +20,7 @@ namespace NICE.NextWeb.API.ScheduledTasks.Niceorg
         }
 
         public string Schedule => "1,16,31,46 * * * *"; //Cron schedule - “At minute 1, 16, 31, and 46.”
-        public string RefreshUrl => "api/TaxonomyMappings";
+        public string RefreshUrl => "api/TaxonomyMappings/";
 
         public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
