aa90f194694a975a9862d6c9751fc96a9e96b4a0
John Davey <john.davey@nice.org.uk>
2021-12-29 17:17:16 +0000

    NXT-71 Add test for quality statements key link

--- web/src/components/SearchSections/SearchSections.test.tsx
+++ web/src/components/SearchSections/SearchSections.test.tsx
@@ -4,10 +4,19 @@ import { SubSection } from "@/components/SearchCard/SearchCardList";
 import { SearchSections } from "@/components/SearchSections/SearchSections";
 import { render } from "@/test-utils";
 
-const mockParsedLinks: SubSection[] = [
+const mockParsedLinksRec: SubSection[] = [
 	{ $: { url: "/testsubsection/1" }, _: "Test subsection 1" },
 	{ $: { url: "/testsubsection/2" }, _: "Test subsection 2" },
 	{ $: { url: "/chapter/recommendations" }, _: "Recommedations subsection" },
+];
+
+const mockParsedLinksQual: SubSection[] = [
+	{ $: { url: "/testsubsection/1" }, _: "Test subsection 1" },
+	{ $: { url: "/testsubsection/2" }, _: "Test subsection 2" },
+	{
+		$: { url: "/chapter/list-of-quality-statements" },
+		_: "Quality subsection",
+	},
 ];
 
 const defaultProps = {
@@ -15,14 +24,20 @@ const defaultProps = {
 };
 
 beforeEach(() => {
-	render(<SearchSections parsedLinks={mockParsedLinks} {...defaultProps} />);
+	// render(<SearchSections parsedLinks={mockParsedLinks} {...defaultProps} />);
 });
 
 describe("SearchSections", () => {
 	it("should render a progressive disclosure element with the text 'show all sections'", () => {
+		render(
+			<SearchSections parsedLinks={mockParsedLinksRec} {...defaultProps} />
+		);
 		expect(screen.getByText(/show all sections/i)).toBeInTheDocument();
 	});
 	it("should render links to the supplied subsections", () => {
+		render(
+			<SearchSections parsedLinks={mockParsedLinksRec} {...defaultProps} />
+		);
 		expect(
 			screen.getByRole("link", { name: /test subsection 1/i })
 		).toBeInTheDocument();
@@ -32,9 +47,23 @@ describe("SearchSections", () => {
 	});
 
 	it("should render a 'View Recommendations' keyLink", () => {
+		render(
+			<SearchSections parsedLinks={mockParsedLinksRec} {...defaultProps} />
+		);
 		expect(
 			screen.getByRole("link", {
 				name: /view recommendations for TestRef999/i,
+			})
+		).toBeInTheDocument();
+	});
+
+	it("should render a 'View quality statements' keyLink", () => {
+		render(
+			<SearchSections parsedLinks={mockParsedLinksQual} {...defaultProps} />
+		);
+		expect(
+			screen.getByRole("link", {
+				name: /view quality statements for TestRef999/i,
 			})
 		).toBeInTheDocument();
 	});
