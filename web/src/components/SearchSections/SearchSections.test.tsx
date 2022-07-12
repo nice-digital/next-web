9d960317fe3408902f198c531a9014bc5ec66c11
John Davey <john.davey@nice.org.uk>
2022-07-12 12:52:18 +0100

    NXT-71 Update SearchSections unit tests to use 'subSections' named property of search-client Document type

--- web/src/components/SearchSections/SearchSections.test.tsx
+++ web/src/components/SearchSections/SearchSections.test.tsx
@@ -1,42 +1,43 @@
 import { screen } from "@testing-library/react";
 
-import { SubSection } from "@/components/SearchCard/SearchCardList";
+import { Document } from "@nice-digital/search-client";
+
 import { SearchSections } from "@/components/SearchSections/SearchSections";
 import { render } from "@/test-utils";
 
-const mockParsedLinks: SubSection[] = [
-	{ $: { url: "/testsubsection/1" }, _: "Test subsection 1" },
-	{ $: { url: "/testsubsection/2" }, _: "Test subsection 2" },
+const mockParsedLinks: Document["subSections"] = [
+	{ url: "/testsubsection/1", title: "Test subsection 1" },
+	{ url: "/testsubsection/2", title: "Test subsection 2" },
 ];
 
-const mockParsedLinksRec: SubSection[] = [
-	{ $: { url: "/testsubsection/1" }, _: "Test subsection 1" },
-	{ $: { url: "/testsubsection/2" }, _: "Test subsection 2" },
-	{ $: { url: "/chapter/recommendations" }, _: "Recommedations subsection" },
+const mockParsedLinksRec: Document["subSections"] = [
+	{ url: "/testsubsection/1", title: "Test subsection 1" },
+	{ url: "/testsubsection/2", title: "Test subsection 2" },
+	{ url: "/chapter/recommendations", title: "Recommedations subsection" },
 	{
-		$: { url: "/chapter/test-recommendations" },
-		_: "Recommedations subsection 2",
+		url: "/chapter/test-recommendations",
+		title: "Recommedations subsection 2",
 	},
 	{
-		$: { url: "/chapter/test-guidance" },
-		_: "Recommedations subsection 3",
+		url: "/chapter/test-guidance",
+		title: "Recommedations subsection 3",
 	},
 ];
 
-const mockParsedLinksQual: SubSection[] = [
-	{ $: { url: "/testsubsection/1" }, _: "Test subsection 1" },
-	{ $: { url: "/testsubsection/2" }, _: "Test subsection 2" },
+const mockParsedLinksQual: Document["subSections"] = [
+	{ url: "/testsubsection/1", title: "Test subsection 1" },
+	{ url: "/testsubsection/2", title: "Test subsection 2" },
 	{
-		$: { url: "/chapter/list-of-quality-statements" },
-		_: "Quality subsection",
+		url: "/chapter/list-of-quality-statements",
+		title: "Quality subsection",
 	},
 	{
-		$: { url: "/chapter/list-of-statements" },
-		_: "Quality subsection 2",
+		url: "/chapter/list-of-statements",
+		title: "Quality subsection 2",
 	},
 	{
-		$: { url: "/chapter/quality-statements" },
-		_: "Quality subsection 3",
+		url: "/chapter/quality-statements",
+		title: "Quality subsection 3",
 	},
 ];
 
@@ -51,13 +52,13 @@ beforeEach(() => {
 describe("SearchSections", () => {
 	it("should render a progressive disclosure element with the text 'show all sections'", () => {
 		render(
-			<SearchSections parsedLinks={mockParsedLinksRec} {...defaultProps} />
+			<SearchSections subSections={mockParsedLinksRec} {...defaultProps} />
 		);
 		expect(screen.getByText(/show all sections/i)).toBeInTheDocument();
 	});
 	it("should render links to the supplied subsections", () => {
 		render(
-			<SearchSections parsedLinks={mockParsedLinksRec} {...defaultProps} />
+			<SearchSections subSections={mockParsedLinksRec} {...defaultProps} />
 		);
 		expect(
 			screen.getByRole("link", { name: /test subsection 1/i })
@@ -69,7 +70,7 @@ describe("SearchSections", () => {
 
 	it("should render section links with the correct href and link text", () => {
 		render(
-			<SearchSections parsedLinks={mockParsedLinksRec} {...defaultProps} />
+			<SearchSections subSections={mockParsedLinksRec} {...defaultProps} />
 		);
 		expect(
 			screen.getByRole("link", { name: "Recommedations subsection" })
@@ -81,21 +82,21 @@ describe("SearchSections", () => {
 
 	it("should render correct number of recommendations key links", () => {
 		render(
-			<SearchSections parsedLinks={mockParsedLinksRec} {...defaultProps} />
+			<SearchSections subSections={mockParsedLinksRec} {...defaultProps} />
 		);
 		expect(screen.getAllByRole("link").length).toBe(6);
 	});
 
 	it("should render correct number of quality standards key links", () => {
 		render(
-			<SearchSections parsedLinks={mockParsedLinksQual} {...defaultProps} />
+			<SearchSections subSections={mockParsedLinksQual} {...defaultProps} />
 		);
 		expect(screen.getAllByRole("link").length).toBe(6);
 	});
 
 	it("should render a 'View Recommendations' keyLink", () => {
 		render(
-			<SearchSections parsedLinks={mockParsedLinksRec} {...defaultProps} />
+			<SearchSections subSections={mockParsedLinksRec} {...defaultProps} />
 		);
 		expect(
 			screen.getByRole("link", {
@@ -106,7 +107,7 @@ describe("SearchSections", () => {
 
 	it("should render a 'View quality statements' keyLink with the correct href", () => {
 		render(
-			<SearchSections parsedLinks={mockParsedLinksQual} {...defaultProps} />
+			<SearchSections subSections={mockParsedLinksQual} {...defaultProps} />
 		);
 		expect(
 			screen.getByRole("link", {
@@ -121,7 +122,7 @@ describe("SearchSections", () => {
 	});
 
 	it("should NOT render a key link when there is no match for recommendations or quality standards", () => {
-		render(<SearchSections parsedLinks={mockParsedLinks} {...defaultProps} />);
+		render(<SearchSections subSections={mockParsedLinks} {...defaultProps} />);
 
 		expect(
 			screen.queryByRole("link", {
@@ -139,7 +140,7 @@ describe("SearchSections", () => {
 	it("should render a heading including the guidanceRef", () => {
 		render(
 			<SearchSections
-				parsedLinks={mockParsedLinksQual}
+				subSections={mockParsedLinksQual}
 				guidanceRef="TestRef1001"
 			/>
 		);
