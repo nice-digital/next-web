ca405f55f8683c4cfb228dcaaf0a5e5d8d460efd
John Davey <john.davey@nice.org.uk>
2022-07-12 09:04:58 +0100

    NXT-71 Add unit test for search page error condition

--- web/src/pages/search.page.test.tsx
+++ web/src/pages/search.page.test.tsx
@@ -1,10 +1,15 @@
 import mockDate from "mockdate";
 import { useRouter } from "next/router";
 
-import { SearchResultsSuccess, SearchUrl } from "@nice-digital/search-client";
+import {
+	SearchResultsError,
+	SearchResultsSuccess,
+	SearchUrl,
+} from "@nice-digital/search-client";
 
 import { render, screen, waitFor, within } from "@/test-utils";
 
+import sampleDataFailed from "../__mocks__/__data__/search/search-results-failed.json";
 import sampleData from "../__mocks__/__data__/search/search-results.json";
 
 import SearchPage from "./search.page";
@@ -18,23 +23,39 @@ import SearchPage from "./search.page";
 }));
 
 describe("search", () => {
-	let container: HTMLElement;
-	beforeEach(() => {
-		mockDate.set("2020-11-22");
-
-		container = render(
-			<SearchPage
-				activeModifiers={[]}
-				results={sampleData as unknown as SearchResultsSuccess}
-				searchUrl={{ route: "/search?q=" } as SearchUrl}
-			/>
-		).container;
-	});
 	describe("SEO", () => {
-		it.only("should render 'Search results' in the page title", async () => {
+		let container: HTMLElement;
+		beforeEach(() => {
+			mockDate.set("2020-11-22");
+
+			container = render(
+				<SearchPage
+					activeModifiers={[]}
+					results={sampleData as unknown as SearchResultsSuccess}
+					searchUrl={{ route: "/search?q=" } as SearchUrl}
+				/>
+			).container;
+		});
+		it("should render 'Search results' in the page title", async () => {
 			await waitFor(() => {
 				expect(document.title).toStartWith("Search results");
 			});
+		});
+	});
+
+	describe("Error message", () => {
+		it("should render an error heading when search results fail", () => {
+			render(
+				<SearchPage
+					activeModifiers={[]}
+					results={sampleDataFailed as unknown as SearchResultsError}
+					searchUrl={{ route: "/search?q=" } as SearchUrl}
+				/>
+			);
+			const errorHeading = screen.getByRole("heading", {
+				name: "Something's gone wrong",
+			});
+			expect(errorHeading).toBeInTheDocument();
 		});
 	});
 });
