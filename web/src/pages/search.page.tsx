e2d2d64d096de793f8d8e741d06bb5bc5801c9d1
John Davey <john.davey@nice.org.uk>
2022-08-11 10:20:05 +0100

    NXT-71 Refactor and apply template logic for no results, no documents or filters returning no results

--- web/src/pages/search.page.tsx
+++ web/src/pages/search.page.tsx
@@ -56,34 +56,36 @@ const flattenNavigators = (navigators: Navigator[]): Navigator[] => {
 };
 
 export function Search({
-	results,
+	results: data,
 	searchUrl: { q, s, from, to },
 	activeModifiers,
 }: SearchPageProps): JSX.Element {
 	// Announcement text, used for giving audible notifications to screen readers when results have changed
 	const [announcement, setAnnouncement] = useState("");
 
-	const { failed } = results;
+	const { failed } = data;
 
 	const { asPath } = useRouter();
-	const [loading, setLoading] = useState<boolean>();
 
 	const {
 		documents,
 		navigators,
 		pageSize,
+		firstResult,
+		lastResult,
+		resultCount,
 		unfilteredResultsUrl,
-		finalSearchText,
-	} = results as SearchResultsSuccess;
+	} = data as SearchResultsSuccess;
 
-	// useEffect(() => {
-	// 	// TODO check if this is redundant
-	// 	setAnnouncement(
-	// 		`Showing ${firstResult} to ${lastResult} of ${resultCount}`
-	// 	);
-	// }, [firstResult, lastResult, resultCount]);
+	useEffect(() => {
+		setAnnouncement(
+			`Showing ${firstResult} to ${lastResult} of ${resultCount}`
+		);
+	}, [firstResult, lastResult, resultCount]);
 
 	const flattenedNavigators = flattenNavigators(navigators);
+
+	if (failed) return <ErrorPageContent />;
 
 	const SummaryRecordCount = ({
 		firstResult,
@@ -107,8 +109,8 @@ export function Search({
 		);
 	};
 
-	const SummaryText = (results: SearchResultsSuccess) => {
-		const { originalSearch, finalSearchText, resultCount } = results;
+	const SummaryText = (data: SearchResultsSuccess) => {
+		const { originalSearch, finalSearchText, resultCount } = data;
 
 		if (resultCount === 0 && !originalSearch) {
 			return (
@@ -127,33 +129,17 @@ export function Search({
 					Your search for <strong>{originalSearch.searchText}</strong> returned
 					no results
 					<br />
-					<SummaryRecordCount {...results} />
+					<SummaryRecordCount {...data} />
 				</>
 			);
 		}
 
 		if (resultCount !== 0) {
-			return <SummaryRecordCount {...results} />;
+			return <SummaryRecordCount {...data} />;
 		}
 
 		return null;
 	};
-
-	useEffect(() => {
-		if (failed) setAnnouncement("There was an error getting search results");
-
-		if (loading) setAnnouncement("Loading search results");
-
-		if (results && !failed) {
-			const summary = `Showing ${results.firstResult} to ${results.lastResult} of ${results.resultCount}`;
-			const spellcheck = results.finalSearchText
-				? ` for ${results.finalSearchText}`
-				: null;
-			setAnnouncement(summary + spellcheck);
-		}
-	}, [results, failed, loading]);
-
-	if (failed) return <ErrorPageContent />;
 
 	return (
 		<>
@@ -164,131 +150,118 @@ export function Search({
 						: `Search results`
 				}
 			/>
-
 			<Announcer announcement={announcement} />
-
 			<h1 className="visually-hidden">Search results</h1>
 
-			<Grid gutter="loose" className="pt--e">
-				<GridItem
-					cols={12}
-					md={4}
-					lg={3}
-					elementType="section"
-					aria-label="Filter results"
-				>
-					<SearchListFilters
-						numActiveModifiers={activeModifiers.length}
-						navigators={flattenedNavigators}
-						pageSize={pageSize === searchUrlDefaults.ps ? "" : pageSize}
-						sortOrder={s}
-						queryText={q}
-						from={from}
-						to={to}
-						showDateFilter={false}
-						showTextFilter={false}
-						navigatorsOrder={["nai", "ndt", "gst", "drm", "ngt", "nat"]}
+			{data?.resultCount === 0 && activeModifiers.length === 0 ? (
+				<>
+					<h2 id="results-title">No results found</h2>
+					<SearchNoResults
+						searchText={
+							data?.originalSearch?.searchText || data?.finalSearchText
+						}
 					/>
-				</GridItem>
+				</>
+			) : (
+				<>
+					<Grid gutter="loose" className="pt--e">
+						<GridItem
+							cols={12}
+							md={4}
+							lg={3}
+							elementType="section"
+							aria-label="Filter results"
+						>
+							<SearchListFilters
+								numActiveModifiers={activeModifiers.length}
+								navigators={flattenedNavigators}
+								pageSize={pageSize === searchUrlDefaults.ps ? "" : pageSize}
+								sortOrder={s}
+								queryText={q}
+								from={from}
+								to={to}
+								showDateFilter={false}
+								showTextFilter={false}
+								navigatorsOrder={["nai", "ndt", "gst", "drm", "ngt", "nat"]}
+							/>
+						</GridItem>
 
-				<GridItem
-					cols={12}
-					md={8}
-					lg={9}
-					elementType="section"
-					aria-labelledby="filter-summary"
-				>
-					<FilterSummary
-						id="filter-summary"
-						activeFilters={activeModifiers.map(
-							({ displayName, toggleUrl }) => ({
-								label: displayName,
-								destination: toggleUrl,
-								method: "href",
-								elementType: ({ children, ...props }) => (
-									<Link {...props} scroll={false}>
-										<a>{children}</a>
-									</Link>
-								),
-							})
-						)}
-						sorting={[
-							{
-								active: true,
-								label: s ? "Date" : "Relevance",
-								destination: s
-									? upsertQueryParam(asPath, "s", "Date")
-									: upsertQueryParam(asPath, "s", "Relevance"),
-							},
-							{
-								active: false,
-								label: s ? "Relevance" : "Date",
-								destination: s
-									? removeQueryParam(asPath, "s")
-									: upsertQueryParam(asPath, "s", "Date"),
-								elementType: ({ ...props }) => (
-									<Link {...props} scroll={false} />
-								),
-							},
-						]}
-					>
-						{results?.resultCount === 0 ? (
-							<>
-								<h2 className="mt--0" id="results-title">
-									No results found
-								</h2>
-								<SummaryText {...results} />
-								<SearchNoResults
-									searchText={
-										results?.originalSearch?.searchText ||
-										results?.finalSearchText
+						<GridItem
+							cols={12}
+							md={8}
+							lg={9}
+							elementType="section"
+							aria-labelledby="filter-summary"
+						>
+							<FilterSummary
+								id="filter-summary"
+								activeFilters={activeModifiers.map(
+									({ displayName, toggleUrl }) => ({
+										label: displayName,
+										destination: toggleUrl,
+										method: "href",
+										elementType: ({ children, ...props }) => (
+											<Link {...props} scroll={false}>
+												<a>{children}</a>
+											</Link>
+										),
+									})
+								)}
+								sorting={[
+									{
+										active: true,
+										label: s ? "Date" : "Relevance",
+										destination: s
+											? upsertQueryParam(asPath, "s", "Date")
+											: upsertQueryParam(asPath, "s", "Relevance"),
+									},
+									{
+										active: false,
+										label: s ? "Relevance" : "Date",
+										destination: s
+											? removeQueryParam(asPath, "s")
+											: upsertQueryParam(asPath, "s", "Date"),
+										elementType: ({ ...props }) => (
+											<Link {...props} scroll={false} />
+										),
+									},
+								]}
+							>
+								<div
+									id="search-results-summary"
+									data-original-search-text={
+										data.originalSearch?.searchText || ""
 									}
-								/>
-							</>
-						) : null}
-
-						{/* TODO add loading state */}
-
-						{results && results.resultCount !== 0 ? (
-							<div
-								id="search-results-summary"
-								data-original-search-text={
-									results.originalSearch?.searchText || ""
-								}
-								data-final-search-text={results.finalSearchText}
-								data-result-count={results.resultCount}
-							>
-								<SummaryText {...results} />
-							</div>
-						) : null}
-					</FilterSummary>
-
-					{results && results.resultCount !== 0 && documents.length === 0 ? (
-						<p id="results">
-							We can&apos;t find any results. Try{" "}
-							<Link
-								to={
-									unfilteredResultsUrl?.fullUrl
-										? (unfilteredResultsUrl.fullUrl as string)
-										: "/unfilteredResultsUrl" //TODO fix unfilteredResultsUrl prop as it is undefined
-								}
-								scroll={false}
-							>
-								clearing your filters
-							</Link>{" "}
-							and starting again.
-						</p>
-					) : (
-						<SearchCardList documents={documents} />
-					)}
-					{documents.length !== 0 ? (
-						<SearchPagination
-							results={results}
-							scrollTargetId="filter-summary"
-						/>
-					) : null}
-				</GridItem>
-			</Grid>
+									data-final-search-text={data.finalSearchText}
+									data-result-count={data.resultCount}
+								>
+									<SummaryText {...data} />
+								</div>
+							</FilterSummary>
+							{documents.length === 0 ? (
+								<p id="results">
+									We can&apos;t find any results. Try{" "}
+									<Link
+										to={unfilteredResultsUrl?.fullUrl as string}
+										scroll={false}
+									>
+										clearing your filters
+									</Link>{" "}
+									and starting again.
+								</p>
+							) : (
+								<>
+									<SearchCardList documents={documents} />
+									<SearchPagination
+										results={data}
+										scrollTargetId="filter-summary"
+									/>
+								</>
+							)}
+						</GridItem>
+					</Grid>
+				</>
+			)}
 		</>
 	);
 }
