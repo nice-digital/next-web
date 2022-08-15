8d36772a31d76c0a63c53fe34210ead8ecc46e2e
John Davey <john.davey@nice.org.uk>
2022-08-15 08:50:45 +0100

    NXT-71 Update no-results unit test/snapshots

--- web/src/components/SearchNoResults/SearchNoResults.test.tsx
+++ web/src/components/SearchNoResults/SearchNoResults.test.tsx
@@ -1,24 +1,10 @@
 import { render } from "@testing-library/react";
 
-import { useSiteMetadata } from "@/hooks/useSiteMetadata";
-
 import { SearchNoResults } from "./SearchNoResults";
 
-const useSiteMetadataMock = useSiteMetadata as jest.Mock;
-
 describe("SearchNoResults", () => {
-	it("should match snapshot for BNF site", () => {
-		useSiteMetadataMock.mockImplementationOnce(() => ({ isBNF: true }));
-
-		render(<SearchNoResults searchText="aspirin" />);
-
-		expect(document.body).toMatchSnapshot();
-	});
-
-	it("should match snapshot for BNFC site", () => {
-		useSiteMetadataMock.mockImplementationOnce(() => ({ isBNF: false }));
-
-		render(<SearchNoResults searchText="aspirin" />);
+	it("should match snapshot", () => {
+		render(<SearchNoResults searchText="wxyzyxxxxyxxy" />);
 
 		expect(document.body).toMatchSnapshot();
 	});
