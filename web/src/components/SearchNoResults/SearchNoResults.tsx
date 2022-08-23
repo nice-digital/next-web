9a1173b0693e795beb4d34a13492dc97c6dcc82e
John Davey <john.davey@nice.org.uk>
2022-08-23 15:41:17 +0100

    NXT-71 Update no results content

--- web/src/components/SearchNoResults/SearchNoResults.tsx
+++ web/src/components/SearchNoResults/SearchNoResults.tsx
@@ -29,6 +29,15 @@ export const SearchNoResults: FC<SearchNoResultsProps> = ({ searchText }) => {
 						<a
 							rel="noreferrer"
 							target="_blank"
+							href={`https://cks.nice.org.uk/search/?q=${searchText}&sp=on`}
+						>
+							CKS
+						</a>
+					</li>
+					<li>
+						<a
+							rel="noreferrer"
+							target="_blank"
 							href={`https://bnf.nice.org.uk/search/?q=${searchText}&sp=on`}
 						>
 							BNF
@@ -38,39 +47,26 @@ export const SearchNoResults: FC<SearchNoResultsProps> = ({ searchText }) => {
 						<a
 							rel="noreferrer"
 							target="_blank"
-							href={`https://www.nice.org.uk/search?om=[{%22ndt%22:[%22Guidance%22]}]&ps=15&q=${searchText}&sp=on`}
+							href={`https://bnfc.nice.org.uk/search/?q=${searchText}&sp=on`}
 						>
-							NICE guidance
-						</a>
-					</li>
-					<li>
-						<a
-							rel="noreferrer"
-							target="_blank"
-							href={`https://cks.nice.org.uk/search/?q=${searchText}&sp=on`}
-						>
-							CKS
+							BNFc
 						</a>
 					</li>
 				</ul>
 				<section aria-labelledby="what-are-other-services">
 					<h4 id="what-are-other-services">What are our other services?</h4>
-					<h5>British National Formulary (BNF)</h5>
-					<p>
-						Information on the selection, prescribing, dispensing and
-						administration of drugs.
-					</p>
-					<h5>NICE guidance</h5>
-					<p>
-						We use the best available evidence to develop recommendations that
-						guide decisions in health, public health and social care.
-					</p>
 					<h5>Clinical Knowledge Summaries (CKS)</h5>
 					<p>
 						Concise, accessible summaries of current evidence for primary care
 						professionals. Information is presented as patient scenarios or
 						clinical presentations with answers and links to supporting
 						evidence.
+					</p>
+					<h5>BNF and BNFc</h5>
+					<p>
+						Drug and prescribing information for healthcare professionals. A
+						reference for correct dosage, indication, interaction and side
+						effects of drugs.
 					</p>
 				</section>
 				<section aria-labelledby="check-the-nhs-website">
