292190f1d752cf505e8df818594905ad6818c9c1
John Davey <john.davey@nice.org.uk>
2022-10-20 12:16:36 +0100

    NXT-100 Remove redundant unit tests from PublicationChapterMenu

--- web/src/components/PublicationsChapterMenu/PublicationsChapterMenu.test.tsx
+++ web/src/components/PublicationsChapterMenu/PublicationsChapterMenu.test.tsx
@@ -21,12 +21,6 @@ const defaultProps = {
 describe("PublicationsChapterMenu", () => {
 	beforeEach(() => {
 		(useRouter as jest.Mock).mockImplementation(() => ({}));
-		// (useRouter as jest.Mock).mockImplementation(() => ({
-		// 	route: "/indicators/[slug]",
-		// 	pathname: "/indicators/test001-test-product-title",
-		// 	query: { slug: "/indicators/test001-test-product-title" },
-		// 	asPath: "/indicators/test001-test-product-title",
-		// }));
 	});
 
 	it("should match the snapshot", () => {
@@ -43,37 +37,6 @@ describe("PublicationsChapterMenu", () => {
 		const link = screen.getByRole("link", { name: linkText });
 		expect(link).toBeInTheDocument();
 		expect(link).toHaveAttribute("href", href);
-	});
-
-	it.skip("should render links starting with correct product type path", () => {
-		render(<PublicationsChapterMenu {...defaultProps} />);
-
-		//TODO is this a worthwhile test and can this be done without direct node access
-		//TODO calculation of path now happens outside this component - move this out
-		const href = screen
-			.getByRole("link", { name: "Test Chapter 2" })
-			.closest("a")
-			?.getAttribute("href");
-
-		expect(href && href.startsWith("/indicators/", 0)).toBe(true);
-	});
-
-	it("should convert the url to lowercase", () => {
-		render(
-			<PublicationsChapterMenu
-				{...defaultProps}
-				chapters={[
-					{
-						title: "Test Chapter 1",
-						url: "/ind1001/CHAPTER/TeSt-CHAPTER-1",
-					},
-				]}
-			/>
-		);
-
-		expect(
-			screen.getByRole("link", { name: "Test Chapter 1" })
-		).toHaveAttribute("href", "/ind1001/CHAPTER/TeSt-CHAPTER-1");
 	});
 
 	it("should highlight the link that matches the current route", () => {
