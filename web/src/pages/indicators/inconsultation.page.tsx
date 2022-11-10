f34da28d2ae7e4e2c648bb5c849c52ee83c721df
John Davey <john.davey@nice.org.uk>
2022-11-10 14:09:01 +0000

    NXT-101 Remove lead from pageHeader for inconsultation and indevelopment indicators

--- web/src/pages/indicators/inconsultation.page.tsx
+++ web/src/pages/indicators/inconsultation.page.tsx
@@ -1,9 +1,100 @@
+import Link from "next/link";
 import React from "react";
 
-export default function InConsultationPage(): JSX.Element {
-	return (
-		<>
-			<p>Indicators in consultation</p>
-		</>
-	);
-}
+import { Breadcrumb } from "@nice-digital/nds-breadcrumbs";
+import { Document, SortOrder } from "@nice-digital/search-client";
+
+import { IndicatorListNav } from "@/components/ProductListNav/IndicatorListNav";
+import {
+	getProductListPage,
+	getGetServerSidePropsFunc,
+} from "@/components/ProductListPage/ProductListPage";
+import { ResponsiveDate } from "@/components/ResponsiveDate/ResponsiveDate";
+
+const defaultSortOrder = SortOrder.dateDescending,
+	dateFilterLabel = "Last updated date";
+
+const tableBodyRender = (documents: Document[]) => (
+	<>
+		<caption className="visually-hidden">
+			Published guidance, NICE advice and quality standards
+		</caption>
+		<thead>
+			<tr>
+				<th scope="col">Title</th>
+				<th scope="col">Reference number</th>
+				<th scope="col">Published</th>
+				<th scope="col">Last updated</th>
+			</tr>
+		</thead>
+		<tbody>
+			{documents.map(
+				({
+					id,
+					title,
+					guidanceRef,
+					publicationDate,
+					lastUpdated,
+					pathAndQuery,
+				}) => {
+					return (
+						<tr key={id}>
+							<td>
+								<a
+									href={pathAndQuery}
+									dangerouslySetInnerHTML={{ __html: title }}
+								/>
+							</td>
+							<td>{guidanceRef}</td>
+							<td>
+								<ResponsiveDate isoDateTime={String(publicationDate)} />
+							</td>
+							<td>
+								<ResponsiveDate isoDateTime={String(lastUpdated)} />
+							</td>
+						</tr>
+					);
+				}
+			)}
+		</tbody>
+	</>
+);
+
+export default getProductListPage({
+	metaDescription: "TODO",
+	listNavType: IndicatorListNav,
+	breadcrumbTrail: [
+		<Breadcrumb to="/standards-and-indicators" key="standards-and-indicators">
+			Standards and Indicators
+		</Breadcrumb>,
+		<Breadcrumb
+			to="/standards-and-indicators/indicators"
+			key="/standards-and-indicators/indicators"
+		>
+			Indicators
+		</Breadcrumb>,
+	],
+	currentBreadcrumb: "In consultation",
+	preheading: "In consultation",
+	heading: <>Indicators</>,
+	title: "In development | Indicators | Standards and indicators | NICE",
+	defaultSort: {
+		order: defaultSortOrder,
+		label: "Date",
+	},
+	secondarySort: {
+		order: SortOrder.titleAscending,
+		label: "Title",
+	},
+	showDateFilter: true,
+	useFutureDates: false,
+	dateFilterLabel,
+	tableBodyRender,
+});
+
+export const getServerSideProps = getGetServerSidePropsFunc({
+	gstPreFilter: "In consultation",
+	defaultSortOrder,
+	dateFilterLabel,
+	index: "indicators",
+});
