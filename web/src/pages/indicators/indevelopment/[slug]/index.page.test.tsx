df6fbfd74d327c7bb617a6ff6153227354bd91cd
John Davey <john.davey@nice.org.uk>
2022-12-19 12:07:31 +0000

    NXT-129 Unit test status complete redirect

--- web/src/pages/indicators/indevelopment/[slug]/index.page.test.tsx
+++ web/src/pages/indicators/indevelopment/[slug]/index.page.test.tsx
@@ -3,7 +3,7 @@ import { type GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
 
 import { FeedPath } from "@/feeds/inDev/types";
-import gidng10014 from "@/mockData/inDev/project/gid-ng10014.json";
+import gidng10237 from "@/mockData/inDev/project/gid-ng10237.json";
 import { addDefaultJSONFeedMocks, axiosJSONMock } from "@/test-utils/feeds";
 
 import InDevelopmentPage, {
@@ -11,12 +11,8 @@ import InDevelopmentPage, {
 	InDevelopmentPageProps,
 } from "./index.page";
 
-// type InDevelopmentPageGetServerSidePropsContext = GetServerSidePropsContext<{
-// 	slug: string;
-// }>;
-
 const productRoot = "indicators",
-	slug = "gid-ng10014",
+	slug = "gid-ng10237",
 	resolvedUrl = `/${productRoot}/indevelopment/${slug}`,
 	getServerSidePropsContext = {
 		params: {
@@ -26,8 +22,6 @@ const productRoot = "indicators",
 		query: { productRoot },
 	} as unknown as GetServerSidePropsContext<{ slug: string }>;
 
-console.log("hello ", resolvedUrl);
-
 describe("/indevelopment/[slug].page", () => {
 	beforeEach(() => {
 		(useRouter as jest.Mock).mockImplementation(() => ({
@@ -35,11 +29,11 @@ describe("/indevelopment/[slug].page", () => {
 			asPath: resolvedUrl,
 		}));
 
-		axiosJSONMock.reset();
-		axiosJSONMock
-			.onGet(new RegExp(FeedPath.ProjectDetail))
-			.reply(200, gidng10014);
-		addDefaultJSONFeedMocks();
+		// axiosJSONMock.reset();
+		// axiosJSONMock
+		// 	.onGet(new RegExp(FeedPath.ProjectDetail))
+		// 	.reply(200, gidng10014);
+		// addDefaultJSONFeedMocks();
 	});
 
 	describe("IndevelopmentPage", () => {
@@ -57,14 +51,41 @@ describe("/indevelopment/[slug].page", () => {
 			expect(document.body).toMatchSnapshot();
 		});
 
-		it.skip("should render the page title with reversed breadcrumbs for SEO", async () => {
+		it("should render the page title with reversed breadcrumbs for SEO", async () => {
 			render(<InDevelopmentPage {...props} />);
 			// eslint-disable-next-line testing-library/no-debugging-utils
-			console.log(screen.debug());
+			// console.log(screen.debug());
 			await waitFor(() => {
 				expect(document.title).toEqual(
-					`Project information | ${gidng10014.Title} | Indicators | Standards and Indicators`
+					`Project information | Adrenal insufficiency: acute and long-term management | Indicators | Standards and Indicators`
 				);
+			});
+		});
+	});
+
+	describe("getServerSideProps", () => {
+		it("should return a correct props when supplied with a valid slug", async () => {
+			await expect(
+				getServerSideProps(getServerSidePropsContext)
+			).resolves.toMatchSnapshot();
+		});
+
+		describe("Redirects", () => {
+			it("should return permanent redirect object URL when project status is 'Complete'", async () => {
+				axiosJSONMock.reset();
+				axiosJSONMock
+					.onGet(new RegExp(FeedPath.ProjectDetail))
+					.reply(200, { ...gidng10237, status: "Complete" });
+				addDefaultJSONFeedMocks();
+
+				await expect(
+					getServerSideProps(getServerSidePropsContext)
+				).resolves.toStrictEqual({
+					redirect: {
+						destination: "/indicators/gid-ng10237-",
+						permanent: true,
+					},
+				});
 			});
 		});
 	});
