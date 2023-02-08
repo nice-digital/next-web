51d24797dae6b570b61d17992324ebc0bf6e5cdf
John Davey <john.davey@nice.org.uk>
2023-02-08 11:18:49 +0000

    NXT-130 Render resourcelist from indev resource panels on feed

--- web/src/pages/indicators/indevelopment/[slug]/documents/index.page.tsx
+++ web/src/pages/indicators/indevelopment/[slug]/documents/index.page.tsx
@@ -1,17 +1,28 @@
 import { type GetServerSideProps } from "next/types";
 
+import { ResourceList } from "@/components/ResourceList/ResourceList";
 import { ProjectDetail } from "@/feeds/inDev/types";
+import { arrayify, byTitleAlphabetically } from "@/utils/array";
+import { getFileTypeNameFromMime } from "@/utils/file";
 import { validateRouteParams } from "@/utils/project";
+import {
+	ResourceGroupViewModel,
+	ResourceSubGroupViewModel,
+} from "@/utils/resource";
 
 export type DocumentsPageProps = {
-	project: Pick<ProjectDetail, "reference" | "title">;
+	project: Pick<ProjectDetail, "reference" | "title"> & {
+		groups: ResourceGroupViewModel[];
+	};
 };
 
-export default function DocumentsPage(props: DocumentsPageProps): JSX.Element {
-	console.log({ props });
+export default function DocumentsPage({
+	project,
+}: DocumentsPageProps): JSX.Element {
 	return (
 		<>
-			<p>Documents page placeholder content</p>
+			<p>--- TODO Documents page title and project heading ---</p>
+			<ResourceList title="Documents" groups={project.groups} />
 		</>
 	);
 }
@@ -26,18 +37,60 @@ export const getServerSideProps: GetServerSideProps<
 
 	const { project, panels, hasPanels } = result;
 
-	console.log(params);
-	console.log({ project });
-	console.log({ panels });
-	console.log({ hasPanels });
-
 	if (!project) return { notFound: true };
+
+	const groups = panels.sort(byTitleAlphabetically).map((panel) => {
+		const indevResource =
+			panel.embedded.niceIndevResourceList.embedded.niceIndevResource;
+
+		const indevResources = arrayify(indevResource);
+
+		const subGroups: ResourceSubGroupViewModel[] = [];
+
+		let currentSubGroup: ResourceSubGroupViewModel;
+
+		indevResources.forEach((resource) => {
+			if (resource.textOnly) {
+				currentSubGroup = { title: resource.title, resourceLinks: [] };
+				subGroups.push(currentSubGroup);
+			} else {
+				if (!currentSubGroup) {
+					currentSubGroup = { title: panel.title, resourceLinks: [] };
+					subGroups.push(currentSubGroup);
+				}
+
+				const { mimeType, length, resourceTitleId, fileName } =
+						resource.embedded.niceIndevFile,
+					isHTML = mimeType === "text/html",
+					fileSize = isHTML ? null : length,
+					fileTypeName = isHTML ? null : getFileTypeNameFromMime(mimeType),
+					href = isHTML ? `/html-todo` : `/download-todo`;
+
+				currentSubGroup.resourceLinks.push({
+					title: resource.title,
+					href,
+					fileTypeName,
+					fileSize,
+					date: resource.publishedDate,
+					type: panel.title,
+				});
+			}
+		});
+
+		return {
+			title: panel.title,
+			subGroups,
+		};
+	});
+
+	console.log({ groups });
 
 	return {
 		props: {
 			project: {
 				reference: project.reference,
 				title: project.title,
+				groups,
 			},
 		},
 	};
