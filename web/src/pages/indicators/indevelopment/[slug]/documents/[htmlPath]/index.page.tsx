ba58ff8eadc8ed16146f762878014ee35bcc9fdd
John Davey <john.davey@nice.org.uk>
2023-02-15 09:45:43 +0000

    NXT-130 Add project props, project heading component and breadcrumbs for html document page

--- web/src/pages/indicators/indevelopment/[slug]/documents/[htmlPath]/index.page.tsx
+++ web/src/pages/indicators/indevelopment/[slug]/documents/[htmlPath]/index.page.tsx
@@ -1,17 +1,64 @@
+import { NextSeo } from "next-seo";
 import { type GetServerSideProps } from "next/types";
 import React from "react";
 
+import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
+
+import { ProjectPageHeading } from "@/components/ProjectPageHeading/ProjectPageHeading";
+import { IndevSchedule, ProjectDetail } from "@/feeds/inDev/types";
+import { arrayify } from "@/utils/array";
 import { validateRouteParams } from "@/utils/project";
 
 export type DocumentHTMLPageProps = {
-	someProp: string;
+	indevScheduleItems?: IndevSchedule[];
+	indevStakeholderRegistration: Record<string, unknown>[];
+	project: Pick<
+		ProjectDetail,
+		"projectType" | "reference" | "title" | "status"
+	>;
 };
 
-export default function HistoryHTMLPage({
-	someProp,
-}: DocumentHTMLPageProps): JSX.Element {
+export default function HistoryHTMLPage(
+	props: DocumentHTMLPageProps
+): JSX.Element {
 	return (
 		<>
+			<NextSeo
+				title={`TODO-HTMLpagetitle | Project documents | ${props.project.title} | Indicators | Standards and Indicators`}
+			/>
+
+			<Breadcrumbs>
+				<Breadcrumb to="/">Home</Breadcrumb>
+				<Breadcrumb to="/standards-and-indicators">
+					Standards and Indicators
+				</Breadcrumb>
+				<Breadcrumb to="/standards-and-indicators/indicators">
+					Indicators
+				</Breadcrumb>
+				<Breadcrumb to="/standards-and-indicators/indicators/indevelopment">
+					In development
+				</Breadcrumb>
+				<Breadcrumb
+					to={`/standards-and-indicators/indicators/indevelopment/${props.project.reference}`}
+				>
+					{props.project.reference}
+				</Breadcrumb>
+				<Breadcrumb
+					to={`/standards-and-indicators/indicators/indevelopment/${props.project.reference}/documents/`}
+				>
+					Project documents
+				</Breadcrumb>
+				<Breadcrumb>TODO-htmldoc title</Breadcrumb>
+			</Breadcrumbs>
+
+			<ProjectPageHeading
+				projectType={props.project.projectType}
+				reference={props.project.reference}
+				title={props.project.title}
+				status={props.project.status}
+				indevScheduleItems={props.indevScheduleItems}
+				indevStakeholderRegistration={props.indevStakeholderRegistration}
+			/>
 			<p>html page content here</p>
 		</>
 	);
@@ -25,11 +72,30 @@ export const getServerSideProps: GetServerSideProps<
 
 	if ("notFound" in result || "redirect" in result) return result;
 
-	const someProp = "TODO";
+	const { project } = result;
+
+	if (!project) return { notFound: true };
+
+	const { projectType, reference, status, title } = project;
+
+	const indevSchedule =
+			project.embedded.niceIndevProvisionalScheduleList?.embedded
+				.niceIndevProvisionalSchedule,
+		indevScheduleItems = arrayify(indevSchedule),
+		indevStakeholderRegistration = arrayify(
+			project.links.niceIndevStakeholderRegistration
+		);
 
 	return {
 		props: {
-			someProp,
+			indevScheduleItems,
+			indevStakeholderRegistration,
+			project: {
+				projectType,
+				reference,
+				status,
+				title,
+			},
 		},
 	};
 };
