1af73ddd9b7a3c76a264100a3307d096bcfddeab
Ian Routledge <ian.routledge@nice.org.uk>
2023-04-19 20:58:29 +0100

    NXT-154 Add initial mock publications

--- functional-tests/support/pagePaths.ts
+++ functional-tests/support/pagePaths.ts
@@ -5,6 +5,8 @@ export const pagePaths = {
 	"awaiting development guidance list": "/guidance/awaiting-development",
 	status: "/status",
 	"published indicators list": "/indicators/published",
+	"IND63 overview":
+		"/indicators/ind63-pregnancy-and-neonates-mental-health-at-booking-appointment",
 } as const;
 
 export type PageName = keyof typeof pagePaths;
