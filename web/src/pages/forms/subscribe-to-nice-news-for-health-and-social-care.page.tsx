1e1e8585e5615bce78db2944a343f9ef985384f9
John Davey <john.davey@nice.org.uk>
2023-09-11 15:58:51 +0100

    NXT-191 Update page title

---
+++ web/src/pages/forms/subscribe-to-nice-news-for-health-and-social-care.page.tsx
@@ -0,0 +1,29 @@
+import {
+	type FormProps,
+	getGetServerSideProps,
+} from "@/components/JotFormPage/getGetServerSideProps";
+import { JotFormPage } from "@/components/JotFormPage/JotFormPage";
+
+export const getServerSideProps = getGetServerSideProps("232422293687865");
+
+export default function HealthAndSocialCareNewsletterForm(
+	props: FormProps
+): JSX.Element {
+	return (
+		<JotFormPage
+			{...props}
+			formName="Subscribe to NICE news for health and social care"
+			lead="Keeping you up-to-date with important developments."
+			parentPages={[
+				{
+					title: "Newsletters and alerts",
+					path: "/home/news/newsletters-and-alerts",
+				},
+				{
+					title: "News",
+					path: "/home/news",
+				},
+			]}
+		/>
+	);
+}
