b035156642c957df4d7acda72086e3d50658c6f7
John Davey <66066685+johndavey72@users.noreply.github.com>
2023-10-09 11:29:58 +0100

    NXT-195 Add international newsletter form and test (#143)

---
+++ web/src/pages/forms/subscribe-to-nice-news-international.page.test.tsx
@@ -0,0 +1,46 @@
+import { render } from "@testing-library/react";
+import { GetServerSidePropsContext } from "next";
+
+import { FormProps } from "@/components/JotFormPage/getGetServerSideProps";
+
+import NewsInternationalNewsletterForm, {
+	getServerSideProps,
+} from "./subscribe-to-nice-news-international.page";
+
+jest.mock("@/feeds/jotform/jotform", () => ({
+	getForm: jest.fn().mockResolvedValue({
+		responseCode: 200,
+		message: "success",
+		content: {
+			id: "232421981351856",
+			username: "nice_teams",
+			title: "Subscribe to NICE news international",
+			height: "539",
+			status: "ENABLED",
+			created_at: "2022-12-08 06:31:44",
+			updated_at: "2023-03-27 05:03:55",
+			last_submission: "2023-03-24 14:43:43",
+			new: "34",
+			count: "46",
+			type: "LEGACY",
+			favorite: "0",
+			archived: "0",
+			url: "https://nice.jotform.com/232421981351856",
+		},
+		duration: "14.98ms",
+	}),
+}));
+
+describe("NewsInternationalNewsletterForm", () => {
+	it("should match snapshot", async () => {
+		const props = (await getServerSideProps({
+			resolvedUrl: "/forms/subscribe-to-nice-news-international",
+		} as GetServerSidePropsContext)) as { props: FormProps };
+
+		const { container } = render(
+			<NewsInternationalNewsletterForm {...props.props} />
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+});
