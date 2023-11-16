6779a0d578776b4e8d988ffb9bdd9f0f490567b5
Chris Barker <chris.barker@nice.org.uk>
2023-11-16 09:55:31 +0000

    NXT-197 Add PM2 resurrect script

--- web-host/pm2.resurrect.bat
+++ web-host/pm2.resurrect.bat
@@ -4,4 +4,5 @@ rem Make sure we're in the correct path as per https://stackoverflow.com/a/46089
 PUSHD %~dp0
 
 rem Resurrect the PM2 process after a restart etc. Used by startup scheduled task.
+powershell -nop -c "& {sleep 10}"
 npm run resurrect
