da0ba7aba5ae041c72a928da6d6df5896bb4814b
John Davey <john.davey@nice.org.uk>
2023-12-20 12:45:13 +0000

    tests running and passing

--- functional-tests/steps/then.ts
+++ functional-tests/steps/then.ts
@@ -1,3 +1,7 @@
-import { Then } from "@cucumber/cucumber";
-
-// TODO
+import { Then } from "@wdio/cucumber-framework";
+ 
+Then('I expect that element {string} contains the text {string}', async (selector, expectedText) => {
+    const element = await browser.$(selector);
+    const actualText = await element.getText();
+    expect(actualText).toContain(expectedText);
+});
