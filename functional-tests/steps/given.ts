204c3a29b6e4f0d365f68178a55add5e78248338
John Davey <66066685+johndavey72@users.noreply.github.com>
2024-01-02 14:09:34 +0000

    NXT 221 Fix functional tests 2 (#168)
    * NXT-208 Add life sciences contact us form and test
    
    * NXT-208 Attempt to fix 'unexpected token export' error in functional tests
    
    * NXT-208 change tsconfig target to try to fix wdio conf issue
    
    * NXT-208 Revert wdio debugging changes
    
    * container comms working
    
    * tests running with failures
    
    * add typescript types to web project
    
    * tests running and passing
    
    * update tsconfig as it is erroring in the container due to missing extends config file
    
    * update container name for test-runner in docker compose and shell scripts. Change import syntax to satisfy typescript
    
    * NXT-221 Linting fixes. Temp remove nice cucumber steps from settings
    
    * NXT-221 Remove redundant files and wdio config lines
    
    * NXT-221 Clean up package-lock
    
    * NXT-221 Update vscode settings
    
    * NXT-221 Add root tsconfig properties to web tsconfig to avoid issues with extended tsconfig in docker container next build
    
    * NXT-221 Undo some inadvertent changes to vscode settings
    
    * NXT-221 Update import path for when step

--- functional-tests/steps/given.ts
+++ functional-tests/steps/given.ts
@@ -1,9 +1,41 @@
-import { Given } from "@cucumber/cucumber";
+import { Given } from "@wdio/cucumber-framework";
 
-import { acceptCookieBanner } from "@nice-digital/wdio-cucumber-steps/lib/support/action/acceptCookieBanner";
-import { openWebsite } from "@nice-digital/wdio-cucumber-steps/lib/support/action/openWebsite";
+import { getPath, PageName } from "../support/pagePaths.js";
 
-import { getPath, PageName } from "../support/pagePaths";
+/**
+ * Accept all cookies using the NICE cookie banner.
+ * We use this as the cookie banner blocks clicks.
+ */
+export async function acceptCookieBanner(): Promise<void> {
+	// The cookie banner (and license key) load async from the CDN
+	// so we need to wait for it to load
+	const cookieBannerElement = await $("body #ccc");
+	await cookieBannerElement.waitForExist({ timeout: 2000 });
+
+	const acceptCookiesButtonElement = await cookieBannerElement.$(
+		"button.ccc-accept-button"
+	);
+
+	// If cookies have already been chosen then the accept button doesn't show
+	if (await acceptCookiesButtonElement.isDisplayed()) {
+		await acceptCookiesButtonElement.click();
+	}
+}
+
+/*! https://github.com/webdriverio/cucumber-boilerplate/blob/master/src/support/action/openWebsite.js */
+/**
+ * Open the given URL
+ * @param  {String}   type Type of navigation (url or site)
+ * @param  {String}   page The URL to navigate to
+ */
+export async function openWebsite(
+	type: "url" | "site",
+	page: string
+): Promise<void> {
+	const url = type === "url" ? page : browser.options.baseUrl + page;
+
+	await browser.url(url);
+}
 
 Given(/^I open the (.*) page$/, async (pageName: PageName) => {
 	await openWebsite("url", getPath(pageName));
