aa41d7c4d02485bb5ee6eb0ff2a0a07c350e6da1
John Davey <66066685+johndavey72@users.noreply.github.com>
2024-01-08 11:00:43 +0000

    NXT 205 Mailchimp integration jotforms (#150)
    * NXT-191 Newsletter subscription nice news for health social care (#140)
    
    * NXT-191 Health and social care newsletter form and test
    
    * NXT-191 Prettier fix
    
    * NXT-191 Update snapshot with title fix
    
    * NXT-191 Update test describe block
    
    * NXT-191 Update page title
    
    * NXT-191 Update snapshot
    
    * NXT-191 Remove mis-named pages and old snapshots
    
    * NXT-191 Update breadcrumbs
    
    * NXT-192 Newsletter subscription nice news for life sciences (#141)
    
    * NXT-192 Add life science newsletter sub form and test - wip
    
    * NXT-192 Update snapshot
    
    * NXT-192 Update page name to match orchard equivalent
    
    * NXT-192 Update snapshot, remove old snapshot
    
    * NXT-192 Update breadcrumbs
    
    * NXT-193 Add primary care form and test (#142)
    
    * NXT-193 Add primary care form and test
    
    * NXT-193 Update breadcrumbs
    
    * NXT-194 Add medicines and prescribing form and test (#147)
    
    * NXT-194 Add medicines and prescribing form and test
    
    * NXT-194 Correct jotform id
    
    * NXT-195 Add international newsletter form and test (#143)
    
    * NXT-196 Add medicines and prescribing alerts form and test (#149)

---
+++ web/src/pages/forms/subscribe-to-nice-news-for-life-sciences.page.tsx
@@ -0,0 +1,29 @@
+import {
+	type FormProps,
+	getGetServerSideProps,
+} from "@/components/JotFormPage/getGetServerSideProps";
+import { JotFormPage } from "@/components/JotFormPage/JotFormPage";
+
+export const getServerSideProps = getGetServerSideProps("232422198305856");
+
+export default function LifeSciencesNewsletterForm(
+	props: FormProps
+): JSX.Element {
+	return (
+		<JotFormPage
+			{...props}
+			formName="Subscribe to NICE news for life sciences"
+			lead="Keeping those working within the life sciences sector up-to-date with important developments."
+			parentPages={[
+				{
+					title: "Newsletters and alerts",
+					path: "/news/newsletters-and-alerts",
+				},
+				{
+					title: "News",
+					path: "/news",
+				},
+			]}
+		/>
+	);
+}
