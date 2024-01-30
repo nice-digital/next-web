4c70ee3750f926c924887efa0d466f555fed58a2
Dale H <dale.hudson@nice.org.uk>
2024-01-30 16:34:11 +0000

    fix: revert StoryblokPageHeader component for stories that use the StoryblokPageHeader blok

--- web/src/components/Storyblok/StoryblokPageHeader/StoryblokPageHeader.tsx
+++ web/src/components/Storyblok/StoryblokPageHeader/StoryblokPageHeader.tsx
@@ -1,71 +1,37 @@
 import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
-import { Grid, GridItem } from "@nice-digital/nds-grid";
 import { Hero } from "@nice-digital/nds-hero";
-import { PageHeader, PageHeaderProps } from "@nice-digital/nds-page-header";
-import { Tag } from "@nice-digital/nds-tag";
 
 import { type Breadcrumb as TypeBreadcrumb } from "@/types/Breadcrumb";
 import { type PageHeaderStoryblok } from "@/types/storyblok";
 import { resolveStoryblokLink } from "@/utils/storyblok";
 
-import styles from "./StoryblokPageHeader.module.scss";
-
 interface PageHeaderBlokProps {
 	blok: PageHeaderStoryblok;
-	breadcrumbs?: React.ReactNode;
-}
-
-function formatDate(date: string): string {
-	return new Intl.DateTimeFormat("en-GB", {
-		year: "numeric",
-		month: "long",
-		day: "numeric",
-	}).format(new Date(date));
+	breadcrumbs?: TypeBreadcrumb[];
 }
 
 export const StoryblokPageHeader = ({
 	blok,
 	breadcrumbs,
 }: PageHeaderBlokProps): React.ReactElement => {
-	// const BreadcrumbComponent = breadcrumbs?.length ? (
-	// 	<Breadcrumbs>
-	// 		{[{ title: "Home", path: "/" }, ...breadcrumbs].map((breadcrumb) => (
-	// 			<Breadcrumb key={breadcrumb.title} to={breadcrumb.path}>
-	// 				{breadcrumb.title}
-	// 			</Breadcrumb>
-	// 		))}
-	// 	</Breadcrumbs>
-	// ) : undefined;
+	const BreadcrumbComponent = breadcrumbs?.length ? (
+		<Breadcrumbs>
+			{[{ title: "Home", path: "/" }, ...breadcrumbs].map((breadcrumb) => (
+				<Breadcrumb key={breadcrumb.title} to={breadcrumb.path}>
+					{breadcrumb.title}
+				</Breadcrumb>
+			))}
+		</Breadcrumbs>
+	) : undefined;
 
-	const Footer = () => {
-		const pageType =
-			blok.pageType === "newsArticle"
-				? "News"
-				: blok.pageType === "blogPost"
-				? "Blog"
-				: null;
-		return (
-			<div className={styles.footerMeta}>
-				{pageType && <Tag outline>{pageType}</Tag>}
-
-				{blok.date && <time dateTime={blok.date}>{formatDate(blok.date)}</time>}
-			</div>
-		);
-	};
+	console.log("Page header blok:", blok);
 
 	return (
 		<>
-			<PageHeader
-				isFullWidth={true}
-				heading={blok.title}
-				lead={blok.summary || undefined}
-				breadcrumbs={breadcrumbs}
-			/>
-			{/* <Hero
+			<Hero
 				title={blok.title}
 				intro={blok.summary || undefined}
 				header={BreadcrumbComponent}
-				footer={<Footer />}
 			/>
 			{blok.description && (
 				<p>
@@ -80,7 +46,7 @@ export const StoryblokPageHeader = ({
 						new page header
 					</a>
 				</p>
-			)} */}
+			)}
 		</>
 	);
 };
