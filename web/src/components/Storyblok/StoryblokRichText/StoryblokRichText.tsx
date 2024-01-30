b37788b5eb233cb4467c7741b4389869c0800478
Dale H <dale.hudson@nice.org.uk>
2024-01-30 17:09:49 +0000

    styles: update style to handle images contained within richtext content

--- web/src/components/Storyblok/StoryblokRichText/StoryblokRichText.tsx
+++ web/src/components/Storyblok/StoryblokRichText/StoryblokRichText.tsx
@@ -4,6 +4,8 @@ import { render } from "storyblok-rich-text-react-renderer";
 
 import { RichtextStoryblok } from "@/types/storyblok";
 
+import styles from "./StoryblokRichText.module.scss";
+
 export interface StoryblokRichTextProps {
 	content: RichtextStoryblok;
 }
@@ -12,13 +14,13 @@ export const StoryblokRichText: React.FC<StoryblokRichTextProps> = ({
 	content,
 }) => {
 	return (
-		<>
+		<div className={styles.storyblokRichTextWrapper}>
 			{render(content, {
 				defaultBlokResolver: (name, props) => {
 					const blok = { ...props, component: name };
 					return <StoryblokComponent blok={blok} key={blok._uid} />;
 				},
 			})}
-		</>
+		</div>
 	);
 };
