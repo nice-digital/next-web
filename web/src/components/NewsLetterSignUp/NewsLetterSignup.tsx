4fe57982cfd25eade6e3517a577d2e1363849b1a
Dale H <dale.hudson@nice.org.uk>
2024-02-02 11:43:51 +0000

    styles: update cta button so inner text wraps

--- web/src/components/NewsLetterSignUp/NewsLetterSignup.tsx
+++ web/src/components/NewsLetterSignUp/NewsLetterSignup.tsx
@@ -3,10 +3,14 @@ import React from "react";
 import { ActionBanner } from "@nice-digital/nds-action-banner";
 import { Button } from "@nice-digital/nds-button";
 
+import styles from "./NewsLetterSignUp.module.scss";
+
 export const NewsLetterSignup = (): React.ReactElement => {
 	const ctaButton = (
 		<Button
+			elementType="a"
 			variant="cta"
+			className={styles["signup-cta"]}
 			href="https://www.nice.org.uk/news/nice-newsletters-and-alerts"
 		>
 			Sign up for newsletters and alerts
