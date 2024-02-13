d2827811e6db4f3c2ba321563670784fecb90404
Dale H <dale.hudson@nice.org.uk>
2024-02-13 13:46:22 +0000

    styles: round image for author image

--- web/src/components/Storyblok/StoryblokAuthor/StoryblokAuthor.tsx
+++ web/src/components/Storyblok/StoryblokAuthor/StoryblokAuthor.tsx
@@ -4,6 +4,8 @@ import { AuthorStoryblok } from "@/types/storyblok";
 
 import { StoryblokImage } from "../StoryblokImage/StoryblokImage";
 
+import styles from "./StoryblokAuthor.module.scss";
+
 export const StoryblokAuthor = ({
 	blok,
 }: AuthorStoryblok): React.ReactElement | null => {
@@ -17,10 +19,9 @@ export const StoryblokAuthor = ({
 				<StoryblokImage
 					src={blok.image.filename}
 					alt={blok.image.alt}
-					serviceOptions="smart/"
-					width={138}
-					height={138}
-					className="author-image"
+					width={136}
+					height={136}
+					className={styles["author-image"]}
 				/>
 			)}
 			{blok.name && <h4>{blok.name}</h4>}
