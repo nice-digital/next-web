53312cc3fa12a10069e28ab236fa35730fe13bb1
John Davey <john.davey@nice.org.uk>
2024-02-19 15:33:39 +0000

    NICE-178 Add current page and pagination destinations

--- web/src/pages/news/articles/index.page.tsx
+++ web/src/pages/news/articles/index.page.tsx
@@ -1,8 +1,10 @@
 import { type ISbStories, type ISbStory } from "@storyblok/react";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 import { SimplePagination } from "@nice-digital/nds-simple-pagination";
 
+import { Link } from "@/components/Link/Link";
 import { logger } from "@/logger";
 import { fetchStories, getStoryVersionFromQuery } from "@/utils/storyblok";
 
@@ -11,13 +13,16 @@ import type { GetServerSidePropsContext } from "next";
 type NewsArticlesProps = {
 	stories: ISbStories[];
 	totalPages: number;
+	currentPage: number;
 };
 
 export const ArticlesIndexPage = ({
 	stories,
 	totalPages,
+	currentPage,
 }: NewsArticlesProps): React.ReactElement => {
-	console.log({ stories });
+	const router = useRouter();
+
 	return (
 		<>
 			<h1>Articles Index Page</h1>
@@ -27,9 +32,15 @@ export const ArticlesIndexPage = ({
 
 			<SimplePagination
 				totalPages={totalPages}
-				currentPage={2}
-				nextPageLink={{ destination: "#", elementType: "a" }}
-				previousPageLink={{ destination: "#", elementType: "a" }}
+				currentPage={currentPage}
+				nextPageLink={{
+					destination: `${router.pathname}?page=${currentPage + 1}`,
+					elementType: Link,
+				}}
+				previousPageLink={{
+					destination: `${router.pathname}?page=${currentPage - 1}`,
+					elementType: Link,
+				}}
 			/>
 		</>
 	);
@@ -39,16 +50,20 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 	logger.warn("Start server side props for /news/articles list page");
 
 	const version = getStoryVersionFromQuery(context.query);
+	const page = context.query.page
+		? parseInt(context.query.page as string, 10)
+		: 1;
 
 	const storiesResult = await fetchStories(version, {
 		starts_with: "news/articles/",
 		per_page: 5,
+		page,
 	});
 
-	console.log("****************** Stories result:", storiesResult);
+	// console.log("****************** Stories result:", storiesResult);
 
 	//TODO handle empty list
-	if (storiesResult.stories.length == 0) return { notFound: true };
+	// if (storiesResult.stories.length == 0) return { notFound: true };
 
 	logger.warn("Finish server side props for news articles list page");
 
@@ -56,6 +71,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 		props: {
 			stories: [...storiesResult.stories],
 			totalPages: storiesResult.total,
+			currentPage: page,
 		},
 	};
 	return result;
