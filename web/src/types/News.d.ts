2df6b88dfa9968fbf79dffa33a52760717b86351
Chris Barker <chris@christopherbarker.co.uk>
2024-02-26 15:06:32 +0000

    NXT-250 Refactor fetchstories utility function (#179)
    Co-authored-by: Chris Barker <chris.barker@nice.org.uk>

--- web/src/types/News.d.ts
+++ web/src/types/News.d.ts
@@ -1,6 +1,3 @@
-import { type StoryblokStory } from "storyblok-generate-ts";
 import { type BlogPostStoryblok, type NewsArticleStoryblok } from "./storyblok";
 
-export type NewsStory =
-	| StoryblokStory<BlogPostStoryblok>
-	| StoryblokStory<NewsArticleStoryblok>;
+export type NewsStory = BlogPostStoryblok | NewsArticleStoryblok;
