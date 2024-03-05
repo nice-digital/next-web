b85eb3435b5401248fd3744d043db756060c07d7
Dale H <dale.hudson@nice.org.uk>
2024-03-05 15:52:17 +0000

    add classname to nestedrichtext component, allowing classname prop to be passed by parent components

--- web/src/components/Storyblok/NestedRichText/NestedRichText.tsx
+++ web/src/components/Storyblok/NestedRichText/NestedRichText.tsx
@@ -1,16 +1,23 @@
 import { renderRichText } from "@storyblok/react";
+import classnames from "classnames";
 
 import { type NestedRichTextStoryblok } from "@/types/storyblok";
 
+import styles from "./NestedRichText.module.scss";
+
 interface NestedRichTextBlokProps {
 	blok: NestedRichTextStoryblok;
+	className?: string;
 }
 
 export const NestedRichText = ({
 	blok,
+	className,
 }: NestedRichTextBlokProps): React.ReactElement => {
+	const nestedRichTextClassNames = classnames(styles.nestedRichText, className);
 	return (
 		<div
+			className={nestedRichTextClassNames}
 			dangerouslySetInnerHTML={{ __html: renderRichText(blok.richText) }}
 		></div>
 	);
