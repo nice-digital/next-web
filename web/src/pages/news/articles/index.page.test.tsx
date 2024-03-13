1d28a108b3d9d4ad3d93a43e14f17def76559b8a
John Davey <john.davey@nice.org.uk>
2024-03-13 10:02:53 +0000

    NICE-215 unit test featured story prop in gssp

--- web/src/pages/news/articles/index.page.test.tsx
+++ web/src/pages/news/articles/index.page.test.tsx
@@ -1,21 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ParsedUrlQuery } from "querystring";
 
-import { render, screen } from "@testing-library/react";
 import { GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
-import { StoryblokStory } from "storyblok-generate-ts";
 
 import { mockNewsArticle as mockStory } from "@/test-utils/storyblok-data";
-import { NewsStory } from "@/types/News";
 
-import { ArticlesIndexPage, NewsArticlesProps } from "./index.page";
+const generateStory = (
+	id: number,
+	name: string,
+	date: string,
+	component = "newsArticle"
+) => ({
+	...mockStory,
+	name,
+	id,
+	content: {
+		...mockStory.content,
+		date,
+		component,
+	},
+});
 
-jest.mock("@/utils/storyblok", () => ({
-	...jest.requireActual("@/utils/storyblok"),
-	initStoryblok: jest.fn(),
+const mockStories = Array.from({ length: 8 }, (_, i) =>
+	generateStory(i + 1, `Test story ${i + 1}`, `2023-0${i + 1}-10`)
+);
+
+const mockFeaturedStory = generateStory(0, "Featured story", "2024-01-10");
+
+const mockNullFeaturedStoryResponse = {
+	featuredStory: null,
+	stories: [],
+	total: 0,
+	currentPage: 2,
+	perPage: undefined,
+};
+const mockSuccessResponse = {
+	featuredStory: mockFeaturedStory,
+	stories: mockStories,
+	total: 8,
+	currentPage: 2,
+	perPage: 4,
+};
+
+jest.mock("@storyblok/react", () => ({
+	getStoryblokApi: jest.fn(),
 }));
 
-xdescribe("getServerSideProps", () => {
+describe("getServerSideProps", () => {
 	beforeEach(() => {
 		(useRouter as jest.Mock).mockReturnValue({
 			route: "/news/articles",
@@ -32,11 +64,36 @@ xdescribe("getServerSideProps", () => {
 	});
 
 	it("should set featuredStory to null if the page is greater than 1", async () => {
+		jest.mock("@/utils/storyblok", () => ({
+			validateRouteParams: jest.fn(() => {
+				return Promise.resolve(mockNullFeaturedStoryResponse);
+			}),
+		}));
+
 		const { getServerSideProps } = await import("./index.page");
 		const result = await getServerSideProps({
 			query: { page: "2" },
 		} as unknown as GetServerSidePropsContext<ParsedUrlQuery>);
 
-		expect(result.props?.featuredStory).toBeNull();
+		expect(
+			result && "props" in result && result.props.featuredStory
+		).toBeNull();
+	});
+
+	it("should set featuredStory to mockFeaturedStory if the page is 1", async () => {
+		jest.mock("@/utils/storyblok", () => ({
+			validateRouteParams: jest.fn(() => {
+				return Promise.resolve(mockSuccessResponse);
+			}),
+		}));
+
+		const { getServerSideProps } = await import("./index.page");
+		const result = await getServerSideProps({
+			query: { page: "1" },
+		} as unknown as GetServerSidePropsContext<ParsedUrlQuery>);
+
+		expect(result && "props" in result && result.props.featuredStory).toEqual(
+			mockFeaturedStory
+		);
 	});
 });
