36d42d3a15c116dc3590800c6eff717dfe130a8c
Dale H <dale.hudson@nice.org.uk>
2024-03-14 18:23:28 +0000

    NICE-215: update author card with iscardauthor and headinglevel props
    - update author to handle different heading levels with default to h4
    - add prop to handle author when in card, updated styles for this too

--- web/src/components/Storyblok/StoryblokAuthor/StoryblokAuthor.tsx
+++ web/src/components/Storyblok/StoryblokAuthor/StoryblokAuthor.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import React from "react";
 
 import { AuthorStoryblok } from "@/types/storyblok";
@@ -6,21 +7,33 @@ import { StoryblokImage } from "../StoryblokImage/StoryblokImage";
 
 import styles from "./StoryblokAuthor.module.scss";
 
+export type AuthorBlockVariant = "cardAuthor";
 export interface StoryblokAuthorProps {
 	blok: AuthorStoryblok;
+	isCardAuthor?: boolean;
 	className?: string;
+	headingLevel?: number;
 }
 
 export const StoryblokAuthor = ({
 	blok,
+	isCardAuthor,
+	className,
+	headingLevel = 4,
 }: StoryblokAuthorProps): React.ReactElement | null => {
 	if (!blok) {
 		return null;
 	}
 
+	const authorClasses = classNames(styles.author, className, {
+		[styles["author--is-card-author"]]: isCardAuthor,
+	});
+
+	const HeadingLevelElement = `h${headingLevel}` as keyof JSX.IntrinsicElements;
+
 	return (
 		<div
-			className={styles.author}
+			className={authorClasses}
 			data-testid={`author-component-${blok._uid}`}
 		>
 			{blok.image && (
@@ -29,13 +42,20 @@ export const StoryblokAuthor = ({
 					alt={blok.image.alt}
 					width={136}
 					height={136}
-					className={styles["author__image"]}
+					className={styles.author__image}
+					serviceOptions="/270x270/smart/filters:quality(80)"
 				/>
 			)}
-			{blok.name && <h4 className={styles["author__name"]}>{blok.name}</h4>}
-			{blok.jobTitle && (
-				<p className={styles["author__job-title"]}>{blok.jobTitle}</p>
-			)}
+			<div className={styles.author__content}>
+				{blok.name && (
+					<HeadingLevelElement className={styles.author__name}>
+						{blok.name}
+					</HeadingLevelElement>
+				)}
+				{blok.jobTitle && (
+					<p className={styles["author__job-title"]}>{blok.jobTitle}</p>
+				)}
+			</div>
 		</div>
 	);
 };
