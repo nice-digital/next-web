712662b20e67c15db8e7ff7d9295bbb02493fb91
Chris Barker <chris.barker@nice.org.uk>
2024-03-18 17:21:01 +0000

    NXT-270 News homepage WIP

--- web/src/pages/news/index.page.tsx
+++ web/src/pages/news/index.page.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import { ISbStoriesParams } from "@storyblok/react";
+import { GetServerSidePropsContext } from "next";
+import { NextSeo } from "next-seo";
+import { StoryblokStory } from "storyblok-generate-ts";
 
+import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
+import { PageHeader } from "@nice-digital/nds-page-header";
+
+import { Link } from "@/components/Link/Link";
+import { FeaturedStory } from "@/components/Storyblok/News/FeaturedStory/FeaturedStory";
+import { NewsGrid } from "@/components/Storyblok/News/NewsGrid/NewsGrid";
 import { NewsListNav } from "@/components/Storyblok/News/NewsListNav/NewsListNav";
+import {
+	BlogPostStoryblok,
+	InDepthArticleStoryblok,
+	NewsArticleStoryblok,
+	PodcastStoryblok,
+} from "@/types/storyblok";
+import { getStoryVersionFromQuery, fetchStories } from "@/utils/storyblok";
 
-export const NewsIndexPage = (): React.ReactElement => {
+interface NewsIndexProps {
+	newsArticles: StoryblokStory<NewsArticleStoryblok>[];
+	inDepthArticles: StoryblokStory<InDepthArticleStoryblok>[];
+	blogPosts: StoryblokStory<BlogPostStoryblok>[];
+	podcasts: StoryblokStory<PodcastStoryblok>[];
+}
+
+export default function NewsIndexPage({
+	newsArticles,
+	inDepthArticles,
+	blogPosts,
+	podcasts,
+}: NewsIndexProps): React.ReactElement {
 	const destinations = [
 		{ url: "/news", title: "News" },
 		{ url: "/news/articles", title: "News articles" },
@@ -10,12 +38,94 @@ export const NewsIndexPage = (): React.ReactElement => {
 		{ url: "/news/blogs", title: "Blogs" },
 		{ url: "/news/podcasts", title: "Podcasts" },
 	];
+
+	const breadcrumbs = (
+		<Breadcrumbs>
+			<Breadcrumb to="/">Home</Breadcrumb>
+			<Breadcrumb>News</Breadcrumb>
+		</Breadcrumbs>
+	);
+
+	const pageTitle = "News, blogs and podcasts";
+
 	return (
 		<>
-			<h1>News Index Page</h1>
+			<NextSeo title={pageTitle} openGraph={{ title: pageTitle }}></NextSeo>
+			<PageHeader
+				breadcrumbs={breadcrumbs}
+				heading={pageTitle}
+				variant="fullWidthDark"
+			/>
 			<NewsListNav destinations={destinations} />
+
+			<h2>Latest articles</h2>
+			<FeaturedStory story={newsArticles[0]} />
+			<NewsGrid news={newsArticles.slice(1, 4)} />
+			<p>
+				<Link href="/news/articles">View all news articles</Link>
+			</p>
+
+			<h2>Latest in-depth</h2>
+			{inDepthArticles.length}
+			<h2>Latest blogs</h2>
+			{blogPosts.length}
+			<h2>Latest podcasts</h2>
+			{podcasts.length}
 		</>
 	);
-};
+}
 
-export default NewsIndexPage;
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+	const version = getStoryVersionFromQuery(context.query);
+
+	// Construct our query params
+	const commonParams: ISbStoriesParams = {
+		resolve_links: "url",
+		sort_by: "content.date:desc",
+	};
+
+	const articleParams = {
+		...commonParams,
+		starts_with: "news/articles",
+		per_page: 4,
+	};
+
+	const inDepthParams = {
+		...commonParams,
+		starts_with: "news/in-depth",
+		per_page: 2,
+	};
+
+	const blogParams = {
+		starts_with: "news/blogs",
+		...commonParams,
+		excluding_slugs: "news/blogs/authors/*",
+		per_page: 2,
+	};
+
+	const podcastParams = {
+		...commonParams,
+		starts_with: "news/in-depth",
+		per_page: 2,
+	};
+
+	const [newsArticles, inDepthArticles, blogPosts, podcasts] =
+		await Promise.all([
+			fetchStories<NewsArticleStoryblok>(version, articleParams),
+			fetchStories<InDepthArticleStoryblok>(version, inDepthParams),
+			fetchStories<BlogPostStoryblok>(version, blogParams),
+			fetchStories<PodcastStoryblok>(version, podcastParams),
+		]);
+
+	console.log({ newsArticles });
+
+	const result = {
+		props: {
+			newsArticles: newsArticles.stories,
+			inDepthArticles: inDepthArticles.stories,
+			blogPosts: blogPosts.stories,
+			podcasts: podcasts.stories,
+		},
+	};
+	return result;
+}
