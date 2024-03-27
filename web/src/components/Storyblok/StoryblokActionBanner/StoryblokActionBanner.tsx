57f9c6ba45d41f47f33d6c6e650e90ebb780e354
John Davey <66066685+johndavey72@users.noreply.github.com>
2024-03-27 10:15:56 +0000

    NICE-254 Handle image optimisation (#205)
    * NICE-254 Optimise images coming from storyblok image service.
    
    Co-authored-by: Dale Hudson <dalecreativ@users.noreply.github.com>
    
    * NICE-254 Update snapshots
    
    * NICE-254 Add optimise image util and unit test
    
    * NICE-254 Reinstate storyblokhero
    
    ---------
    
    Co-authored-by: Dale Hudson <dalecreativ@users.noreply.github.com>

--- web/src/components/Storyblok/StoryblokActionBanner/StoryblokActionBanner.tsx
+++ web/src/components/Storyblok/StoryblokActionBanner/StoryblokActionBanner.tsx
@@ -2,6 +2,7 @@ import { ActionBanner } from "@nice-digital/nds-action-banner";
 
 import { StoryblokButtonLink } from "@/components/Storyblok/StoryblokButtonLink/StoryblokButtonLink";
 import { ActionBannerStoryblok, RichtextStoryblok } from "@/types/storyblok";
+import { optimiseImage } from "@/utils/storyblok";
 
 import { StoryblokRichText } from "../StoryblokRichText/StoryblokRichText";
 
@@ -21,7 +22,7 @@ export const StoryblokActionBanner: React.FC<StoryblokActionBannerProps> = ({
 			title={heading}
 			variant="fullWidth"
 			cta={<StoryblokButtonLink button={cta[0]} />}
-			image={image.filename}
+			image={optimiseImage({filename:image.filename,size: "899x0"})}
 			className={className}
 		>
 			<StoryblokRichText content={body as RichtextStoryblok} />
