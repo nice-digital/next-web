3523cbf8b0e4eb21b118722eb98dd995320b6ad0
lizaadebowale <liza.adebowale@nice.org.uk>
2024-04-22 10:24:25 +0100

    NXT-241-add-stages-plus-the-noConnectionReuse

--- performance-tests/k6-scripts/top_1000_requests.js
+++ performance-tests/k6-scripts/top_1000_requests.js
@@ -2,6 +2,39 @@ import http from "k6/http";
 import { SharedArray } from "k6/data";
 import { sleep } from "k6";
 
+export const options = {
+	stages: [
+	  { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+	  { duration: '20m', target: 100 }, // stay at 100 users for 20 minutes
+	  { duration: '5m', target: 0 }, // ramp-down to 0 users
+	],
+	thresholds: {
+	  'http_req_duration{status:200}': ['p(99)<1500'], // 99% of requests must complete below 1500ms
+	  'http_req_duration{expected_response:true}': ['p(99)<1500'], // 99% of requests must complete below 1500ms
+	  'iteration_duration': ['p(95)<1500'], // 95% of requests must complete below 1500ms
+	  'http_req_waiting': ['p(99)<1000'], // 99% of requests must complete below 1s   
+	},
+  };
+
+  // export const options = {
+//   stages: [
+//     { duration: '1m', target: 100 }, // below normal load
+//     { duration: '2m', target: 100 },
+//     { duration: '3m', target: 200 }, // normal load
+//     { duration: '3m30s', target: 200 },
+//     { duration: '4m30s', target: 300 }, // around the breaking point
+//     { duration: '6m', target: 300 },
+//     { duration: '4m', target: 400 }, // beyond the breaking point
+//     { duration: '3m', target: 400 },
+//     { duration: '3m', target: 0 }, // scale down. Recovery stage.
+//   ],
+//   thresholds: {
+//     'http_req_duration{status:200}': ['p(90)<1500'], // 99% of requests must complete below 1500ms
+//     'iteration_duration': ['p(95)<1500'], // 95% of requests must complete below 1500ms
+//     'http_req_waiting': ['p(90)<1000'], // 90% of requests must complete below 1s   
+//   },
+// };
+
 const searchTerms = new SharedArray("terms", function () {
 	const t = JSON.parse(open("./combined_paths.json"));
 	return t;
@@ -17,7 +50,10 @@ let params = {
 
 export default function () {
 	const urlpath = searchTerms[Math.floor(Math.random() * searchTerms.length)];
-	const res = http.get("https://alpha.nice.org.uk" + urlpath, params);
+	const res = http.get("https://alpha.nice.org.uk", {noConnectionReuse: true} + urlpath, params);
+	check(res, {
+		'status is 200': (r) => r.status === 200
+
 	// console.log(urlpath);
 
 	// sleep(Math.random() * 2);
@@ -31,4 +67,5 @@ export default function () {
 	// console.log("\n\n\n\n");
 
 	// console.log(res.body + "\n\n\n\n");
+})
 }
