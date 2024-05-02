49ce660ea2264f4996c6b0ce2320c95974c8fb16
John Davey <john.davey@nice.org.uk>
2024-05-02 16:28:27 +0100

    NXT-343 Exclude next-js manifest files from url lowercasing middleware and unit test.
    Co-authored-by: John Holland

--- web/src/middleware.page.tsx
+++ web/src/middleware.page.tsx
@@ -2,6 +2,15 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export const middleware = (req: NextRequest): NextResponse => {
 	const url = req.nextUrl;
+
+	// As we are standardising all the routes to lowercase, we need to exclude these next.js files to avoid 404 errors in production
+	if (
+		url.pathname.includes("_ssgManifest.js") ||
+		url.pathname.includes("_buildManifest.js")
+	) {
+		return NextResponse.next();
+	}
+
 	if (url.pathname === url.pathname.toLowerCase()) {
 		return NextResponse.next();
 	}
