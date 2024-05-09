09b7d94791df354ed0d126c948cc6fb2ddf36630
John Davey <john.davey@nice.org.uk>
2024-05-09 14:11:20 +0100

    NXT-343 Modify comment to trigger TC build

--- web/src/middleware.page.tsx
+++ web/src/middleware.page.tsx
@@ -3,7 +3,7 @@ import { NextResponse, type NextRequest } from "next/server";
 export const middleware = (req: NextRequest): NextResponse => {
 	const url = req.nextUrl;
 
-	// As we are standardising all the routes to lowercase, we need to exclude these next.js files to avoid 404 errors in production
+	// As we are standardising all routes to lowercase, we need to exclude these next.js files to avoid 404 errors in production
 	if (
 		url.pathname.includes("_ssgManifest.js") ||
 		url.pathname.includes("_buildManifest.js")
