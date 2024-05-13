b4bb61b4e81acd7e21dcb52d88a38866bc711bb8
John Davey <66066685+johndavey72@users.noreply.github.com>
2024-05-13 16:45:21 +0100

    NXT-353 Remove middleware page (#235)
    * NXT-353 Remove middleware page
    
    * NXT-353 Reinstate error causing middleware files

--- web/src/middleware.page.test.tsx
+++ web/src/middleware.page.test.tsx
@@ -28,30 +28,4 @@ describe("Middleware", () => {
 			new URL("http://www.nice.org.uk/standards-and-indicators/nlindicators/")
 		);
 	});
-
-	it("should ignore _buildManifest.js file", () => {
-		const req = new NextRequest(
-			new Request(
-				"http://www.nice.org.uk/_next/static/1fwfzfzjgtl2m8mqlorhm/_buildManifest.js"
-			),
-			{}
-		);
-
-		middleware(req);
-
-		expect(redirectSpy).toHaveBeenCalledTimes(0);
-	});
-
-	it("should ignore _ssgManifest.js file", () => {
-		const req = new NextRequest(
-			new Request(
-				"http://www.nice.org.uk/_next/static/1fwfzfzjgtl2m8mqlorhm/_ssgManifest.js"
-			),
-			{}
-		);
-
-		middleware(req);
-
-		expect(redirectSpy).toHaveBeenCalledTimes(0);
-	});
 });
