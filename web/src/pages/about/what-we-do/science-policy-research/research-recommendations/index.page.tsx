e6722bc297373e9be83f91b62fea51754ffd926d
John Holland <john.holland@nice.org.uk>
2024-05-17 17:11:30 +0100

    NXT-253 Updated snapshot and linting

--- web/src/pages/about/what-we-do/science-policy-research/research-recommendations/index.page.tsx
+++ web/src/pages/about/what-we-do/science-policy-research/research-recommendations/index.page.tsx
@@ -71,10 +71,18 @@ export default getProductListPage({
 	intro: (
 		<>
 			<p className="lead">
-				As we develop guidance, we identify gaps, uncertainties or conflicts in the existing evidence base which could benefit from further research. The most important unanswered questions are developed into research recommendations.
+				As we develop guidance, we identify gaps, uncertainties or conflicts in
+				the existing evidence base which could benefit from further research.
+				The most important unanswered questions are developed into research
+				recommendations.
 			</p>
 			<p className="lead">
-				Read our <a href="https://www.nice.org.uk/process/pmg45">research recommendations process and methods guide</a>. For more information email <a href="mailto:research@nice.org.uk">research@nice.org.uk</a>.
+				Read our{" "}
+				<a href="https://www.nice.org.uk/process/pmg45">
+					research recommendations process and methods guide
+				</a>
+				. For more information email{" "}
+				<a href="mailto:research@nice.org.uk">research@nice.org.uk</a>.
 			</p>
 		</>
 	),
