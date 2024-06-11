6b77e5aa7ebb5c865c7d4b083c9a975e1142b7e2
Chris Barker <chris.barker@nice.org.uk>
2024-06-11 14:42:40 +0100

    NICE-306 Add denyRobots config var

--- web/src/pages/api/robots.api.ts
+++ web/src/pages/api/robots.api.ts
@@ -6,14 +6,15 @@ export default function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ): void {
-	if (publicRuntimeConfig.environment === "live") {
+	if (publicRuntimeConfig.denyRobots === "true") {
+		res.send(`User-agent: *
+Disallow: /
+	`);
+	} else {
 		res.send(`User-agent: bingbot
 Crawl-delay: 1
 User-agent: *
-Allow: /`);
-	} else {
-		res.send(`User-agent: *
-Disallow: /
-`);
+Allow: /
+	`);
 	}
 }
