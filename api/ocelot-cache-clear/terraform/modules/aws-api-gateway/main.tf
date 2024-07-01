7152e3f715be32319d719b33c8a043c1c87c9082
AlexPowell-nice <alex.powell@nice.org.uk>
2024-07-01 11:03:23 +0100

    NXT-366 Changed openapi to file

--- api/ocelot-cache-clear/terraform/modules/aws-api-gateway/main.tf
+++ api/ocelot-cache-clear/terraform/modules/aws-api-gateway/main.tf
@@ -1,34 +1,5 @@
 resource "aws_api_gateway_rest_api" "ocelot_cache_clear_rest_api" {
-  body = jsonencode({
-    openapi = "3.0.1"
-    info = {
-      title   = "OcelotCacheClear"
-      version = "1.0"
-    }
-    paths = {
-      "/OcelotCacheClear" = {
-        get = {
-          x-amazon-apigateway-integration = {
-            httpMethod           = "GET"
-            type                 = "aws_proxy"
-            uri                  = "arn:aws:apigateway:eu-west-1:lambda:path/2015-03-31/functions/arn:aws:lambda:eu-west-1:362524971603:function:$${stageVariables.OcelotCacheClear}/invocations",
-            responses: {
-            default: {
-              statusCode: 200
-            }
-          },
-          passthroughBehavior: "when_no_match",
-          contentHandling: "CONVERT_TO_TEXT"
-          },
-          responses: {
-            200: {
-              description: "topic browse mappings response"
-            }
-          }
-        }
-      }
-    }
-  })
+  body        = file("${path.module}/openapi.json")
   name        = var.api_name
   description = var.api_name
   endpoint_configuration {
