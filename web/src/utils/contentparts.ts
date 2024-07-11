709f8ca02cd0c8aa1c9bbdab16077cb870516216
tresaknice <tresa.kuruvilla@nice.org.uk>
2024-07-11 15:18:13 +0100

    Update contentparts.ts

--- web/src/utils/contentparts.ts
+++ web/src/utils/contentparts.ts
@@ -2,11 +2,11 @@
  * Filters and maps an array of content parts to a specific type.
  *
  * @export
- * @template T Type "T" can be either "UploadAndConvertContentPart", "EditableContentPart",
+ * @template T - Type "T" can be either "UploadAndConvertContentPart", "EditableContentPart",
  * 			   "UploadContentPart", or "ExternalUrlContentPart"
- * @param {[]} contentParts The array of content parts to filter, each with a "type" property.
- * @param {string} type The type of content parts to filter by (example: "UploadAndConvertContentPart")
- * @returns {T[]} returns an array of content parts matching the specified type, cast to "T".
+ * @param {[]} contentParts - The array of content parts to filter, each with a "type" property.
+ * @param {string} type - The type of content parts to filter by (example: "UploadAndConvertContentPart")
+ * @returns {T[]} - returns an array of content parts matching the specified type, cast to "T".
  */
 export function fetchAndMapContentParts<T>(
 	contentParts: [],
