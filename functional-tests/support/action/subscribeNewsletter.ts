42c24ad93a873f588832b2adc22ab86c77fd33c6
FunmiOsho <60607637+FunmiOsho@users.noreply.github.com>
2024-07-15 10:15:25 +0100

    NXT-333 update supporting action file and fix eslint issue

--- functional-tests/support/action/subscribeNewsletter.ts
+++ functional-tests/support/action/subscribeNewsletter.ts
@@ -3,8 +3,6 @@ import { pause } from "@nice-digital/wdio-cucumber-steps/lib/support/action/paus
 import { setInputField } from "@nice-digital/wdio-cucumber-steps/lib/support/action/setInputField.js";
 import { waitForDisplayed } from "@nice-digital/wdio-cucumber-steps/lib/support/action/waitForDisplayed.js";
 
-import selectors from "../selectors.js";
-
 // import scroll from '@nice-digital/wdio-cucumber-steps/lib/support/action/scroll';
 // import emailInput from '../check/emailInput';
 
@@ -12,55 +10,29 @@ export async function subscribeNewsletter(): Promise<void> {
 	const sectorValue = await $("#input_11");
 	const jobRoleValue = await $("#input_12");
 
-	await waitForDisplayed(selectors.SubscribeToNewsletter.emailAddress, "");
-	await setInputField(
-		"set",
-		"commenttestuser+1@gmail.com",
-		selectors.SubscribeToNewsletter.emailAddress
-	);
+	await waitForDisplayed("#input6", "");
+	await setInputField("set", "commenttestuser+1@gmail.com", "#input6");
+	await pause("1000");
 
+	await waitForDisplayed("#first_15", "");
+	await setInputField("set", "comment", "#first_15");
 	await pause("1000");
-	await waitForDisplayed(
-		selectors.SubscribeToNewsletter.firstnameforNewsletter,
-		""
-	);
-	await setInputField(
-		"set",
-		"comment",
-		selectors.SubscribeToNewsletter.firstnameforNewsletter
-	);
+
+	await waitForDisplayed("#last_15", "");
+	await setInputField("set", "user", "#last_15");
 	await pause("1000");
-	await waitForDisplayed(
-		selectors.SubscribeToNewsletter.lastnameforNewsletter,
-		""
-	);
-	await setInputField(
-		"set",
-		"user",
-		selectors.SubscribeToNewsletter.lastnameforNewsletter
-	);
-	await pause("1000");
-	await waitForDisplayed(selectors.SubscribeToNewsletter.sector, "");
-	await clickElement(
-		"click",
-		"selector",
-		selectors.SubscribeToNewsletter.sector
-	);
+
+	await waitForDisplayed("sectorValue", "");
+	await clickElement("click", "selector", "sectorValue");
 	await sectorValue.selectByVisibleText("Consultancy");
 	await pause("1000");
-	await waitForDisplayed(selectors.SubscribeToNewsletter.jobRole, "");
-	await clickElement(
-		"click",
-		"selector",
-		selectors.SubscribeToNewsletter.sector
-	);
+
+	await waitForDisplayed("jobRoleValue", "");
+	await clickElement("click", "selector", "jobRoleValue");
 	await jobRoleValue.selectByVisibleText("Academic");
 	await pause("1000");
-	await clickElement(
-		"click",
-		"selector",
-		selectors.SubscribeToNewsletter.subscribeButton
-	);
+
+	await clickElement("click", "selector", "#input_4");
 	await pause("1000");
 }
 
