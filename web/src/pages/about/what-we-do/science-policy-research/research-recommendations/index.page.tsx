3c61bc1ab2d2613668af5d8acc2cc2d0bdb2dbd1
John Holland <john.holland@nice.org.uk>
2024-07-22 13:29:56 +0100

    NXT-125 Updated snapshot and linting fix

--- web/src/pages/about/what-we-do/science-policy-research/research-recommendations/index.page.tsx
+++ web/src/pages/about/what-we-do/science-policy-research/research-recommendations/index.page.tsx
@@ -3,7 +3,7 @@ import { Document, SortOrder } from "@nice-digital/search-client";
 
 import {
 	getGetServerSidePropsFunc,
-	getProductListPage
+	getProductListPage,
 } from "@/components/ProductListPage/ProductListPage";
 import { ResponsiveDate } from "@/components/ResponsiveDate/ResponsiveDate";
 import { publicRuntimeConfig } from "@/config";
