b499b1960fd0b1875417343192e70fa33d1a3015
John Davey <66066685+johndavey72@users.noreply.github.com>
2024-08-02 10:32:33 +0100

    NXT-421 Life sciences enquiry form update jotform (#285)
    * NXT-421 Update life sciences contact us form and test
    
    * NXT-421 Update snapshot

--- web/src/pages/forms/life-sciences-contact-us.page.tsx
+++ web/src/pages/forms/life-sciences-contact-us.page.tsx
@@ -3,8 +3,9 @@ import {
 	getGetServerSideProps,
 } from "@/components/JotFormPage/getGetServerSideProps";
 import { JotFormPage } from "@/components/JotFormPage/JotFormPage";
+import { Link } from "@/components/Link/Link";
 
-export const getServerSideProps = getGetServerSideProps("232991221799063");
+export const getServerSideProps = getGetServerSideProps("242062773274053");
 
 export default function LifeSciencesContactUsForm(
 	props: FormProps
@@ -13,7 +14,7 @@ export default function LifeSciencesContactUsForm(
 		<JotFormPage
 			{...props}
 			formName="Life sciences contact us form"
-			lead=""
+			lead="Briefly tell us why you're getting in touch. We'll then give you an indication of how we can support you. In most cases, we'll include estimated costs and timelines sent straight to your inbox."
 			parentPages={[
 				{
 					title: "Life sciences: how to get your product to market",
@@ -28,6 +29,16 @@ export default function LifeSciencesContactUsForm(
 					path: "/about",
 				},
 			]}
+			formFooter={
+				<p>
+					As you have expressed an interest in NICE&apos;s support services,
+					your details will be added to our mailing list so that you can receive
+					the latest news about NICE&apos;s support for the life sciences
+					sector. You can unsubscribe from this at any time. Our legal basis for
+					processing your data in this way is that of public task. View our{" "}
+					<Link to="/privacy-notice">privacy notice</Link> for more information.
+				</p>
+			}
 		/>
 	);
 }
