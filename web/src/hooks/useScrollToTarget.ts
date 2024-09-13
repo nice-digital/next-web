13cc6620f5cfe8c0031449d526acc579c8ef449e
John Holland <john.holland@nice.org.uk>
2024-09-13 12:23:43 +0100

    NXT-432 Linting fixes

--- web/src/hooks/useScrollToTarget.ts
+++ web/src/hooks/useScrollToTarget.ts
@@ -1,27 +1,22 @@
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 
-export const useScrollToTarget = (
-	scrollTargetId: string
-): (() => void) => {
+export const useScrollToTarget = (scrollTargetId: string): (() => void) => {
 	const { events } = useRouter();
 
-	const scrollToTarget = useCallback(
-		() => {
-			const targetElement = document.getElementById(scrollTargetId);
+	const scrollToTarget = useCallback(() => {
+		const targetElement = document.getElementById(scrollTargetId);
 
-			if (targetElement) {
-				targetElement.setAttribute("tabIndex", "-1");
-				targetElement.focus();
-				targetElement.scrollIntoView();
-			} else {
-				console.warn(`Element with id ${scrollTargetId} could not be found`);
-			}
+		if (targetElement) {
+			targetElement.setAttribute("tabIndex", "-1");
+			targetElement.focus();
+			targetElement.scrollIntoView();
+		} else {
+			console.warn(`Element with id ${scrollTargetId} could not be found`);
+		}
 
-			events.off("routeChangeComplete", scrollToTarget);
-		},
-		[events, scrollTargetId]
-	);
+		events.off("routeChangeComplete", scrollToTarget);
+	}, [events, scrollTargetId]);
 
 	return scrollToTarget;
 };
