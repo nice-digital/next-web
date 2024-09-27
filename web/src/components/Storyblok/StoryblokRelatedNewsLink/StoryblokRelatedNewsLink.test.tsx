878ccb464f5810a20fa8755caef6c672c2674adf
Dale Hudson <dalefrontend@gmail.com>
2024-09-27 12:30:24 +0100

    Nxt 459 replace next js links with anchors on storyblok related links component to avoid next web handling guidance detail routes (#300)
    * NXT-459 : (refactor) handle internal story link urls with cache fall back and preceding slash for internal slugs
    
    - url was always empty on story linkTypes. updated to use story.url from guidance here: https://www.storyblok.com/faq/link-object-history#feature-resolve_links-url-and-resolve_links-story-was-added
    - added slash to the start of story urls, as all responses show link urls as relative
    - updated tests
    
    Co-authored-by: John Davey <johndavey72@users.noreply.github.com>
    
    * NXT-459 : (fix) update components using resolveStoryblokLink util
    
    - updated StoryblokButton to remove need to prepend slash on internal link.  Now handled via util function
    - refactored StoryblokRelatedLink to use` a` tag if link is external url and Link component if it's an internal url
    - update tests across components and update snapshots
    
    Co-authored-by: John Davey <johndavey72@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: John Davey <johndavey72@users.noreply.github.com>

--- web/src/components/Storyblok/StoryblokRelatedNewsLink/StoryblokRelatedNewsLink.test.tsx
+++ web/src/components/Storyblok/StoryblokRelatedNewsLink/StoryblokRelatedNewsLink.test.tsx
@@ -5,14 +5,20 @@ import { RelatedNewsLinkStoryblok } from "@/types/storyblok";
 import { StoryblokRelatedNewsLink } from "./StoryblokRelatedNewsLink";
 
 const mockRelatedNewsLinkResponse: RelatedNewsLinkStoryblok = {
-	title: "Test title",
-	publisher: "Test publisher",
-	date: "2021-01-01",
+	_uid: "42dac477-17b6-4052-9025-16a24dc1a438",
+	date: "2024-05-09 12:00",
 	link: {
-		url: "https://www.google.com",
+		id: "",
+		url: "https://www.standard.co.uk/news/health/mark-chapman-nhs-wales-swansea-university-b1156651.html",
+		linktype: "url",
+		fieldtype: "multilink",
+		cached_url:
+			"https://www.standard.co.uk/news/health/mark-chapman-nhs-wales-swansea-university-b1156651.html",
 	},
-	_uid: "123",
+	title:
+		"Health watchdog backs wider use of breast cancer tumour profiling tests",
 	component: "relatedNewsLink",
+	publisher: "The Standard",
 };
 
 describe("StoryblokRelatedNewsLink Component", () => {
@@ -33,7 +39,7 @@ describe("StoryblokRelatedNewsLink Component", () => {
 	it("should render the date of the related news link", () => {
 		render(<StoryblokRelatedNewsLink blok={mockRelatedNewsLinkResponse} />);
 
-		const date = screen.getByText("1 January 2021");
+		const date = screen.getByText("9 May 2024");
 		expect(date).toBeInTheDocument();
 	});
 
@@ -44,5 +50,35 @@ describe("StoryblokRelatedNewsLink Component", () => {
 			name: mockRelatedNewsLinkResponse.title,
 		});
 		expect(link).toBeInTheDocument();
+	});
+
+	it("should handle internal links to news articles", () => {
+		const pathWithoutLeadingSlash = "news/articles/mark-chapman";
+		const mockInternalNewsLinkResponse: RelatedNewsLinkStoryblok = {
+			...mockRelatedNewsLinkResponse,
+			link: {
+				...mockRelatedNewsLinkResponse.link,
+				url: "",
+				linktype: "story",
+				cached_url: pathWithoutLeadingSlash,
+				story: {
+					name: "Mark Chapman",
+					id: 907,
+					uuid: "730222",
+					slug: "mark-chapman",
+					url: pathWithoutLeadingSlash,
+					full_slug: pathWithoutLeadingSlash,
+					_stopResolving: true,
+				},
+			},
+		};
+
+		render(<StoryblokRelatedNewsLink blok={mockInternalNewsLinkResponse} />);
+
+		const link = screen.getByRole("link", {
+			name: mockInternalNewsLinkResponse.title,
+		});
+		expect(link).toBeInTheDocument();
+		expect(link).toHaveAttribute("href", `/${pathWithoutLeadingSlash}`);
 	});
 });
