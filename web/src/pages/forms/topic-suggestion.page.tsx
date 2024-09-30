463cfef36c881df258dffd2db522ef371dd22ca7
John Davey <john.davey@nice.org.uk>
2024-09-30 14:32:23 +0100

    NXT-462, NXT-444 Add topic suggestion form and unit test

---
+++ web/src/pages/forms/topic-suggestion.page.tsx
@@ -0,0 +1,34 @@
+import {
+	type FormProps,
+	getGetServerSideProps,
+} from "@/components/JotFormPage/getGetServerSideProps";
+import { JotFormPage } from "@/components/JotFormPage/JotFormPage";
+
+import styles from "./forms.module.scss";
+
+export const getServerSideProps = getGetServerSideProps("241421977936062");
+241421977936062;
+
+export default function TopicSuggestionForm(props: FormProps): JSX.Element {
+	return (
+		<JotFormPage
+			{...props}
+			formName="Topic suggestion"
+			lead=""
+			parentPages={[
+				{
+					title: "Prioritising our guidance topics",
+					path: "/about/what-we-do/prioritising-our-guidance-topics",
+				},
+				{
+					title: "What we do",
+					path: "/about/what-we-do",
+				},
+				{
+					title: "About",
+					path: "/about",
+				},
+			]}
+		/>
+	);
+}
