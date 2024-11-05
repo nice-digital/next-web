ba70e88d0faf3f1800b9ca2a3b47e2462a03a469
Dale Hudson <dalefrontend@gmail.com>
2024-11-05 16:46:22 +0000

    NXT-477, NXT-478 hero component for category landing page (#312)
    * NXT-475 Add base categoryLandingPage
    
    - Add base template for categoryLandingPage with tests
    - update storyblok types from live space
    
    * NXT-475 add categoryLandingPage components to catch all setComponents
    
    * NXT-478 add updated hero with description prop to Storyblok and CategoryLandingPage
    
    - Bump DS version for updated Hero props
    - Add updated props to StoryblokHero component
    - Make Hero component available on Category Landing Page
    - Update tests and snapshots
    
    * NXT-478 Add theme options to StoryblokHero component to allow nds-hero 'isDark' variant to be used in storyblok
    
    * NXT-478 Add updated generated storyblok types from alpha space
    
    * NXT-478 update snapshots
    
    * NXT-478 Add theme logic to select light and dark variants
    
    * NXT-478 Version bump design system
    
    ---------
    
    Co-authored-by: John Davey <john.davey@nice.org.uk>

---
+++ web/src/components/Storyblok/CategoryLandingPage/CategoryLandingPage.tsx
@@ -0,0 +1,30 @@
+import { StoryblokComponent } from "@storyblok/react";
+
+import { type CategoryLandingPageStoryblok } from "@/types/storyblok";
+
+// import styles from "./CategoryLandingPage.module.scss";
+
+export interface CategoryLandindPageBlokProps {
+	blok: CategoryLandingPageStoryblok;
+}
+
+export const CategoryLandingPage = ({
+	blok,
+}: CategoryLandindPageBlokProps): React.ReactElement => {
+	return (
+		<>
+			{blok.metadata &&
+				blok.metadata.length > 0 &&
+				blok.metadata?.map((nestedBlok) => (
+					<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+				))}
+			{blok.header.map((nestedBlok) => (
+				<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+			))}
+			{/* {blok.content &&
+				blok.content.map((nestedBlok) => (
+					<StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+				))} */}
+		</>
+	);
+};
