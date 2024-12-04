e110f34b7d7ead4ff177d54e46cbf29486514209
John Davey <john.davey@nice.org.uk>
2024-12-04 10:22:51 +0000

    NXT-511 Add .page extension to app not-found page

---
+++ web/src/app/not-found.page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { ErrorPageContent } from "@/components/ErrorPageContent/ErrorPageContent";
+
+// Note: 500 (and other status codes) are handled via LINK ./_error.page.tsx
+
+export default function NotFoundPage(): JSX.Element {
+	return (
+		<ErrorPageContent
+			title="Page not found"
+			heading="We can't find this&nbsp;page"
+			lead="It's probably been moved, updated or&nbsp;deleted."
+		/>
+	);
+}
