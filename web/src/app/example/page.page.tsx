481cfa766b15eed106f2f9ca75e3a3abc3d60109
John Davey <john.davey@nice.org.uk>
2024-12-04 16:42:31 +0000

    NXT-511 Metadata for example page wip

--- web/src/app/example/page.page.tsx
+++ web/src/app/example/page.page.tsx
@@ -1,4 +1,22 @@
 import React from "react";
+import { ResolvingMetadata, Metadata } from 'next';
+import { getDefaultSeoConfig } from "src/pages/next-seo.config";
+
+
+type Props = {
+	params: Promise<{ id: string }>
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+  }
+
+  export const generateMetadata = async(
+	props: Props,
+	parent: ResolvingMetadata
+  ): Promise<Metadata> => {
+
+	const defaultSeoConfig = getDefaultSeoConfig("example") as unknown as Metadata;
+
+	return {title: "Example Page | NICE",...defaultSeoConfig}
+  }
 
 const ExamplePage = (): React.ReactNode => {
 	return (
