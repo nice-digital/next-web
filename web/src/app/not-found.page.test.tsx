1b954414421c4f49976f5817767550989991b38f
John Holland <john.holland@nice.org.uk>
2024-12-04 10:46:50 +0000

    NXT-511 Unit tests for new app not-found page

---
+++ web/src/app/not-found.page.test.tsx
@@ -0,0 +1,45 @@
+import { screen, waitFor } from "@testing-library/react";
+
+import { render } from "@/test-utils/rendering";
+
+import NotFoundPage from "./not-found.page";
+
+describe("404", () => {
+	describe("SEO", () => {
+		it("should render 'Page not found' in the page title", async () => {
+			render(<NotFoundPage />);
+			await waitFor(() => {
+				expect(document.title).toStartWith("Page not found");
+			});
+		});
+	});
+
+	describe("Breadcrumbs", () => {
+		it("should render home breadcrumb linking to the homepage", () => {
+			render(<NotFoundPage />);
+			expect(
+				screen.queryByText("Home", {
+					selector: ".breadcrumbs a",
+				})
+			).toHaveAttribute("href", "/");
+		});
+
+		it("should render page not found as current page breadcrumb without link", () => {
+			render(<NotFoundPage />);
+			expect(
+				screen.getByText("Page not found", {
+					selector: ".breadcrumbs span",
+				})
+			).toBeInTheDocument();
+		});
+	});
+
+	it("should render heading 1 with correct text", () => {
+		render(<NotFoundPage />);
+		expect(
+			screen.getByRole("heading", {
+				level: 1,
+			})
+		).toHaveTextContent("We can't find this page");
+	});
+});
