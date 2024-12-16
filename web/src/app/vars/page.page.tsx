a8de0e66da36e284b8c8d533f3de5eda9855fe01
John Davey <john.davey@nice.org.uk>
2024-12-16 14:26:05 +0000

    NXT-511 lint fix

--- web/src/app/vars/page.page.tsx
+++ web/src/app/vars/page.page.tsx
@@ -1,9 +1,15 @@
 export default async function Home() {
-	const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:4000'}/api/env2`, {
-	  cache: 'no-store',
-	});
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:4000"}/api/env2`,
+		{
+			cache: "no-store",
+		}
+	);
 	if (!response.ok) {
-		console.error('Failed to fetch environment variables:', response.statusText);
+		console.error(
+			"Failed to fetch environment variables:",
+			response.statusText
+		);
 		return (
 			<div>
 				<p>Error fetching environment variables</p>
@@ -14,7 +20,7 @@ export default async function Home() {
 	try {
 		env = await response.json();
 	} catch (error) {
-		console.error('Invalid JSON response:', error);
+		console.error("Invalid JSON response:", error);
 		return (
 			<div>
 				<p>Error parsing environment variables</p>
@@ -22,14 +28,14 @@ export default async function Home() {
 		);
 	}
 
-	console.log({env})
+	console.log({ env });
 
 	return (
-	  <>
-	  	<h1>Env vars</h1>
-		<p>BASE URL: {env.NEXT_PUBLIC_BASE_URL}</p>
-		<p>Auth Environment: {env.NEXT_PUBLIC_AUTH_ENVIRONMENT}</p>
-		<p>Build number: {env.NEXT_PUBLIC_BUILD_NUMBER}</p>
-	  </>
+		<>
+			<h1>Env vars</h1>
+			<p>BASE URL: {env.NEXT_PUBLIC_BASE_URL}</p>
+			<p>Auth Environment: {env.NEXT_PUBLIC_AUTH_ENVIRONMENT}</p>
+			<p>Build number: {env.NEXT_PUBLIC_BUILD_NUMBER}</p>
+		</>
 	);
-  }
+}
