4ad0182edf7f426dd0f120457dc1dd11c89ced0b
John Holland <john.holland@nice.org.uk>
2024-12-19 14:09:23 +0000

    NXT-511 Reinstated john variables to api

--- web/src/app/api/env/route.api.ts
+++ web/src/app/api/env/route.api.ts
@@ -8,8 +8,8 @@ export async function GET(): Promise<NextResponse> {
 		NEXT_PUBLIC_BUILD_NUMBER: process.env.NEXT_PUBLIC_BUILD_NUMBER,
 		NEXT_PUBLIC_SEARCH_BASE_URL: process.env.NEXT_PUBLIC_SEARCH_BASE_URL,
 		PUBLIC_DENY_ROBOTS: process.env.PUBLIC_DENY_ROBOTS,
-		// NEXT_PUBLIC_JOHN: process.env.NEXT_PUBLIC_JOHN,
-		// PUBLIC_JOHN: process.env.PUBLIC_JOHN,
+		NEXT_PUBLIC_JOHN: process.env.NEXT_PUBLIC_JOHN,
+		PUBLIC_JOHN: process.env.PUBLIC_JOHN,
 	};
 
 	return NextResponse.json(envVars);
