81a786ffb0ff94b0e802dae4368184c22511d915
John Davey <john.davey@nice.org.uk>
2024-12-23 14:31:50 +0000

    NXT-511 lint fix

--- web/src/app/env-dynamic/page.page.tsx
+++ web/src/app/env-dynamic/page.page.tsx
@@ -4,18 +4,20 @@ import EnvPageClient from "./client";
 export const dynamic = "force-dynamic";
 
 const EnvPage = async (): Promise<JSX.Element> => {
-  try {
-    const envVars = await getNextPublicEnvVars();
-    return <EnvPageClient envVars={envVars} />;
-  } catch (error) {
-    console.error("Error fetching environment variables:", error);
-    return (
-      <div>
-        <h1>Error</h1>
-        <p>Failed to fetch environment variables. Check the console for details.</p>
-      </div>
-    );
-  }
+	try {
+		const envVars = await getNextPublicEnvVars();
+		return <EnvPageClient envVars={envVars} />;
+	} catch (error) {
+		console.error("Error fetching environment variables:", error);
+		return (
+			<div>
+				<h1>Error</h1>
+				<p>
+					Failed to fetch environment variables. Check the console for details.
+				</p>
+			</div>
+		);
+	}
 };
 
 export default EnvPage;
