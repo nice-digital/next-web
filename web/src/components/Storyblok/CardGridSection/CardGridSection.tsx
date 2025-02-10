9df8443fedc1452f0e272f2137f81e82d7a3fad1
Becca Dawson <rebecca.dawson@nice.org.uk>
2025-02-10 14:51:40 +0000

    NXT-493 Start adding Card Grid pattern pseudo code

---
+++ web/src/components/Storyblok/CardGridSection/CardGridSection.tsx
@@ -0,0 +1,8 @@
+// import { Card, type CardHeadingLinkProps } from "@nice-digital/nds-card";
+// import { Grid, GridItem } from "@nice-digital/nds-grid";
+// import { CalloutCard } from "@/components/Storyblok/CalloutCard/CalloutCard"
+// import { TestimonialGridItem } from "@/components/Storyblok/TestimonialGridItem/TestimonialGridItem"
+
+<Grid elementType="ul" equalHeight>
+
+</Grid>
