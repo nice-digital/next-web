2197dcf4620650af9e04189315856904f35d44eb
Becca Dawson <rebecca.dawson@nice.org.uk>
2025-02-24 14:30:31 +0000

    NXT-531 Update StoryblokImage to use image intrinsic dimensions for height/width attrs from image src

--- web/src/components/Storyblok/StoryblokImage/StoryblokImage.tsx
+++ web/src/components/Storyblok/StoryblokImage/StoryblokImage.tsx
@@ -44,11 +44,20 @@ export const StoryblokImage = forwardRef<HTMLImageElement, StoryblokImageProps>(
 		const webPSrc = constructStoryblokImageSrc(src, serviceOptions, "webp");
 		const avifSrc = constructStoryblokImageSrc(src, serviceOptions, "avif");
 		const jpgSrc = constructStoryblokImageSrc(src, serviceOptions, "jpeg");
-		// Set height & width to values provided from image service with fallback as 3x2 as per design system Callout Card example
+
+		// Set height & width to values provided from image service with fallback as 3x2 aspect ratio as per design system Callout Card example
+		// Will need updating if shape of image url changes; assumes https://a.storyblok.com/f/292509/648x349/be49eaa335/image-name.JPG/m/filters:format%28avif%29:quality%2880%29
+		const widthSubstr = src.includes("/") ? src.split("/")[5].split("x")[0] : "";
+		const heightSubstr = src.includes("/") ? src.split("/")[5].split("x")[1] : "";
 		const dimensions = {
-			width: src ? src.split("/")[5].split("x")[0] : 600,
-			height: src ? src.split("/")[5].split("x")[1] : 400,
+			width: !isNaN(parseFloat(widthSubstr)) ? widthSubstr : 600,
+			height: !isNaN(parseFloat(heightSubstr)) ? heightSubstr : 400,
 		};
+
+		if (isNaN(parseFloat(widthSubstr)) || isNaN(parseFloat(heightSubstr))) {
+			console.warn("Dimensions are not valid numbers");
+		}
+
 		return (
 			<picture>
 				<source srcSet={avifSrc || src} type="image/avif" />
