695d72b0cecc207204f4ec3ae0f44be331fdc4b4
John Davey <john.davey@nice.org.uk>
2025-03-19 17:00:05 +0000

    NICE-340 3 api calls to gather required insection nav data  - wip

--- web/src/pages/in-section-spike.page.tsx
+++ web/src/pages/in-section-spike.page.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { GetServerSideProps } from "next";
 import { publicRuntimeConfig } from "@/config";
 import React from "react";
@@ -7,23 +9,35 @@ type Link = {
 	name: string;
 	is_folder: boolean;
 }
-const InSectionSpike = ({ siblings }: { siblings: Link[] }): JSX.Element => {
+const InSectionSpike = ({ siblings, parentAndSiblings }: { siblings: Link[] }): JSX.Element => {
 
 	const linksArray = Object.values(siblings);
+	const parentAndSiblingsArray = Object.values(parentAndSiblings);
 
 
 	return (
 		<>
 		<h2>In section spike</h2>
 		<ul>
-			<h3>Flat structure with folders highlighted</h3>
+			<h3>Flat structure with folders highlighted - current page and its siblings</h3>
 		{linksArray.map((link: Link) => (
 			<li key={link.id}>
 				{link.name} {" "}
 				{link.is_folder && `📁`}
 			</li>
 		))}
-		</ul></>
+		</ul>
+		<ul>
+			<h3>Flat structure with folders highlighted - parent page and its siblings</h3>
+		{parentAndSiblingsArray.map((link: Link) => (
+			<li key={link.id}>
+				{link.name} {" "}
+				{link.is_folder && `📁`	}
+			</li>
+		))}
+		</ul>
+
+		</>
 	);
 }
 
@@ -31,30 +45,50 @@ const InSectionSpike = ({ siblings }: { siblings: Link[] }): JSX.Element => {
 // fetch data server side
 export const getServerSideProps: GetServerSideProps = async (context) => {
 	const slug = Array.isArray(context.query.slug) ? context.query.slug[0] : context.query.slug;
-	const exampleMap = {
-		"implementing-nice-guidance": "563840532",
-		"implementing-nice-guidance/cost-saving-resource-planning-and-audit": "642423873"
-	}
 
+	const exampleMap = {
+        "implementing-nice-guidance": "563840532",
+        "implementing-nice-guidance/cost-saving-resource-planning-and-audit": "642423873"
+    }
+
+	const parentID = slug && slug in exampleMap ? exampleMap[slug as keyof typeof exampleMap] : undefined;
 	// storyID 642423873 is a folder
 	// id 642430829 is the root story of the parent folder
-	const parentID = slug && slug in exampleMap ? exampleMap[slug as keyof typeof exampleMap] : undefined;
 	console.log("slug from querystring:", slug);
 	const token = publicRuntimeConfig.storyblok.accessToken;
 
+	//https://api.storyblok.com/v2/cdn/links?version=published&token=O8HhG6D7S3KeDqGrZeDMoAtt&starts_with=implementing-nice-guidance/cost-saving
 
 	const res = await fetch(`https://api.storyblok.com/v2/cdn/links?version=published&token=${token}&with_parent=${parentID}`);
 	const data = await res.json();
 	const siblings = data.links;
 
 	// we need other pages with the parent's parent id...
+	// id 642423873 is the folder
+	// id 563840532 is the parent id
+
+	console.log("siblings", siblings);
+	// find the object in siblings where the is_folder is true AND it the slug matches the current slug
+
+	const startsWithres = await fetch(`https://api.storyblok.com/v2/cdn/links?version=published&token=${token}&starts_with=${slug}`);
+	const startsWithData = await startsWithres.json();
+	const startsWith = startsWithData.links;
+
+	const currentFolder = Object.values(startsWith).find(item => item.is_folder && item.slug === slug);
 
 
+	const parentRes = await fetch(`https://api.storyblok.com/v2/cdn/links?version=published&token=${token}&with_parent=${currentFolder.parent_id}`);
+	const parentData = await parentRes.json();
+
+	const parentAndSiblings = parentData.links;
+
+	console.log("parentAndSiblings >>>>>>", parentAndSiblings);
 
 
 	return {
 		props: {
 			siblings,
+			parentAndSiblings
 		}
 	}
 }
