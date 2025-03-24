21bb5b5bc24e1bb00bbceea18946c4629e30b49a
Dale H <dale.hudson@nice.org.uk>
2025-03-24 13:21:36 +0000

    NICE-440 reintroduce layout client components wrapped with Suspense and test build output

---
+++ web/src/app/example/example-child-page/page.page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TestExamplePage(): JSX.Element {
+	return (
+		<React.Fragment>
+			<h2>This is a test Example page child page</h2>
+		</React.Fragment>
+	);
+}
--- web/src/app/example/page.page.tsx
+++ web/src/app/example/page.page.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-// import { publicRuntimeConfig, PublicConfig } from "@/config";
+import { publicRuntimeConfig, PublicConfig } from "@/config";
 
 export default function TestExamplePage(): JSX.Element {
-	// const configList = [];
-	// for (const property in publicRuntimeConfig) {
-	// 	configList.push(<li key={property}>{property}: </li>);
-	// }
+	const configList = [];
+	for (const property in publicRuntimeConfig) {
+		configList.push(<li key={property}>{property}: </li>);
+	}
 	return (
 		<React.Fragment>
 			<h2>This is a test Example page that should be statically rendered</h2>
-			{/* <ul>{configList}</ul>
-			<p>cookieBannerScriptURL: {publicRuntimeConfig.cookieBannerScriptURL}</p> */}
+			<ul>{configList}</ul>
+			<p>cookieBannerScriptURL: {publicRuntimeConfig.cookieBannerScriptURL}</p>
 		</React.Fragment>
 	);
 }
