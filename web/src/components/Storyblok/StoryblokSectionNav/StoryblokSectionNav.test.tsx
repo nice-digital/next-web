23f49011449b880e7f85b51143316a2cb6e90b42
Shilpa Unni <shilpa.unni@nice.org.uk>
2025-04-17 17:19:54 +0100

    NXT-574 Add additional unit test to cover the  isCurrent scenario

--- web/src/components/Storyblok/StoryblokSectionNav/StoryblokSectionNav.test.tsx
+++ web/src/components/Storyblok/StoryblokSectionNav/StoryblokSectionNav.test.tsx
@@ -1,19 +1,13 @@
 import { render, screen } from "@testing-library/react";
 
 import { StoryblokSectionNav } from "./StoryblokSectionNav";
-import mockData from "@/mockData/storyblok/sectionNavData.json"
+import mockData from "@/mockData/storyblok/sectionNavData.json";
 
 // Example mock data for the StoryblokSectionNav component
 
-
 describe("StoryblokSectionNav", () => {
 	it("should render the parent and child links correctly", () => {
-		render(
-			<StoryblokSectionNav
-				tree={mockData.tree}
-				slug={mockData.slug}
-			/>
-		);
+		render(<StoryblokSectionNav tree={mockData.tree} slug={mockData.slug} />);
 
 		// Check if the parent link is rendered
 		const parentLink = screen.getByText(
@@ -24,11 +18,18 @@ describe("StoryblokSectionNav", () => {
 			"stacked-nav__root stacked-nav__root--no-link"
 		);
 	});
+	it("should not render the parent and child links correctly", () => {
+		render(<StoryblokSectionNav tree={[]} slug={mockData.slug} />);
 
+		// Check if the parent link is rendered
+		const parentLink = screen.queryByText(
+			"Cost saving, resource planning and audit"
+		);
+		expect(parentLink).not.toBeInTheDocument();
+	});
 	it("should render the current level and level above it correctly when there are no children", () => {
 		render(
 			<StoryblokSectionNav
-
 				tree={mockData.currentPageNoChildrenTree}
 				slug={
 					"implementing-nice-guidance/cost-saving-resource-planning-and-audit/nice-and-health-inequalities/nice-and-core20plus5-adults/smoking-cessation"
@@ -43,29 +44,34 @@ describe("StoryblokSectionNav", () => {
 		expect(childLink1).toHaveClass("stacked-nav__content-wrapper");
 		expect(childLink2).toHaveClass("stacked-nav__content-wrapper");
 	});
+
 	it("should highlight the current child link based on the slug", () => {
 		render(
 			<StoryblokSectionNav
-
 				tree={mockData.currentPageNoChildrenTree}
 				slug={
-					"implementing-nice-guidance/cost-saving-resource-planning-and-audit/nice-and-health-inequalities/nice-and-core20plus5-adults/nice-and-core20plus5-adults/smoking-cessation"
+					"implementing-nice-guidance/cost-saving-resource-planning-and-audit/nice-and-health-inequalities/nice-and-core20plus5-adults/smoking-cessation"
 				}
 			/>
 		);
 
-		// Get all child links and check the aria-current value
-		const childLinks = mockData.currentPageNoChildrenTree[0].childLinks;
-
-		// Check the aria-current value for each child link
-		childLinks.forEach((link) => {
-			if (link.slug === mockData.slug) {
-				// The current link should have aria-current="true"
-				expect(link).toHaveAttribute("aria-current", "true");
-			} else {
-				// Other links should have aria-current="false"
-				expect(link).toHaveAttribute("aria-current", "false");
-			}
-		});
+		const link = screen.getByRole("link", { name: /smoking cessation/i });
+		expect(link).toHaveAttribute("aria-current", "true");
 	});
+
+	it("should not highlight the child link if the slug doesn't match", () => {
+		render(
+			<StoryblokSectionNav
+				tree={mockData.currentPageNoChildrenTree}
+				slug={
+					"implementing-nice-guidance/cost-saving-resource-planning-and-audit/nice-and-health-inequalities/nice-and-core20plus5-adults/nice-and-core20plus5-adults"
+				}
+			/>
+		);
+
+		const link = screen.getByRole("link", { name: /smoking cessation/i });
+		console.log(link.outerHTML)
+		expect(link).toHaveAttribute("aria-current", "false");
+	});
+
 });
