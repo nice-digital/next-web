9f864ba78870fc8b70bf7e75adeaa15e2982b125
Dale H <dale.hudson@nice.org.uk>
2025-05-09 09:26:51 +0100

    NXT-601 fix lint warn

--- web/src/utils/storyblok.ts
+++ web/src/utils/storyblok.ts
@@ -45,8 +45,7 @@ export const newsTypes = {
 
 // Are we using the Ocelot cache?
 // If not, then we can assume we're not in production and can just request the latest version of the content
-export const usingOcelotCache =
-	!!publicRuntimeConfig.storyblok.ocelotEndpoint;
+export const usingOcelotCache = !!publicRuntimeConfig.storyblok.ocelotEndpoint;
 
 // Default podcast image
 export const defaultPodcastImage =
