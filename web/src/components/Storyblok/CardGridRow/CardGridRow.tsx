7688798725b2b5dfe139bb7a076d95e79ee9b677
Becca Dawson <rebecca.dawson@nice.org.uk>
2025-05-19 11:56:13 +0100

    NXT-536 Create generic CardGridRow component for rendering rows without being part of parent component

---
+++ web/src/components/Storyblok/CardGridRow/CardGridRow.tsx
@@ -0,0 +1,27 @@
+import {
+	CardGridRowBasicStoryblok,
+	CardGridRowCalloutStoryblok,
+	CardGridRowCalloutWithImageStoryblok,
+} from "@/types/storyblok";
+
+import { CardGrid } from "../CardGrid/CardGrid";
+
+export type CardGridRow =
+	| CardGridRowBasicStoryblok
+	| CardGridRowCalloutStoryblok
+	| CardGridRowCalloutWithImageStoryblok;
+
+export interface CardGridRowProps {
+	blok: CardGridRow;
+}
+
+export const CardGridRow: React.FC<CardGridRowProps> = ({
+	blok,
+}: CardGridRowProps) => {
+
+	return (
+		<>
+			<CardGrid row={blok as CardGridRow} />
+		</>
+	);
+};
