6ebfbb6df31811e3420afcedb2b9b6a696c0602e
John Davey <john.davey@nice.org.uk>
2025-05-22 07:53:42 +0100

    NXT-622 Trying dynamic imports - some hydration errors - wip
    Co-authored-by: Dale Hudson <dalecreativ@users.noreply.github.com>
    Co-authored-by: Shilpa Unni <shilpanice@users.noreply.github.com>
    Co-authored-by: beccadawson <beccadawson@users.noreply.github.com>

--- web/src/components/Storyblok/CorporateContentPage/CorporateContentpage.tsx
+++ web/src/components/Storyblok/CorporateContentPage/CorporateContentpage.tsx
@@ -5,39 +5,22 @@ import {
 import { NextSeo } from "next-seo";
 import React, { useMemo } from "react";
 
-import { ErrorPageContent } from "@/components/ErrorPageContent/ErrorPageContent";
-import { BasicCardGrid } from "@/components/Storyblok/BasicCardGrid/BasicCardGrid";
-import { Blockquote } from "@/components/Storyblok/Blockquote/Blockquote";
-import { CardGridSection } from "@/components/Storyblok/CardGridSection/CardGridSection";
-import { CardListSection } from "@/components/Storyblok/CardListSection/CardListSection";
-import { CategoryLandingPage } from "@/components/Storyblok/CategoryLandingPage/CategoryLandingPage";
-import { CategoryNavigation } from "@/components/Storyblok/CategoryNavigation/CategoryNavigation";
-import { InfoPage } from "@/components/Storyblok/InfoPage/InfoPage";
-import { Metadata } from "@/components/Storyblok/Metadata/Metadata";
-import { NestedRichText } from "@/components/Storyblok/NestedRichText/NestedRichText";
-import { PromoBox } from "@/components/Storyblok/PromoBox/PromoBox";
-import { StoryblokAccordion } from "@/components/Storyblok/StoryblokAccordion/StoryblokAccordion";
-import { StoryblokAccordionGroup } from "@/components/Storyblok/StoryblokAccordionGroup/StoryblokAccordionGroup";
-import { StoryblokActionBannerDefault } from "@/components/Storyblok/StoryblokActionBanner/StoryblokActionBannerDefault";
-import { StoryblokActionBannerFullWidth } from "@/components/Storyblok/StoryblokActionBanner/StoryblokActionBannerFullWidth";
-import { StoryblokCalloutCard } from "@/components/Storyblok/StoryblokCalloutCard/StoryblokCalloutCard";
-import { StoryblokHero } from "@/components/Storyblok/StoryblokHero/StoryblokHero";
-import { StoryblokIframe } from "@/components/Storyblok/StoryblokIframe/StoryblokIframe";
-import { StoryblokPageHeader } from "@/components/Storyblok/StoryblokPageHeader/StoryblokPageHeader";
-import { StoryblokTable } from "@/components/Storyblok/StoryblokTable/StoryblokTable";
-import { StoryblokTestimonialFullWidth } from "@/components/Storyblok/StoryblokTestimonialFullWidth/StoryblokTestimonialFullWidth";
-import { StoryblokTestimonialGridItem } from "@/components/Storyblok/StoryblokTestimonialGridItem/StoryblokTestimonialGridItem";
-import { StoryblokYoutubeEmbed } from "@/components/Storyblok/StoryblokYoutubeEmbed/StoryblokYoutubeEmbed";
+
 import { logger } from "@/logger";
 import {
 	getAdditionalMetaTags,
 } from "@/utils/storyblok";
 
 import { SlugCatchAllProps } from "@/types/SBCorporateContent";
+import dynamic from "next/dynamic";
+
+
 
 export default function CorporateContentPageTemplate(
 	props: SlugCatchAllProps
 ): React.ReactElement {
+
+
 	const story = "story" in props ? props.story : null;
 
 	const additionalMetaTags = useMemo(() => {
@@ -56,40 +39,133 @@ export default function CorporateContentPageTemplate(
 		return <ErrorPageContent title="Error" heading={error} />;
 	}
 
-	const { story: storyData, breadcrumbs, siblingPages, component } = props;
+	// const { story: storyData, breadcrumbs, siblingPages, component } = props;
 
-	const commonComponents = {
-		cardGrid: BasicCardGrid,
-		metadata: Metadata,
-		pageHeader: StoryblokPageHeader,
+	// const commonComponents = {
+	// 	cardGrid: BasicCardGrid,
+	// 	metadata: Metadata,
+	// 	pageHeader: StoryblokPageHeader,
+	// };
+
+	// //TODO: add the rest of the components as we iterate through the page build
+	// const categoryLandingPageComponents = {
+	// 	categoryLandingPage: CategoryLandingPage,
+	// 	hero: StoryblokHero,
+	// 	actionBanner: StoryblokActionBannerFullWidth,
+	// 	actionBannerDefault: StoryblokActionBannerDefault,
+	// 	cardGridSection: CardGridSection,
+	// 	cardListSection: CardListSection,
+	// 	promoBox: PromoBox,
+	// 	calloutCard: StoryblokCalloutCard,
+	// 	calloutCardWithImage: StoryblokCalloutCard,
+	// 	testimonialFullWidth: StoryblokTestimonialFullWidth,
+	// 	testimonialGridItem: StoryblokTestimonialGridItem,
+	// };
+
+	// const infoPageComponents = {
+	// 	accordion: StoryblokAccordion,
+	// 	accordionGroup: StoryblokAccordionGroup,
+	// 	hero: StoryblokHero,
+	// 	iframe: StoryblokIframe,
+	// 	infoPage: InfoPage,
+	// 	nestedRichText: NestedRichText,
+	// 	quote: Blockquote,
+	// 	youtubeEmbed: StoryblokYoutubeEmbed,
+	// 	actionBannerDefault: StoryblokActionBannerDefault,
+	// 	nestedTable: StoryblokTable,
+	// };
+
+	// const components = {
+	// 	...commonComponents,
+	// 	...(component === "infoPage"
+	// 		? infoPageComponents
+	// 		: component === "categoryLandingPage"
+	// 		? categoryLandingPageComponents
+	// 		: { categoryNavigation: CategoryNavigation }),
+	// };
+
+    const { story: storyData, breadcrumbs, siblingPages, component } = props;
+
+    const commonComponents = {
+		cardGrid: dynamic(() =>
+			import(/* webpackChunkName: "common-card-grid" */ "@/components/Storyblok/BasicCardGrid/BasicCardGrid").then((mod) => mod.BasicCardGrid)
+		),
+		metadata: dynamic(() =>
+			import(/* webpackChunkName: "common-metadata" */ "@/components/Storyblok/Metadata/Metadata").then((mod) => mod.Metadata)
+		),
+		pageHeader: dynamic(() =>
+			import(/* webpackChunkName: "common-page-header" */ "@/components/Storyblok/StoryblokPageHeader/StoryblokPageHeader").then((mod) => mod.StoryblokPageHeader)
+		),
 	};
 
 	//TODO: add the rest of the components as we iterate through the page build
 	const categoryLandingPageComponents = {
-		categoryLandingPage: CategoryLandingPage,
-		hero: StoryblokHero,
-		actionBanner: StoryblokActionBannerFullWidth,
-		actionBannerDefault: StoryblokActionBannerDefault,
-		cardGridSection: CardGridSection,
-		cardListSection: CardListSection,
-		promoBox: PromoBox,
-		calloutCard: StoryblokCalloutCard,
-		calloutCardWithImage: StoryblokCalloutCard,
-		testimonialFullWidth: StoryblokTestimonialFullWidth,
-		testimonialGridItem: StoryblokTestimonialGridItem,
+		categoryLandingPage: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-page" */ "@/components/Storyblok/CategoryLandingPage/CategoryLandingPage").then((mod) => mod.CategoryLandingPage)
+		),
+		hero: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-hero" */ "@/components/Storyblok/StoryblokHero/StoryblokHero").then((mod) => mod.StoryblokHero)
+		),
+		actionBanner: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-action-banner" */ "@/components/Storyblok/StoryblokActionBanner/StoryblokActionBannerFullWidth").then((mod) => mod.StoryblokActionBannerFullWidth)
+		),
+		actionBannerDefault: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-action-banner-default" */ "@/components/Storyblok/StoryblokActionBanner/StoryblokActionBannerDefault").then((mod) => mod.StoryblokActionBannerDefault)
+		),
+		cardGridSection: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-card-grid-section" */ "@/components/Storyblok/CardGridSection/CardGridSection").then((mod) => mod.CardGridSection)
+		),
+		cardListSection: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-card-list-section" */ "@/components/Storyblok/CardListSection/CardListSection").then((mod) => mod.CardListSection)
+		),
+		promoBox: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-promo-box" */ "@/components/Storyblok/PromoBox/PromoBox").then((mod) => mod.PromoBox)
+		),
+		calloutCard: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-callout-card" */ "@/components/Storyblok/StoryblokCalloutCard/StoryblokCalloutCard").then((mod) => mod.StoryblokCalloutCard)
+		),
+		calloutCardWithImage: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-callout-card-with-image" */ "@/components/Storyblok/StoryblokCalloutCard/StoryblokCalloutCard").then((mod) => mod.StoryblokCalloutCard)
+		),
+		testimonialFullWidth: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-testimonial-full-width" */ "@/components/Storyblok/StoryblokTestimonialFullWidth/StoryblokTestimonialFullWidth").then((mod) => mod.StoryblokTestimonialFullWidth)
+		),
+		testimonialGridItem: dynamic(() =>
+			import(/* WebpackChunkName: "category-landing-testimonial-grid-item" */ "@/components/Storyblok/StoryblokTestimonialGridItem/StoryblokTestimonialGridItem").then((mod) => mod.StoryblokTestimonialGridItem)
+		),
 	};
 
 	const infoPageComponents = {
-		accordion: StoryblokAccordion,
-		accordionGroup: StoryblokAccordionGroup,
-		hero: StoryblokHero,
-		iframe: StoryblokIframe,
-		infoPage: InfoPage,
-		nestedRichText: NestedRichText,
-		quote: Blockquote,
-		youtubeEmbed: StoryblokYoutubeEmbed,
-		actionBannerDefault: StoryblokActionBannerDefault,
-		nestedTable: StoryblokTable,
+		accordion: dynamic(() =>
+			import(/* WebpackChunkName: "accordion" */ "@/components/Storyblok/StoryblokAccordion/StoryblokAccordion").then((mod) => mod.StoryblokAccordion)
+		),
+		accordionGroup: dynamic(() =>
+			import(/* WebpackChunkName: "accordion-group" */ "@/components/Storyblok/StoryblokAccordionGroup/StoryblokAccordionGroup").then((mod) => mod.StoryblokAccordionGroup)
+		),
+		hero: dynamic(() =>
+			import(/* WebpackChunkName: "hero" */ "@/components/Storyblok/StoryblokHero/StoryblokHero").then((mod) => mod.StoryblokHero)
+		),
+		iframe: dynamic(() =>
+			import(/* WebpackChunkName: "iframe" */ "@/components/Storyblok/StoryblokIframe/StoryblokIframe").then((mod) => mod.StoryblokIframe)
+		),
+		infoPage: dynamic(() =>
+			import(/* WebpackChunkName: "info-page" */ "@/components/Storyblok/InfoPage/InfoPage").then((mod) => mod.InfoPage)
+		),
+		nestedRichText: dynamic(() =>
+			import(/* WebpackChunkName: "nested-rich-text" */ "@/components/Storyblok/NestedRichText/NestedRichText").then((mod) => mod.NestedRichText)
+		),
+		quote: dynamic(() =>
+			import(/* WebpackChunkName: "blockquote" */ "@/components/Storyblok/Blockquote/Blockquote").then((mod) => mod.Blockquote)
+		),
+		youtubeEmbed: dynamic(() =>
+			import(/* WebpackChunkName: "youtube-embed" */ "@/components/Storyblok/StoryblokYoutubeEmbed/StoryblokYoutubeEmbed").then((mod) => mod.StoryblokYoutubeEmbed)
+		),
+		actionBannerDefault: dynamic(() =>
+			import(/* WebpackChunkName: "action-banner-default" */ "@/components/Storyblok/StoryblokActionBanner/StoryblokActionBannerDefault").then((mod) => mod.StoryblokActionBannerDefault)
+		),
+		nestedTable: dynamic(() =>
+			import(/* WebpackChunkName: "nested-table" */ "@/components/Storyblok/StoryblokTable/StoryblokTable").then((mod) => mod.StoryblokTable)
+		),
 	};
 
 	const components = {
@@ -98,7 +174,13 @@ export default function CorporateContentPageTemplate(
 			? infoPageComponents
 			: component === "categoryLandingPage"
 			? categoryLandingPageComponents
-			: { categoryNavigation: CategoryNavigation }),
+			: {
+					categoryNavigation: dynamic(() =>
+						import(
+							/* webpackChunkName: "category-navigation" */ "@/components/Storyblok/CategoryNavigation/CategoryNavigation"
+						).then((mod) => mod.CategoryNavigation)
+					),
+			  }),
 	};
 
 	setComponents(components);
