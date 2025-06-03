17fcd2733c94a823ed4418480046950540221899
Becca Dawson <rebecca.dawson@nice.org.uk>
2025-06-03 18:08:14 +0100

    NXT-544 linter fixes

--- web/src/components/Storyblok/InfoPage/InfoPage.test.tsx
+++ web/src/components/Storyblok/InfoPage/InfoPage.test.tsx
@@ -1,15 +1,11 @@
 import { render, screen } from "@testing-library/react";
 
-import sampleDataPageHeaderSectionNav from "@/mockData/storyblok/infoPageWithPageHeaderAndSectionNav.json";
 import sampleDataHeroInPageNav from "@/mockData/storyblok/infoPageWithHeroAndInPageNav.json";
 import sampleDataNoNav from "@/mockData/storyblok/infoPageWithNoNav.json";
-
+import sampleDataPageHeaderSectionNav from "@/mockData/storyblok/infoPageWithPageHeaderAndSectionNav.json";
 import { InfoPageStoryblok } from "@/types/storyblok";
 
-import {
-    InfoPage,
-    type InfoPageBlokProps
-} from "./InfoPage";
+import { InfoPage, type InfoPageBlokProps } from "./InfoPage";
 
 // test mocking the StoryblokComponent
 jest.mock("@storyblok/react", () => ({
@@ -23,29 +19,30 @@ jest.mock("@storyblok/react", () => ({
 	},
 }));
 
-const mockPageHeaderSectionNavData = sampleDataPageHeaderSectionNav.story.content;
+const mockPageHeaderSectionNavData =
+	sampleDataPageHeaderSectionNav.story.content;
 const mockHeroInPageNavData = sampleDataHeroInPageNav.story.content;
 const mockNoNavData = sampleDataNoNav.story.content;
 
 const mockPropsWithPageHeaderAndSectionNav: InfoPageBlokProps = {
-    blok: mockPageHeaderSectionNavData as InfoPageStoryblok,
-    breadcrumbs: sampleDataPageHeaderSectionNav.breadcrumbs,
-    tree: sampleDataPageHeaderSectionNav.tree,
-    slug: sampleDataPageHeaderSectionNav.slug
+	blok: mockPageHeaderSectionNavData as InfoPageStoryblok,
+	breadcrumbs: sampleDataPageHeaderSectionNav.breadcrumbs,
+	tree: sampleDataPageHeaderSectionNav.tree,
+	slug: sampleDataPageHeaderSectionNav.slug,
 };
 
 const mockPropsWithHeroAndInPageNav: InfoPageBlokProps = {
-    blok: mockHeroInPageNavData as InfoPageStoryblok,
-    breadcrumbs: sampleDataHeroInPageNav.breadcrumbs,
-    tree: sampleDataHeroInPageNav.tree,
-    slug: sampleDataHeroInPageNav.slug
+	blok: mockHeroInPageNavData as InfoPageStoryblok,
+	breadcrumbs: sampleDataHeroInPageNav.breadcrumbs,
+	tree: sampleDataHeroInPageNav.tree,
+	slug: sampleDataHeroInPageNav.slug,
 };
 
 const mockPropsWithNoNav: InfoPageBlokProps = {
-    blok: mockNoNavData as InfoPageStoryblok,
-    breadcrumbs: sampleDataNoNav.breadcrumbs,
-    tree: sampleDataNoNav.tree,
-    slug: sampleDataNoNav.slug
+	blok: mockNoNavData as InfoPageStoryblok,
+	breadcrumbs: sampleDataNoNav.breadcrumbs,
+	tree: sampleDataNoNav.tree,
+	slug: sampleDataNoNav.slug,
 };
 
 describe("InfoPage", () => {
@@ -53,60 +50,66 @@ describe("InfoPage", () => {
 		jest.clearAllMocks();
 	});
 
-    it("renders all metadata StoryblokComponents if present", () => {
-        render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
+	it("renders all metadata StoryblokComponents if present", () => {
+		render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
 		expect(
 			screen.getByText(mockPageHeaderSectionNavData.metadata[0].component)
 		).toBeInTheDocument();
-    });
+	});
 
-    it("renders the Page Header through the StoryblokComponent if present", () => {
-        render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
-        expect(
+	it("renders the Page Header through the StoryblokComponent if present", () => {
+		render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
+		expect(
 			screen.getByText(mockPageHeaderSectionNavData.header[0].component)
 		).toBeInTheDocument();
-    });
+	});
 
 	it("renders the Page Hero through the StoryblokComponent if present", () => {
-        render(<InfoPage {...mockPropsWithHeroAndInPageNav} />);
-        expect(
-			screen.getByTestId("storyblok-component-hero")
-		).toBeInTheDocument();
-    });
+		render(<InfoPage {...mockPropsWithHeroAndInPageNav} />);
+		expect(screen.getByTestId("storyblok-component-hero")).toBeInTheDocument();
+	});
 
 	it("handles missing metadata and header gracefully", () => {
-        const blok = { ...mockPropsWithHeroAndInPageNav.blok, metadata: undefined, header: undefined };
-        render(<InfoPage {...mockPropsWithHeroAndInPageNav} />);
-        expect(screen.getByTestId("storyblok-rich-text")).toBeInTheDocument();
-    });
+		const blok = {
+			...mockPropsWithHeroAndInPageNav.blok,
+			metadata: undefined,
+			header: undefined,
+		};
+		render(<InfoPage {...mockPropsWithHeroAndInPageNav} />);
+		expect(screen.getByTestId("storyblok-rich-text")).toBeInTheDocument();
+	});
 
 	it("renders content using StoryblokRichText", () => {
-        render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
-        expect(screen.getByTestId("storyblok-rich-text")).toBeInTheDocument();
-    });
+		render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
+		expect(screen.getByTestId("storyblok-rich-text")).toBeInTheDocument();
+	});
 
 	it("renders section nav when hideSectionNav is not 'true' and sectionNavIsPopulated(tree) is true", () => {
-        render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
-        expect(screen.getByTestId("section-nav")).toBeInTheDocument();
-        expect(screen.queryByText("On this page")).not.toBeInTheDocument(); //TODO: add more meaningful check
-    });
+		render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
+		expect(screen.getByTestId("section-nav")).toBeInTheDocument();
+		expect(screen.queryByText("On this page")).not.toBeInTheDocument(); //TODO: add more meaningful check
+	});
 
-    xit("renders in-page nav when hideSectionNav is 'true' and hideInPageNav is not 'true'", () => {
-		render(<InfoPage {...mockPropsWithHeroAndInPageNav}/>);
-        expect(screen.getByText("On this page")).toBeInTheDocument();
-        expect(screen.queryByTestId("section-nav")).not.toBeInTheDocument();
-    }); // TODO: mock in-page nav or figure out how to proxy it renders correctly
+	xit("renders in-page nav when hideSectionNav is 'true' and hideInPageNav is not 'true'", () => {
+		render(<InfoPage {...mockPropsWithHeroAndInPageNav} />);
+		expect(screen.getByText("On this page")).toBeInTheDocument();
+		expect(screen.queryByTestId("section-nav")).not.toBeInTheDocument();
+	}); // TODO: mock in-page nav or figure out how to proxy it renders correctly
 
-    it("does not render nav area when both hideSectionNav and hideInPageNav are 'true'", () => {
-        const blok = { ...mockPropsWithNoNav.blok };
-        render(<InfoPage {...mockPropsWithNoNav} />);
-        expect(screen.queryByTestId("info-page-nav-wrapper")).not.toBeInTheDocument();
-    });
-
-
+	it("does not render nav area when both hideSectionNav and hideInPageNav are 'true'", () => {
+		const blok = { ...mockPropsWithNoNav.blok };
+		render(<InfoPage {...mockPropsWithNoNav} />);
+		expect(
+			screen.queryByTestId("info-page-nav-wrapper")
+		).not.toBeInTheDocument();
+	});
 
 	xit("renders in-page nav when hideSectionNav is not 'true' but sectionNavIsPopulated(tree) is false and hideInPageNav is not 'true'", () => {
-		const blok = { ...mockPropsWithNoNav.blok, hideSectionNav: "false", hideInPageNav: "false" };
+		const blok = {
+			...mockPropsWithNoNav.blok,
+			hideSectionNav: "false",
+			hideInPageNav: "false",
+		};
 		render(<InfoPage {...mockPropsWithNoNav} tree={[]} />);
 		expect(screen.getByTestId("in-page-nav")).toBeInTheDocument();
 		expect(screen.queryByTestId("section-nav")).not.toBeInTheDocument();
@@ -130,18 +133,21 @@ describe("InfoPage", () => {
 		expect(screen.getByTestId("in-page-nav")).toBeInTheDocument();
 	});
 
+	it("handles empty blok object", () => {
+		const blok = {} as InfoPageStoryblok;
+		render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
+		expect(screen.getByTestId("section-nav")).toBeInTheDocument();
+	});
 
-
-    it("handles empty blok object", () => {
-        const blok = {} as InfoPageStoryblok;
-        render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} />);
-        expect(screen.getByTestId("section-nav")).toBeInTheDocument();
-    });
-
-    it("does not try to render section nav if tree undefined", () => {
-        const blok = { ...mockPropsWithPageHeaderAndSectionNav.blok};
-        render(<InfoPage {...mockPropsWithPageHeaderAndSectionNav} tree={undefined as any} />);
+	it("does not try to render section nav if tree undefined", () => {
+		const blok = { ...mockPropsWithPageHeaderAndSectionNav.blok };
+		render(
+			<InfoPage
+				{...mockPropsWithPageHeaderAndSectionNav}
+				tree={undefined as any}
+			/>
+		);
 		expect(screen.getByTestId("storyblok-rich-text")).toBeInTheDocument();
-        expect(screen.queryByTestId("section-nav")).not.toBeInTheDocument();
-    });
+		expect(screen.queryByTestId("section-nav")).not.toBeInTheDocument();
+	});
 });
