9983a86fad90154068f09cf08475c74660bcf334
John Davey <66066685+johndavey72@users.noreply.github.com>
2025-06-10 14:15:42 +0100

    NXT-630 Refactor catchall logic for reuse (#390)
    * NXT-622 Make getServerSideProps re-useable in catch all routes
    
    Co-authored-by: Dale Hudson <dalecreativ@users.noreply.github.com>
    Co-authored-by: beccadawson <beccadawson@users.noreply.github.com>
    Co-authored-by: Shilpa Unni <shilpanice@users.noreply.github.com>
    
    * NXT-622 Make catchAll route template re-useable
    
    Co-authored-by: Dale Hudson <dalecreativ@users.noreply.github.com>
    Co-authored-by: Shilpa Unni <shilpanice@users.noreply.github.com>
    Co-authored-by: beccadawson <beccadawson@users.noreply.github.com>
    
    * NXT-622 Relocate corp content gssp file. Rename route catch all functions.
    
    * NXT-622 Relocate types for corp content. Remove unused imports from catch all routes
    
    * NXT-622 Trying dynamic imports - some hydration errors - wip
    
    Co-authored-by: Dale Hudson <dalecreativ@users.noreply.github.com>
    Co-authored-by: Shilpa Unni <shilpanice@users.noreply.github.com>
    Co-authored-by: beccadawson <beccadawson@users.noreply.github.com>
    
    * NICE-502 Merge spike work. Move component registration into initStoryblok phase - import statically.
    
    * NICE-502 remove commented out code
    
    * NICE-502 Add 404 bail out logic back in for root catch all
    
    * NICE-502 Reinstate root catchAll feature flag. Use resolved url to derive basePath
    
    * NICE-502 Add custom header to id the root catch all template for debugging. Typescript wip
    
    * NICE-502 Make typescript generic for getCorporateContentGssp. Fix unit tests. Fix lint errors
    
    * NICE-502 Add unit tests for getCorporateContentGssp
    
    * NICE-502 improve unit test coverage for catch all template and add it for root catch all
    
    * NICE-502 Add punctuation to custom headers for clarity
    
    * NICE-502 Remove early return error logic from CorporateContentPage template to fix issue with static catchall routes erroring
    
    * NICE-502 update import name
    
    * NICE-502 Update import filename and make relative path in test to avoid linting issues
    
    * NICE-502 Update corporate content module name to resolve import woes
    
    * NICE-502 Change CorporateContentPage to named export and remove tricky lowercase filename
    
    * NICE-502 Switch to relative paths due to path aliasing issue/difference with linting on TC
    
    * NICE-502 Rename module to try to get paths to work correctly on TC
    
    * NICE-502 Add todo around hybrid static/dynamic imports for components. Add path aliasing back in for CorporateContent to see if it is causing build failulres.
    
    * NICE-502 linting fixes around import ordering
    
    * NICE-502 change name of static route catch all to be more descriptive
    
    * NXT-630 update response header logic for template identification
    
    * NXT-630 Add cardList component to initStoryblok
    
    * NXT-630 Incorporate in section nav work into shared corporate content gssp
    
    * NXT-630 Add phase 1 migration routes
    
    * NXT-630 Add todo around buildTree mocking
    
    * NXT-630 Rename CorporateContent component to CorporateContentPage
    
    * NXT-630 Thin wrapper applied for static route folders, with shared template added. Runtime safe pattern added so enableRootCatchAll feature flag can still be mocked and unit tested
    
    * NXT-630 Remove TODO around path normalisation utility re-use.
    
    * NXT-630 Move common components into commonComponents object. Alphabetise.
    
    * NXT-630 Correct alphabetisation of infoPage components
    
    ---------
    
    Co-authored-by: Dale Hudson <dalecreativ@users.noreply.github.com>
    Co-authored-by: beccadawson <beccadawson@users.noreply.github.com>
    Co-authored-by: Shilpa Unni <shilpanice@users.noreply.github.com>

--- web/src/utils/initStoryblok.ts
+++ web/src/utils/initStoryblok.ts
@@ -1,7 +1,81 @@
-import { apiPlugin, storyblokInit } from "@storyblok/react";
+import {
+	apiPlugin,
+	SbReactComponentsMap,
+	storyblokInit,
+} from "@storyblok/react";
 
+import { BasicCardGrid } from "@/components/Storyblok/BasicCardGrid/BasicCardGrid";
+import { Blockquote } from "@/components/Storyblok/Blockquote/Blockquote";
+import { CardGridSection } from "@/components/Storyblok/CardGridSection/CardGridSection";
+import { CardList } from "@/components/Storyblok/CardList/CardList";
+import { CardListSection } from "@/components/Storyblok/CardListSection/CardListSection";
+import { CategoryLandingPage } from "@/components/Storyblok/CategoryLandingPage/CategoryLandingPage";
+import { CategoryNavigation } from "@/components/Storyblok/CategoryNavigation/CategoryNavigation";
+import { InfoPage } from "@/components/Storyblok/InfoPage/InfoPage";
+import { Metadata } from "@/components/Storyblok/Metadata/Metadata";
+import { NestedRichText } from "@/components/Storyblok/NestedRichText/NestedRichText";
+import { PromoBox } from "@/components/Storyblok/PromoBox/PromoBox";
+import { StoryblokAccordion } from "@/components/Storyblok/StoryblokAccordion/StoryblokAccordion";
+import { StoryblokAccordionGroup } from "@/components/Storyblok/StoryblokAccordionGroup/StoryblokAccordionGroup";
+import { StoryblokActionBannerDefault } from "@/components/Storyblok/StoryblokActionBanner/StoryblokActionBannerDefault";
+import { StoryblokActionBannerFullWidth } from "@/components/Storyblok/StoryblokActionBanner/StoryblokActionBannerFullWidth";
+import { StoryblokCalloutCard } from "@/components/Storyblok/StoryblokCalloutCard/StoryblokCalloutCard";
+import { StoryblokHero } from "@/components/Storyblok/StoryblokHero/StoryblokHero";
+import { StoryblokIframe } from "@/components/Storyblok/StoryblokIframe/StoryblokIframe";
+import { StoryblokImageRichText } from "@/components/Storyblok/StoryblokImageRichText/StoryblokImageRichText";
+import { StoryblokPageHeader } from "@/components/Storyblok/StoryblokPageHeader/StoryblokPageHeader";
+import { StoryblokTable } from "@/components/Storyblok/StoryblokTable/StoryblokTable";
+import { StoryblokTestimonialFullWidth } from "@/components/Storyblok/StoryblokTestimonialFullWidth/StoryblokTestimonialFullWidth";
+import { StoryblokTestimonialGridItem } from "@/components/Storyblok/StoryblokTestimonialGridItem/StoryblokTestimonialGridItem";
+import { StoryblokYoutubeEmbed } from "@/components/Storyblok/StoryblokYoutubeEmbed/StoryblokYoutubeEmbed";
 import { publicRuntimeConfig } from "@/config";
 import { logger } from "@/logger";
+
+//TODO: consider a mix of static imports for common components and dynamic imports for lesser used components
+const commonComponents = {
+	accordion: StoryblokAccordion,
+	accordionGroup: StoryblokAccordionGroup,
+	actionBannerDefault: StoryblokActionBannerDefault,
+	cardGrid: BasicCardGrid,
+	cardList: CardList,
+	hero: StoryblokHero,
+	metadata: Metadata,
+	nestedRichText: NestedRichText,
+	pageHeader: StoryblokPageHeader,
+	quote: Blockquote,
+	youtubeEmbed: StoryblokYoutubeEmbed,
+};
+
+//TODO: add the rest of the components as we iterate through the page build
+const categoryLandingPageComponents = {
+	actionBanner: StoryblokActionBannerFullWidth,
+	calloutCard: StoryblokCalloutCard,
+	calloutCardWithImage: StoryblokCalloutCard,
+	cardGridSection: CardGridSection,
+	cardListSection: CardListSection,
+	categoryLandingPage: CategoryLandingPage,
+	promoBox: PromoBox,
+	testimonialFullWidth: StoryblokTestimonialFullWidth,
+	testimonialGridItem: StoryblokTestimonialGridItem,
+};
+
+const infoPageComponents = {
+	iframe: StoryblokIframe,
+	imageRichText: StoryblokImageRichText,
+	infoPage: InfoPage,
+	nestedTable: StoryblokTable,
+};
+
+const categoryNavigationPageComponents = {
+	categoryNavigation: CategoryNavigation,
+};
+
+export const allComponents: SbReactComponentsMap = {
+	...commonComponents,
+	...infoPageComponents,
+	...categoryLandingPageComponents,
+	...categoryNavigationPageComponents,
+};
 
 // Init connection to Storyblok
 export const initStoryblok = (): void => {
@@ -19,6 +93,7 @@ export const initStoryblok = (): void => {
 		storyblokInit({
 			accessToken,
 			use: [apiPlugin],
+			components: allComponents,
 			apiOptions: {
 				cache: {
 					clear: usingOcelotCache ? "manual" : "auto",
