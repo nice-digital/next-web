986b395542965ae1721bfad8e683b1c2212a6339
Becca Dawson <rebecca.dawson@nice.org.uk>
2025-06-17 18:43:39 +0100

    NXT-645 lint fixes

--- web/next-sitemap.config.js
+++ web/next-sitemap.config.js
@@ -1,8 +1,8 @@
 const config = require("config");
 
-const isTeamCity = !!process.env.TEAMCITY_VERSION;
-
-const siteUrl = `${config.get("public.baseURL")}${config.get("public.publicBaseURL")}`; //TODO check if public.[public]BaseURL can be used in all envs (needs deploying to check); octo var removed in https://nicedigital.atlassian.net/browse/NXT-375
+const siteUrl = `${config.get("public.baseURL")}${config.get(
+	"public.publicBaseURL"
+)}`; //TODO check if public.[public]BaseURL can be used in all envs (needs deploying to check); octo var removed in https://nicedigital.atlassian.net/browse/NXT-375
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
@@ -10,12 +10,7 @@ module.exports = {
 	generateIndexSitemap: false,
 	generateRobotsTxt: false,
 	sitemapBaseFileName: "sitemap-0",
-	exclude: [
-		"/guidance/errortest",
-		"/status",
-		"/search",
-		"/sitemap-next.xml",
-	],
+	exclude: ["/guidance/errortest", "/status", "/search", "/sitemap-next.xml"],
 	transform: (config, loc) => ({
 		// Strip out changefreq, priority, lastmod etc:
 		// we either don't need them or don't know what the values are
