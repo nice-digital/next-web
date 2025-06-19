86ced6aeeadb764d47b7dd154fae99ba388f50e2
beccadawson <rebecca.dawson@nice.org.uk>
2025-06-19 12:09:16 +0100

    NXT-645-update-sitemaps-with-new-ur-ls-of-pages-included-in-phase-2-test-launch (#402)
    * NXT-654 convert next web sitemap function to use array of paths rather than single static news path
    
    * NXT-645 lint fixes
    
    * NXT-645 manually remove Test Phase Migration pages from sitemap-static.xml
    And related pages that are being decommed
    
    * Add new storyblok routes to list of pages to be excluded from next dynamic sitemap
    
    * NXT-645 fix XML syntax error in sitemap and add validator to project's recommended extensions
    
    * NXT-645 remove storyblok folders and exclusions for static news/* routes from dynamic NextJS sitemap
    
    * NXT-645 Remove broken variable replacement for siteURL from dynamic sitemap in favour of always using value from config
    
    * NXT-645 lint fixes
    
    * NXT-645 move filteredLinks logic out of generateSitemap function so it can be called with final version of array
    
    * NXT-645 lint fixes (just a new line? seriously?? :| )
    
    * NXT-645 remove trainling slash from root page urls in sitemap as these redirect to non-trailing-slash versions on load
    
    * NXT-645 fix domain in dynamic next sitemap and update TODO regarding config vars
    
    * NXT-645 remove duplicate urls from sitemap-0.xml
    
    * NXT-645 add clarifying comments to new sitemap exclusions

--- web/src/utils/storyblok.ts
+++ web/src/utils/storyblok.ts
@@ -312,7 +312,6 @@ export const fetchStories = async <T>(
 export const fetchLinks = async (
 	sbParams: ISbStoriesParams
 ): Promise<SBLink[]> => {
-	// const {version = "published", per_page = 1000} = sbParams;
 	const storyblokApi = getStoryblokApi();
 	const cacheVersion = await fetchCacheVersion();
 	const finalParams = {
