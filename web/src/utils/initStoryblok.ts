d5275730e397ddfaef58304c17b5b417da55df87
Shilpa Unni <shilpa.unni@nice.org.uk>
2025-06-24 14:19:38 +0100

    NXT-671 Console error log checks (#409)
    * NXT-671 move all setComponent calls into initStoryblok to test fixing hydration error
    
    * NXT-671 remove unused component imports and setComponent calls from home & news pages
    
    * NXT-671 combine new component imports with existing imports in initStoryblok and remove duplication from commonComponents (and alphebetise (: )
    
    * NXT-671 tidy up remaining commented setComponents code
    
    * NXT-671 Stub out storyblok component for snapshot testing as the old unit test snapshot won't render content due to behaviour of storyblok init in conjunction with jest
    
    * NXT-671 Refactor Storyblok component mocks in tests to improve snapshot consistency and clarity
    
    ---------
    
    Co-authored-by: Becca Dawson <rebecca.dawson@nice.org.uk>
    Co-authored-by: John Davey <john.davey@nice.org.uk>

--- web/src/utils/initStoryblok.ts
+++ web/src/utils/initStoryblok.ts
@@ -12,19 +12,26 @@ import { CardList } from "@/components/Storyblok/CardList/CardList";
 import { CardListSection } from "@/components/Storyblok/CardListSection/CardListSection";
 import { CategoryLandingPage } from "@/components/Storyblok/CategoryLandingPage/CategoryLandingPage";
 import { CategoryNavigation } from "@/components/Storyblok/CategoryNavigation/CategoryNavigation";
+import { Homepage } from "@/components/Storyblok/Homepage/Homepage";
+import { HomepageHero } from "@/components/Storyblok/Homepage/HomepageHero/HomepageHero";
 import { InfoPage } from "@/components/Storyblok/InfoPage/InfoPage";
 import { Metadata } from "@/components/Storyblok/Metadata/Metadata";
 import { NestedRichText } from "@/components/Storyblok/NestedRichText/NestedRichText";
 import { PromoBox } from "@/components/Storyblok/PromoBox/PromoBox";
+import { Spotlight } from "@/components/Storyblok/Spotlight/Spotlight";
 import { StoryblokAccordion } from "@/components/Storyblok/StoryblokAccordion/StoryblokAccordion";
 import { StoryblokAccordionGroup } from "@/components/Storyblok/StoryblokAccordionGroup/StoryblokAccordionGroup";
 import { StoryblokActionBannerDefault } from "@/components/Storyblok/StoryblokActionBanner/StoryblokActionBannerDefault";
 import { StoryblokActionBannerFullWidth } from "@/components/Storyblok/StoryblokActionBanner/StoryblokActionBannerFullWidth";
+import { StoryblokBlogPost } from "@/components/Storyblok/StoryblokBlogPost/StoryblokBlogPost";
 import { StoryblokCalloutCard } from "@/components/Storyblok/StoryblokCalloutCard/StoryblokCalloutCard";
 import { StoryblokHero } from "@/components/Storyblok/StoryblokHero/StoryblokHero";
 import { StoryblokIframe } from "@/components/Storyblok/StoryblokIframe/StoryblokIframe";
 import { StoryblokImageRichText } from "@/components/Storyblok/StoryblokImageRichText/StoryblokImageRichText";
+import { StoryblokNewsArticle } from "@/components/Storyblok/StoryblokNewsArticle/StoryblokNewsArticle";
 import { StoryblokPageHeader } from "@/components/Storyblok/StoryblokPageHeader/StoryblokPageHeader";
+import { StoryblokRelatedLink } from "@/components/Storyblok/StoryblokRelatedLink/StoryblokRelatedLink";
+import { StoryblokRelatedNewsLink } from "@/components/Storyblok/StoryblokRelatedNewsLink/StoryblokRelatedNewsLink";
 import { StoryblokTable } from "@/components/Storyblok/StoryblokTable/StoryblokTable";
 import { StoryblokTestimonialFullWidth } from "@/components/Storyblok/StoryblokTestimonialFullWidth/StoryblokTestimonialFullWidth";
 import { StoryblokTestimonialGridItem } from "@/components/Storyblok/StoryblokTestimonialGridItem/StoryblokTestimonialGridItem";
@@ -40,6 +47,7 @@ const commonComponents = {
 	cardGrid: BasicCardGrid,
 	cardList: CardList,
 	hero: StoryblokHero,
+	iframe: StoryblokIframe,
 	metadata: Metadata,
 	nestedRichText: NestedRichText,
 	pageHeader: StoryblokPageHeader,
@@ -47,7 +55,16 @@ const commonComponents = {
 	youtubeEmbed: StoryblokYoutubeEmbed,
 };
 
-//TODO: add the rest of the components as we iterate through the page build
+const articleComponents = {
+	newsArticle: StoryblokNewsArticle,
+	relatedLink: StoryblokRelatedLink,
+	relatedNewsLink: StoryblokRelatedNewsLink,
+};
+
+const blogComponents = {
+	blogPost: StoryblokBlogPost,
+};
+
 const categoryLandingPageComponents = {
 	actionBanner: StoryblokActionBannerFullWidth,
 	calloutCard: StoryblokCalloutCard,
@@ -60,25 +77,38 @@ const categoryLandingPageComponents = {
 	testimonialGridItem: StoryblokTestimonialGridItem,
 };
 
+const categoryNavigationPageComponents = {
+	categoryNavigation: CategoryNavigation,
+};
+
+// TODO make homepage component names consistent with common component names
+const homepageComponents = {
+	basicCardGrid: BasicCardGrid,
+	homepage: Homepage,
+	homepageHero: HomepageHero,
+	promobox: PromoBox,
+	spotlight: Spotlight,
+	storyblokActionBannerFullWidth: StoryblokActionBannerFullWidth,
+	storyblokHero: StoryblokHero,
+};
+
 const infoPageComponents = {
 	cardGridRowBasic: CardGridRow,
 	cardGridRowCallout: CardGridRow,
 	cardGridRowCalloutWithImage: CardGridRow,
-	iframe: StoryblokIframe,
 	imageRichText: StoryblokImageRichText,
 	infoPage: InfoPage,
 	nestedTable: StoryblokTable,
 };
 
-const categoryNavigationPageComponents = {
-	categoryNavigation: CategoryNavigation,
-};
-
 export const allComponents: SbReactComponentsMap = {
 	...commonComponents,
-	...infoPageComponents,
+	...articleComponents,
+	...blogComponents,
 	...categoryLandingPageComponents,
 	...categoryNavigationPageComponents,
+	...homepageComponents,
+	...infoPageComponents,
 };
 
 // Init connection to Storyblok
