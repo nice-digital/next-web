27b0bdf48ef5c036342495d53bafafda20ecdb1f
Becca Dawson <rebecca.dawson@nice.org.uk>
2025-08-11 12:29:35 +0100

    NXT-658 add updated content to Candidates for Modular Updates form

--- web/src/pages/forms/candidates-for-modular-updates.page.tsx
+++ web/src/pages/forms/candidates-for-modular-updates.page.tsx
@@ -11,9 +11,10 @@ const FormHeader = () => (
 		<p>
 			Tell us about a modular update that you think should be considered by
 			NICE. Please do not include any confidential information in your
-			responses. For information on how personal data are used, see NICE&apos;s
-			privacy notice. If your organisation or professional association is being
-			engaged with separately then there is no need to complete this form.
+			responses. For information on how personal data is used, see{" "}
+			<a href="/privacy-notice">NICE&apos;s privacy notice</a>. If your
+			organisation or professional association is being engaged with separately
+			then there is no need to complete this form.
 		</p>
 		<p>
 			You will not receive a formal response to your submission but we may get
@@ -21,17 +22,37 @@ const FormHeader = () => (
 			addition to considering your suggestion for a modular update, NICE will
 			also consider whether it may be more suitable as the topic of an
 			exploratory project led by the NICE Health Technology Assessment
-			Innovation Laboratory (HTA Lab). For example, this could happen when: the
-			suggested method is particularly novel and may still be maturing; there is
-			at least one alternative new method that could also be considered
-			alongside your suggestion; or the method appears promising but has not yet
-			been tested in the appropriate context. For more information and an
-			overview of current and past HTA Lab projects.
+			Innovation Laboratory (HTA Lab). For example, this could happen when:
 		</p>
+		<ul>
+			<li>
+				the suggested method is particularly novel and may still be maturing
+			</li>
+			<li>
+				there is at least one alternative new method that could also be
+				considered alongside your suggestion
+			</li>
+			<li>
+				the method appears promising but has not yet been tested in the
+				appropriate context.
+			</li>
+		</ul>
+
+		<p>
+			For more information and an overview visit the{" "}
+			<a href="/what-nice-does/our-research-work/hta-lab/hta-lab-projects">
+				HTA Lab projects page
+			</a>
+			.
+		</p>
+
 		<p>
 			The outcome of the modular updates selection process and information about
-			modular updates in progress will be communicated on the modular update
-			page.
+			modular updates in progress will be communicated on the{" "}
+			<a href="/what-nice-does/our-guidance/modular-updates">
+				modular update page
+			</a>
+			.
 		</p>
 		<p>By submitting this form you agree to be contacted by NICE.</p>
 		<p>Fields marked with an asterisk (*) are mandatory.</p>
