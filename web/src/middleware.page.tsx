9bdda870d67a29bbbf9df2f14399adfeef4bc2fd
John Davey <john.davey@nice.org.uk>
2025-08-19 10:58:32 +0100

    NXT-743 Remove Edge logger debug from middleware

--- web/src/middleware.page.tsx
+++ web/src/middleware.page.tsx
@@ -1,10 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-export const logger = {
-	info: (...args: unknown[]): void => console.info("[Edge]", ...args),
-	warn: (...args: unknown[]): void => console.warn("[Edge]", ...args),
-	error: (...args: unknown[]): void => console.error("[Edge]", ...args),
-};
 
 const SKIP_REGEXES: RegExp[] = [
 	/^\/_next\//,
@@ -26,10 +21,6 @@ export function middleware(req: NextRequest): NextResponse {
 	const url = req.nextUrl.clone();
 	const pathname = url.pathname;
 
-	logger.warn(`Request ###########: ${JSON.stringify(req)} ###########`);
-	logger.warn(`Request URL ###########: ${url} ###########`);
-	logger.warn(`Request pathname ###########: ${pathname} ###########`);
-
 	// Skip static/asset/manifest/api/internal requests
 	if (shouldSkip(pathname)) {
 		return NextResponse.next();
@@ -37,10 +28,6 @@ export function middleware(req: NextRequest): NextResponse {
 
 	// If the path is already lowercase, do nothing
 	if (pathname === pathname.toLowerCase()) {
-		logger.warn(`url ########### ${url} ###########`);
-		logger.warn(
-			`Request pathname is already lowercase ###########: ${pathname} ###########`
-		);
 		return NextResponse.next();
 	}
 
@@ -49,8 +36,6 @@ export function middleware(req: NextRequest): NextResponse {
 		url.origin + pathname.toLowerCase() + url.search + url.hash
 	);
 
-	// const isProduction = process.env.NODE_ENV === "production";
-
 	// Only replace localhost:3000 with production domain for mixed-case redirects
 	if (redirectURL.hostname === "localhost") {
 		redirectURL.hostname = "www.nice.org.uk";
@@ -58,9 +43,6 @@ export function middleware(req: NextRequest): NextResponse {
 		redirectURL.port = "";
 	}
 
-	logger.warn(
-		`Redirecting to lowercase pathname ###########: ${redirectURL} ###########`
-	);
 	return NextResponse.redirect(redirectURL, 308);
 }
 
