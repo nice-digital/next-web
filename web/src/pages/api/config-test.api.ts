57999600af0d736da2203c3185c158b558a155af
John Davey <john.davey@nice.org.uk>
2025-08-20 17:44:12 +0100

    NXT-757 fix remaining lint issues

--- web/src/pages/api/config-test.api.ts
+++ web/src/pages/api/config-test.api.ts
@@ -1,25 +1,41 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { serverRuntimeConfig, publicRuntimeConfig } from "@/config";
+import type { ServerConfig } from "@/config";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+interface ConfigTestResponse {
+	message: string;
+	serverConfigKeys: string[];
+	publicConfigKeys: string[];
+	serverFeeds: string[];
+	serverCache: string[];
+	jotFormConfigured: {
+		server: boolean;
+		public: boolean;
+	};
+}
+
+export default function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<ConfigTestResponse>
+): void {
 	// This API endpoint will help us test if server config loading works
-	const serverConfig = serverRuntimeConfig;
-	const publicConfig = publicRuntimeConfig;
+	const serverConfig = serverRuntimeConfig as ServerConfig;
+	const publicConfig = publicRuntimeConfig as unknown as Record<
+		string,
+		unknown
+	>;
 
 	res.status(200).json({
 		message: "Config test endpoint",
 		serverConfigKeys: Object.keys(serverConfig || {}),
 		publicConfigKeys: Object.keys(publicConfig || {}),
-		serverFeeds: (serverConfig as any)?.feeds
-			? Object.keys((serverConfig as any).feeds)
-			: [],
-		serverCache: (serverConfig as any)?.cache
-			? Object.keys((serverConfig as any).cache)
-			: [],
+		serverFeeds: serverConfig?.feeds ? Object.keys(serverConfig.feeds) : [],
+		serverCache: serverConfig?.cache ? Object.keys(serverConfig.cache) : [],
 		jotFormConfigured: {
-			server: !!(serverConfig as any)?.feeds?.jotForm?.apiKey,
-			public: !!(publicConfig as any)?.jotForm?.baseURL,
+			server: !!serverConfig?.feeds?.jotForm?.apiKey,
+			public: !!(publicConfig as { jotForm?: { baseURL?: string } })?.jotForm
+				?.baseURL,
 		},
 	});
 }
