bef6435f442001168daedc77e424fe7e8710068a
John Holland <john.holland@nice.org.uk>
2025-10-23 11:04:33 +0100

    NXT-734 Terminated and deferred tabs (#481)
    * NXT-734 Mock data for new galp tests
    
    * NXT-734 Added new terminated and deferred GALPs
    
    * NXT-734 Changed variable from search api
    
    * NXT-734 Updated column for guidance programme to type

---
+++ web/src/pages/guidance/deferred.page.tsx
@@ -0,0 +1,88 @@
+import { Breadcrumb } from "@nice-digital/nds-breadcrumbs";
+import { Document, SortOrder } from "@nice-digital/search-client";
+
+import { GuidanceListNav } from "@/components/ProductListNav/GuidanceListNav";
+import {
+	getGetServerSidePropsFunc,
+	getProductListPage,
+} from "@/components/ProductListPage/ProductListPage";
+import { ResponsiveDate } from "@/components/ResponsiveDate/ResponsiveDate";
+import { publicRuntimeConfig } from "@/config";
+
+const defaultSortOrder = SortOrder.dateDescending,
+	dateFilterLabel = "Deferred date",
+	textFilterHeading = "Keyword or reference number",
+	textFilterLabel = "Keyword or reference number";
+
+const tableBodyRender = (documents: Document[]) => (
+	<>
+		<caption className="visually-hidden">Deferred</caption>
+		<thead>
+			<tr>
+				<th scope="col">Title</th>
+				<th scope="col">Type</th>
+				<th scope="col">Deferred date</th>
+			</tr>
+		</thead>
+		<tbody>
+			{documents.map(
+				({ id, title, pathAndQuery, niceResultType, deferredDate }) => {
+					return (
+						<tr key={id}>
+							<td>
+								<a
+									href={publicRuntimeConfig.baseURL + pathAndQuery}
+									dangerouslySetInnerHTML={{ __html: title }}
+								/>
+							</td>
+							<td>{niceResultType || "n/a"}</td>
+							<td>
+								{deferredDate ? (
+									<ResponsiveDate isoDateTime={deferredDate} />
+								) : (
+									<abbr title="To be confirmed">TBC</abbr>
+								)}
+							</td>
+						</tr>
+					);
+				}
+			)}
+		</tbody>
+	</>
+);
+
+export default getProductListPage({
+	metaDescription: "Find out what guidance is being considered for development",
+	listNavType: GuidanceListNav,
+	breadcrumbTrail: [
+		<Breadcrumb to="/guidance" key="NICE guidance">
+			NICE guidance
+		</Breadcrumb>,
+	],
+	currentBreadcrumb: "Deferred",
+	preheading: "",
+	heading: "Deferred",
+	title: "Deferred | Guidance",
+	defaultSort: {
+		order: defaultSortOrder,
+		label: "Deferred date",
+	},
+	secondarySort: {
+		order: SortOrder.titleAscending,
+		label: "Title",
+	},
+	showDateFilter: true,
+	useFutureDates: false,
+	dateFilterLabel,
+	textFilterHeading,
+	tableBodyRender,
+	searchInputPlaceholder: "E.g. 'diabetes' or 'NG28'",
+});
+
+export const getServerSideProps = getGetServerSidePropsFunc({
+	gstPreFilter: "Deferred",
+	defaultSortOrder,
+	dateFilterLabel,
+	textFilterLabel,
+	index: "guidance",
+});
