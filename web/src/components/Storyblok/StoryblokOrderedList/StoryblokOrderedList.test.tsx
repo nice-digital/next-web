745045b1e75702fac2142282c12f7684ad0cf977
Shilpa Unni <shilpa.unni@nice.org.uk>
2025-10-27 16:26:58 +0000

    NXT-499  update unit tests to cover default start and type attribute behavior

--- web/src/components/Storyblok/StoryblokOrderedList/StoryblokOrderedList.test.tsx
+++ web/src/components/Storyblok/StoryblokOrderedList/StoryblokOrderedList.test.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { StoryblokOrderedList } from "./StoryblokOrderedList";
-import orderedList from "@/mocks/__data__/storyblok/orderedList.json";
+import {
+	orderedList,
+	orderedListWithoutStartingNumber,
+} from "@/mocks/__data__/storyblok/orderedList.js";
 import { OrderedListStoryblok } from "@/types/storyblok";
 
 jest.mock("../StoryblokRichText/StoryblokRichText", () => ({
@@ -12,14 +15,13 @@ jest.mock("../StoryblokRichText/StoryblokRichText", () => ({
 }));
 
 describe("OrderedList", () => {
+	it("should render the ordered list div", () => {
+		render(<StoryblokOrderedList blok={orderedList as OrderedListStoryblok} />);
+		const orderedLists = screen.getAllByTestId("ordered-list");
+		expect(orderedLists).toHaveLength(2);
+	});
 	it("should render the ordered list with correct start and type attributes", () => {
-		render(
-			<StoryblokOrderedList
-				blok={orderedList as OrderedListStoryblok}
-				component={"orderedList"}
-				_uid={""}
-			/>
-		);
+		render(<StoryblokOrderedList blok={orderedList as OrderedListStoryblok} />);
 		const olElements = screen.getAllByRole("list");
 		const topLevelList = olElements[0];
 		expect(topLevelList).toHaveAttribute("start", "1");
@@ -27,25 +29,13 @@ describe("OrderedList", () => {
 	});
 
 	it("should render the correct number of list items", () => {
-		render(
-			<StoryblokOrderedList
-				blok={orderedList as OrderedListStoryblok}
-				component={"orderedList"}
-				_uid={""}
-			/>
-		);
+		render(<StoryblokOrderedList blok={orderedList as OrderedListStoryblok} />);
 		const listItems = screen.getAllByRole("listitem");
 		expect(listItems).toHaveLength(2);
 	});
 
 	it("should render nested ordered list when children exist", () => {
-		render(
-			<StoryblokOrderedList
-				blok={orderedList as OrderedListStoryblok}
-				component={"orderedList"}
-				_uid={""}
-			/>
-		);
+		render(<StoryblokOrderedList blok={orderedList as OrderedListStoryblok} />);
 		const orderedLists = screen.getAllByRole("list");
 		expect(orderedLists).toHaveLength(2);
 		expect(orderedLists[1]).toHaveAttribute("start", "1");
@@ -53,15 +43,18 @@ describe("OrderedList", () => {
 	});
 
 	it("should render StoryblokRichText content correctly", () => {
-		render(
-			<StoryblokOrderedList
-				blok={orderedList as OrderedListStoryblok}
-				component={"orderedList"}
-				_uid={""}
-			/>
-		);
-
+		render(<StoryblokOrderedList blok={orderedList as OrderedListStoryblok} />);
 		expect(screen.getByText("test list item 1")).toBeInTheDocument();
 		expect(screen.getByText("nested list item of 1")).toBeInTheDocument();
+	});
+	it("should render correct start and type attributes when starting number and list type are empty", () => {
+		render(
+			<StoryblokOrderedList
+				blok={orderedListWithoutStartingNumber as OrderedListStoryblok}
+			/>
+		);
+		const topLevelList = screen.getByRole("list");
+		expect(topLevelList).toHaveAttribute("start", "1");
+		expect(topLevelList).toHaveAttribute("type", "1");
 	});
 });
--- web/src/components/Storyblok/StoryblokOrderedList/StoryblokOrderedList.tsx
+++ web/src/components/Storyblok/StoryblokOrderedList/StoryblokOrderedList.tsx
@@ -2,26 +2,35 @@ import React from "react";
 import { StoryblokRichText } from "../StoryblokRichText/StoryblokRichText";
 import { ListItemStoryblok, OrderedListStoryblok } from "@/types/storyblok";
 
-export const StoryblokOrderedList: React.FC<OrderedListStoryblok> = ({
-	blok
+export const StoryblokOrderedList: React.FC<{ blok: OrderedListStoryblok }> = ({
+	blok,
 }) => {
-	const start = Number(blok.startingNumber) || 1;
-	const type = blok.listType || "1";
+	const { startingNumber, listType, listItems } = blok;
+
+	const start = Number(startingNumber) || 1;
+	const type = listType || "1";
+
+	if (!listItems?.length) return null;
+
 	return (
-		<ol start={start} type={type}>
-			{blok.listItems?.map((item: ListItemStoryblok) => (
-				<ListItem blok={item} />
-			))}
-		</ol>
+		<div data-component="ordered-list" data-testid="ordered-list">
+			<ol start={start} type={type}>
+				{listItems.map((item) => (
+					<ListItem key={item._uid} blok={item} />
+				))}
+			</ol>
+		</div>
 	);
 };
 
-export const ListItem = ({ blok }: { blok: ListItemStoryblok }) => {
+const ListItem: React.FC<{ blok: ListItemStoryblok }> = ({ blok }) => {
+	const { text, children } = blok;
+
 	return (
 		<li>
-			<StoryblokRichText content={blok.text ?? { type: "doc", content: [] }} />
-			{blok?.children?.length && blok.children.length > 0 && (
-				<StoryblokOrderedList blok={blok.children[0]} component={"orderedList"} _uid={""} />
+			<StoryblokRichText content={text ?? { type: "doc", content: [] }} />
+			{Array.isArray(children) && children.length > 0 && (
+				<StoryblokOrderedList blok={children[0]} />
 			)}
 		</li>
 	);
