698efe88cf901982e5a335022e6e0231021b2e7b
John Holland <john.holland@nice.org.uk>
2025-10-31 17:44:39 +0000

    NXT-694 Tests for ConvertedDocument components

---
+++ web/src/components/ConvertedDocumentChapterMenu/ConvertedDocumentChapterMenu.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen, within } from "@testing-library/react";
+import { useRouter } from "next/router";
+
+import { ConvertedDocumentChapterMenu,
+	type ConvertedDocumentChapterMenuProps,
+} from "./ConvertedDocumentChapterMenu";
+
+describe("ConvertedDocumentChapterMenu", () => {
+	const props: ConvertedDocumentChapterMenuProps = {
+		ariaLabel: "Chapters",
+		chapters: [
+			{
+				href: "/guidance/IND999/documents/html-conversion/chapter/recommendations",
+				slug: "recommendations",
+				title: "1 Recommendations"
+			},
+			{
+				href: "/guidance/IND999/documents/html-conversion/chapter/the-technologies",
+				slug: "the-technologies",
+				title: "2 The technologies"
+			}
+		],
+	};
+
+	beforeEach(() => {
+		(useRouter as jest.Mock).mockReturnValue({
+			asPath: "/somewhere#somewhere",
+		});
+	});
+
+	it("should render labelled navigation wrapper", () => {
+		render(<ConvertedDocumentChapterMenu {...props} />);
+		expect(
+			screen.getByRole("navigation", { name: "Chapters" })
+		).toBeInTheDocument();
+	});
+
+	it("should render one linked menu item when there is only one chapter", () => {
+		let propsOneChapter = {...props};
+		propsOneChapter.chapters = [propsOneChapter.chapters[0]];
+
+		render(<ConvertedDocumentChapterMenu {...propsOneChapter} />);
+		const menuNav = screen.getByRole("navigation", { name: "Chapters" });
+		expect(menuNav).toBeInTheDocument();
+		expect(within(menuNav).getAllByRole("link")).toHaveLength(1);
+	});
+
+	it("should render two linked menu items when there are two chapters", () => {
+		render(<ConvertedDocumentChapterMenu {...props} />);
+		const menuNav = screen.getByRole("navigation", { name: "Chapters" });
+		expect(menuNav).toBeInTheDocument();
+		expect(within(menuNav).getAllByRole("link")).toHaveLength(2);
+	});
+});
---
+++ web/src/components/ConvertedDocumentPrevNext/ConvertedDocumentPrevNext.test.tsx
@@ -0,0 +1,71 @@
+import { render, screen } from "@testing-library/react";
+import { NextRouter, useRouter } from "next/router";
+
+import { ConvertedDocumentPrevNext,
+	type ConvertedDocumentPrevNextProps,
+} from "./ConvertedDocumentPrevNext";
+
+const mockRouterPath = (asPath: string) => {
+	jest.mocked(useRouter).mockReturnValue({
+		asPath,
+	} as NextRouter);
+};
+
+const indicatorSlug = "ind999-indicator-history-tab-html-conversion",
+		firstOverviewChapterPath = `/indicators/${indicatorSlug}/html-conversion`,
+		secondChapterPath = `/indicators/${indicatorSlug}/html-conversion/chapter/the-technologies`,
+		thirdAndLastChapterPath = `/indicators/${indicatorSlug}/html-conversion/chapter/committee-discussion`;
+
+	const props: ConvertedDocumentPrevNextProps = {
+		chapters: [
+			{
+				href: "/guidance/IND999/documents/html-conversion/chapter/recommendations",
+				slug: "recommendations",
+				title: "1 Recommendations"
+			},
+			{
+				href: "/guidance/IND999/documents/html-conversion/chapter/the-technologies",
+				slug: "the-technologies",
+				title: "2 The technologies"
+			},
+			{
+				href: "/guidance/IND999/documents/html-conversion/chapter/committee-discussion",
+				slug: "committee-discussion",
+				title: "3 Committee discussion"
+			}
+		]
+	};
+
+describe("ConvertedDocumentPrevNext", () => {
+	it("should render the next chapter page pagination link", () => {
+		mockRouterPath(secondChapterPath);
+		render(<ConvertedDocumentPrevNext {...props} />);
+		expect(
+			screen.getByRole("link", {
+				name: "Next page 3 Committee discussion",
+			})
+		).toHaveAttribute("href", thirdAndLastChapterPath);
+	});
+
+	it("should render the previous chapter page pagination link", () => {
+		mockRouterPath(secondChapterPath);
+		render(<ConvertedDocumentPrevNext {...props} />);
+		expect(
+			screen.getByRole("link", {
+				name: "Previous page 1 Recommendations",
+			})
+		).toHaveAttribute("href", firstOverviewChapterPath);
+	});
+
+	it("should not render a next page link on last chapter", () => {
+		mockRouterPath(thirdAndLastChapterPath);
+		render(<ConvertedDocumentPrevNext {...props} />);
+		expect(screen.queryByText("Next page")).not.toBeInTheDocument();
+	});
+
+	it("should not render a previous page link on first chapter", () => {
+		mockRouterPath(firstOverviewChapterPath);
+		render(<ConvertedDocumentPrevNext {...props} />);
+		expect(screen.queryByText("Previous page")).not.toBeInTheDocument();
+	});
+});
