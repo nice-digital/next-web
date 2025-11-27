59667f36043b0b7385523a91ae133dcce9c2b644
Dale H <dale.hudson@nice.org.uk>
2025-11-27 15:07:21 +0000

    NXT-839 update FormEmbed formId prop name, after SB model updates
    - Added TODO to look in to current types on JotFormEmbed component

--- web/src/components/Storyblok/FormEmbed/FormEmbed.tsx
+++ web/src/components/Storyblok/FormEmbed/FormEmbed.tsx
@@ -1,20 +1,31 @@
 import React from "react";
+
 import { JotFormEmbed } from "@/components/JotFormEmbed/JotFormEmbed";
 import { FormEmbedStoryblok } from "@/types/storyblok";
-
 
 interface FormEmbedBlokProps {
 	blok: FormEmbedStoryblok;
 }
 
-
+/** TODO: look at alternative types to handle jotFormID before we switch over forms to SB.
+ *
+ * jotFormID is currently typed as:
+ *  - export type FormID = `${number}`;
+ *
+ * The Storyblok field for the formId is text, so it's typed as a string.
+ *
+ *  Which currently means we have to pass the value from SB like this:
+ *  jotFormID={`${Number(formId)}`} which seems off.
+ *
+ * Type = string that only contains numeric characters.
+ */
 
 export const FormEmbed = ({ blok }: FormEmbedBlokProps): React.ReactElement => {
-	const { title, jotFormID } = blok;
+	const { title, formId } = blok;
 
 	return (
 		<>
-			<JotFormEmbed title={title} jotFormID={jotFormID} />
+			<JotFormEmbed title={title} jotFormID={`${Number(formId)}`} />
 		</>
 	);
 };
