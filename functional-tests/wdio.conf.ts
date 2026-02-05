d500653013c892387086d3ecfbfcebed443896f7
FunmiOsho <60607637+FunmiOsho@users.noreply.github.com>
2026-02-05 18:41:08 +0000

    NXT-866 comment out indicator specs in the wdio ts file

--- functional-tests/wdio.conf.ts
+++ functional-tests/wdio.conf.ts
@@ -14,6 +14,7 @@ export const config: WebdriverIO.Config = {
 
 	specs: ["./features/**/*.feature"],
 	exclude: [
+		"./features/**/indicator.feature",
 		"./features/**/researchListPage.feature",
 		"./features/**/researchPagination.feature",
 		"./features/**/filterRrlp.feature",
