08cf27e5851bdac8657221e8fbc88a637a47991d
John Holland <john.holland@nice.org.uk>
2026-02-19 10:15:48 +0000

    NXT-863 Updates to MHRA for deferred GALP (#515)
    * NXT-734 Mock data for new galp tests
    
    * NXT-734 Added new terminated and deferred GALPs
    
    * NXT-734 Changed variable from search api
    
    * NXT-734 Updated column for guidance programme to type
    
    * NXT-863 Added new deferred id column
    
    And updated snapshots.
    
    * NXT-863 Removed terminated galp page
    
    * NXT-863 Updated snapshots
    
    * NXT-863 Reinstated topic prioritisation type

--- web/src/components/ProductListNav/GuidanceListNav.tsx
+++ web/src/components/ProductListNav/GuidanceListNav.tsx
@@ -40,6 +40,7 @@ export const GuidanceListNav: FC = () => (
 		<NavLink href="/guidance/published">Published</NavLink>
 		<NavLink href="/guidance/inconsultation">In consultation</NavLink>
 		<NavLink href="/guidance/indevelopment">In development</NavLink>
+		<NavLink href="/guidance/deferred">Deferred</NavLink>
 		<NavLink href="/guidance/awaiting-development">
 			Awaiting development
 		</NavLink>
