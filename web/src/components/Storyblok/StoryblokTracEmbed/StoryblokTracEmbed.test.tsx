7a75b90c343fb4a39f668f11738ca0200819b1c3
Shilpa Unni <shilpa.unni@nice.org.uk>
2026-03-11 15:07:05 +0000

    NXT-930 Add valid unit test cases

--- web/src/components/Storyblok/StoryblokTracEmbed/StoryblokTracEmbed.test.tsx
+++ web/src/components/Storyblok/StoryblokTracEmbed/StoryblokTracEmbed.test.tsx
@@ -26,60 +26,60 @@ describe("StoryblokTracEmbed", () => {
 		process.env = ORIGINAL_ENV;
 	});
 
-	// it("should render the quote ", () => {
-	// 	render(<StoryblokTracEmbed blok={mockTracEmbed} />);
-	// 	const container = screen.getByLabelText("Trac Jobs Board");
-	// 	expect(container).toBeInTheDocument();
-	// 	expect(container).toHaveAttribute("id", "trac-jobs-container");
-	// 	expect(container).toHaveAttribute("aria-live", "polite");
-	// });
-	// it("should load the Trac embed script with correct attributes", () => {
-	// 	render(<StoryblokTracEmbed blok={mockTracEmbed} />);
-	// 	const script = screen.getByTestId("trac-feed-script");
-	// 	expect(script).toBeInTheDocument();
-	// 	expect(script).toHaveAttribute(
-	// 		"src",
-	// 		"https://feeds.trac.jobs/js/v18/EmbeddedJobsBoard.js"
-	// 	);
-	// 	expect(script).toHaveAttribute("data-JobsBoardID", "123456");
-	// 	expect(script).toHaveAttribute("data-crossorigin", "anonymous");
-	// 	expect(script).toHaveAttribute("data-IncludeCSS", "false");
-	// });
-	// it("should not load the script multiple times on re-render", () => {
-	// 	const { rerender } = render(<StoryblokTracEmbed blok={mockTracEmbed} />);
-	// 	const initialScript = screen.getByTestId("trac-feed-script");
-	// 	expect(initialScript).toBeInTheDocument();
+	it("should render Trac Embed div", () => {
+		render(<StoryblokTracEmbed blok={mockTracEmbed} />);
+		const container = screen.getByLabelText("Trac Jobs Board");
+		expect(container).toBeInTheDocument();
+		expect(container).toHaveAttribute("id", "trac-jobs-container");
+		expect(container).toHaveAttribute("aria-live", "polite");
+	});
+	it("should load the Trac embed script with correct attributes", () => {
+		render(<StoryblokTracEmbed blok={mockTracEmbed} />);
+		const script = screen.getByTestId("trac-feed-script");
+		expect(script).toBeInTheDocument();
+		expect(script).toHaveAttribute(
+			"src",
+			"https://feeds.trac.jobs/js/v18/EmbeddedJobsBoard.js"
+		);
+		expect(script).toHaveAttribute("data-JobsBoardID", "123456");
+		expect(script).toHaveAttribute("data-crossorigin", "anonymous");
+		expect(script).toHaveAttribute("data-IncludeCSS", "false");
+	});
+	it("should not load the script multiple times on re-render", () => {
+		const { rerender } = render(<StoryblokTracEmbed blok={mockTracEmbed} />);
+		const initialScript = screen.getByTestId("trac-feed-script");
+		expect(initialScript).toBeInTheDocument();
 
-	// 	rerender(<StoryblokTracEmbed blok={mockTracEmbed} />);
-	// 	const scripts = screen.getAllByTestId("trac-feed-script");
-	// 	expect(scripts).toHaveLength(1);
-	// });
+		rerender(<StoryblokTracEmbed blok={mockTracEmbed} />);
+		const scripts = screen.getAllByTestId("trac-feed-script");
+		expect(scripts).toHaveLength(1);
+	});
 
-	// it("should push a tracjobs-hashchange event to dataLayer when the hash changes", () => {
-	// 	const dataLayerPush = jest.spyOn(window.dataLayer, "push");
+	it("should push a tracjobs-hashchange event to dataLayer when the hash changes", () => {
+		const dataLayerPush = jest.spyOn(window.dataLayer, "push");
 
-	// 	render(<StoryblokTracEmbed blok={mockTracEmbed} />);
+		render(<StoryblokTracEmbed blok={mockTracEmbed} />);
 
-	// 	window.location.hash = "#job_123";
-	// 	fireEvent(window, new HashChangeEvent("hashchange"));
+		window.location.hash = "#job_123";
+		fireEvent(window, new HashChangeEvent("hashchange"));
 
-	// 	expect(dataLayerPush).toHaveBeenCalledWith({
-	// 		event: "tracjobs-hashchange",
-	// 		hash: "#job_123",
-	// 		url: window.location.href,
-	// 	});
-	// });
-	// it("should remove event listener on unmount", () => {
-	// 	const removeEventListenerSpy = jest.spyOn(window, "removeEventListener");
-	// 	const { unmount } = render(<StoryblokTracEmbed blok={mockTracEmbed} />);
+		expect(dataLayerPush).toHaveBeenCalledWith({
+			event: "tracjobs-hashchange",
+			hash: "#job_123",
+			url: window.location.href,
+		});
+	});
+	it("should remove event listener on unmount", () => {
+		const removeEventListenerSpy = jest.spyOn(window, "removeEventListener");
+		const { unmount } = render(<StoryblokTracEmbed blok={mockTracEmbed} />);
 
-	// 	unmount();
+		unmount();
 
-	// 	expect(removeEventListenerSpy).toHaveBeenCalledWith(
-	// 		"hashchange",
-	// 		expect.any(Function)
-	// 	);
-	// });
+		expect(removeEventListenerSpy).toHaveBeenCalledWith(
+			"hashchange",
+			expect.any(Function)
+		);
+	});
 	it("should return invalid trac configuration message if the version undefined", () => {
 		const originalEnv = process.env;
 
