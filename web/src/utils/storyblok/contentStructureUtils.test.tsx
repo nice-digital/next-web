966fb0a40b759c8ad2fd53d06691516db6397713
Dale H <dale.hudson@nice.org.uk>
2026-04-10 10:49:03 +0100

    NXT-802 add test coverage for derivePreheaderFromBreadCrumbs

--- web/src/utils/storyblok/contentStructureUtils.test.tsx
+++ web/src/utils/storyblok/contentStructureUtils.test.tsx
@@ -1,8 +1,15 @@
-// import { render, screen } from "@testing-library/react";
-
 import mockData from "@/mockData/storyblok/sectionNavData.json";
+import { type Breadcrumb as TypeBreadcrumb } from "@/types/Breadcrumb";
+import { fetchLinks } from "@/utils/storyblok";
 
-import { treeHasItems } from "./contentStructureUtils";
+import {
+	treeHasItems,
+	derivePreheaderFromBreadcrumbs,
+} from "./contentStructureUtils";
+
+jest.mock("@/utils/storyblok", () => ({
+	fetchLinks: jest.fn(),
+}));
 
 describe("sectionNavIsNotEmpty", () => {
 	it("returns true for a non-empty array", () => {
@@ -12,5 +19,55 @@ describe("sectionNavIsNotEmpty", () => {
 
 	it("returns false for an empty array", () => {
 		expect(treeHasItems([])).toBe(false);
+	});
+});
+
+describe("derivePreheaderFromBreadcrumbs", () => {
+	it("should return undefined if breadcrumbs are undefined", () => {
+		expect(derivePreheaderFromBreadcrumbs(undefined)).toBeUndefined();
+	});
+
+	it("should return undefined if breadcrumbs only contains one item", () => {
+		const mockBreadcrumbs: TypeBreadcrumb[] = [{ title: "Home", path: "/" }];
+
+		expect(derivePreheaderFromBreadcrumbs(mockBreadcrumbs)).toBeUndefined();
+	});
+
+	it("should return undefined when the derived candidate is Home", () => {
+		const mockBreadcrumbs: TypeBreadcrumb[] = [
+			{ title: "Section", path: "/section" },
+			{ title: "Home", path: "/" },
+		];
+
+		expect(derivePreheaderFromBreadcrumbs(mockBreadcrumbs)).toBeUndefined();
+	});
+
+	it("should return undefined breadcrumbs are empty", () => {
+		const mockBreadcrumbs: TypeBreadcrumb[] = [];
+
+		expect(derivePreheaderFromBreadcrumbs(mockBreadcrumbs)).toBeUndefined();
+	});
+
+	it("should return the last breadcrumb title", () => {
+		const mockBreadcrumbs: TypeBreadcrumb[] = [
+			{ title: "Home", path: "/" },
+			{
+				title: "Implementing NICE guidance",
+				path: "/implementing-nice-guidance",
+			},
+			{
+				title: "Cost saving, resource planning and audit",
+				path: "/implementing-nice-guidance/cost-saving-resource-planning-and-audit",
+			},
+			{
+				title:
+					"Supporting the health and care system to implement virtual wards",
+				path: "/implementing-nice-guidance/cost-saving-resource-planning-and-audit/supporting-the-health-care-system-to-implement-virtual-wards",
+			},
+		];
+
+		expect(derivePreheaderFromBreadcrumbs(mockBreadcrumbs)).toEqual(
+			mockBreadcrumbs[mockBreadcrumbs.length - 1].title
+		);
 	});
 });
