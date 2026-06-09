7c58f529814284e822170a242724093f541361c1
John Holland <john.holland@nice.org.uk>
2026-06-09 10:09:43 +0100

    NXT-902 Change to alert type to fix failing test

--- web/src/feeds/publications/types.ts
+++ web/src/feeds/publications/types.ts
@@ -599,7 +599,7 @@ export type ProductDetail = ProductAndResourceBase & {
 	productTypeName: string;
 	/** The plural name of the product type e.g. "NICE indicators" */
 	productTypeNamePlural: string;
-	alert: string;
+	alert: string | null;
 };
 
 /** The type of the response from a chapter endpoint e.g. /feeds/product/ind69/part/1/chapter/overview */
